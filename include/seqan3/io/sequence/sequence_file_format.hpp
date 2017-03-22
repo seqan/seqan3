36e78ce38d9551b1c1358c02e8c1fb8a9b6d3186
marehr <marehr@zedat.fu-berlin.de>
2017-03-22 21:50:37 +0000

    make meets_sequence_file_format_concept great again

--- include/seqan3/io/sequence/sequence_file_format.hpp
+++ include/seqan3/io/sequence/sequence_file_format.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <utility>
+#include <variant>
+
 namespace seqan3
 {
 
@@ -12,22 +15,17 @@ concept bool sequence_file_format_concept = requires (t v)
     // TODO constructor
 
     //TODO how do you specifiy a function template?
-    v.read(sequence_type & seq, id_type & id, qual_type & qual);
+    // v.read(sequence_type & seq, id_type & id, qual_type & qual);
 
 };
 
 namespace detail
 {
 
-template <size_t index, typename variant_type>
-constexpr bool meets_concept_sequence_file_format()
+template <typename variant_type, std::size_t ...I>
+constexpr bool meets_sequence_file_format_concept(std::index_sequence<I...>)
 {
-    if constexpr (index == variant_size_v<variant_type>)
-        return true;
-    else if constexpr (!sequence_file_format_concept<variant_alternative_t<index, variant_type>>)
-        return false;
-    else
-        return meets_concept_sequence_file_format<index+1, variant_type>();
+    return (sequence_file_format_concept<std::variant_alternative_t<I, variant_type>> && ...);
 }
 
 } // namespace detail
