c7e366a07ec08ac59718e7ad475aa10efa094148
Marie Hoffmann <marie.hoffmann@fu-berlin.de>
2017-03-22 12:05:14 +0000

    Add initial phred quality concept, illumina 1.8 implementation and test.

--- include/seqan3/alphabet/quality.hpp
+++ include/seqan3/alphabet/quality.hpp
@@ -3,9 +3,34 @@
 #include <iostream>
 #include <string>
 
-namespace seqan3
+#include "alphabet.hpp"
+
+namespace seqan3 {
+
+// ------------------------------------------------------------------
+// concept
+// ------------------------------------------------------------------
+
+template<typename q>
+concept bool quality_concept = requires(q quality)
 {
+    // requires fulfillment of alphabet concept
+    requires alphabet_concept<q>;
+
+    // static data members of type value_type and char_type
+    requires std::is_same_v<typename q::integral_type, decltype(q::value)>;
+
+    // offsets for char and integer encoding
+    q::offset_phred;
+    q::offset_char;
+    requires std::is_same_v<typename q::phred_type, decltype(q::offset_phred)>;
+    requires std::is_same_v<typename q::char_type, decltype(q::offset_char)>;
+
+    // conversion functions from t -> c and vice versa
+    { quality.to_phred() } -> typename q::phred_type;
+    { quality.from_phred(typename q::integral_type{}) } -> q;
 
 
+};
 
 }
--- include/seqan3/alphabet/quality/phred.hpp
+++ include/seqan3/alphabet/quality/phred.hpp
@@ -1,3 +1,39 @@
+// ============================================================================
+//                 SeqAn - The Library for Sequence Analysis
+// ============================================================================
+//
+// Copyright (c) 2006-2017, Knut Reinert & Freie Universitaet Berlin
+// Copyright (c) 2016-2017, Knut Reinert & MPI Molekulare Genetik
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Knut Reinert or the FU Berlin nor the names of
+//       its contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL KNUT REINERT OR THE FU BERLIN BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
+//
+// ============================================================================
+// Author: Marie Hoffmann <marie.hoffmann AT fu-berlin.de>
+// ============================================================================
+
 #pragma once
 
 #include "../alphabet.hpp"
@@ -7,25 +43,24 @@
 // assume sequential mapping w.r.t. ascii alphabet, only (char_start, val_start) and (char_end, val_end) differ
 namespace seqan3
 {
-    // 0 -> '!' ... 41 -> 'J'
+    // integer score in [0 .. 41], char score in  ['!' .. 'J']
     struct illumina18
     {
-        // strictly typed enum, unfortunately with scope
-        using c_type = uint8_t;
-        char offset = '!';
-        uint8_t size = 42;
+        using char_type = char;
+        using integral_type = uint8_t;
+        using phred_type = int8_t;
+
+        // todo: make this offsets concept
+        static constexpr char_type offset_char{'!'};
+        static constexpr integral_type offset_phred{0};
 
         // the value
-        c_type value;
+        integral_type value;
 
         // implicit compatibility to inner_type
-        constexpr illumina18 & operator =(c_type const c)
+        constexpr illumina18 & operator =(integral_type const c)
         {
             value = c;
-        }
-        constexpr operator c_type() const
-        {
-            return value;
         }
 
         // explicit compatibility to char
@@ -34,20 +69,19 @@ namespace seqan3
             return to_char();
         }
 
-        //
-        constexpr char to_char() const
+        constexpr char_type to_char() const
         {
-            return this->value + this->offset;
+            return value + offset_char;
         }
 
-        constexpr illumina18 from_char(char const c)
+        constexpr illumina18 from_char(char_type const c)
         {
             value = c - '!';
             return *this;
         }
 
         // explicit compatibility to integral
-        constexpr char to_integral() const
+        constexpr integral_type to_integral() const
         {
             return value;
         }
@@ -58,8 +92,19 @@ namespace seqan3
             return *this;
         }
 
-        // conversion tables
-        static constexpr uint8_t value_size{};
+        constexpr illumina18 from_phred(int8_t const p)
+        {
+            value = p - offset_phred;
+            return *this;
+        }
+
+        constexpr phred_type to_phred(uint8_t const p)
+        {
+            return value + offset_phred;
+        }
+
+        //
+        static constexpr uint8_t value_size{42};
 
     };
 
