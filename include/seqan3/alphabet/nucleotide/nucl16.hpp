9eb709b28c355d8ff8e878aa270000a53d34542f
Hannes Hauswedell <hannes.hauswedell@fu-berlin.de>
2017-04-17 22:08:12 +0200

    [INTERNAL] bring nucl16 up to style guide + fixes

--- include/seqan3/alphabet/nucleotide/nucl16.hpp
+++ include/seqan3/alphabet/nucleotide/nucl16.hpp
@@ -31,48 +31,159 @@
 // DAMAGE.
 //
 // ============================================================================
-// Author: Sara Hetzel <sara.hetzel AT fu-berlin.de>
-// ============================================================================
+
+/*!\file alphabet/nucleotide/nucl16.hpp
+ * \ingroup alphabet
+ * \author Sara Hetzel <sara.hetzel AT fu-berlin.de>
+ * \brief Contains seqan3::nucl16, container aliases and string literals.
+ */
 
 #pragma once
 
 #include <cassert>
 
-#include <seqan3/alphabet/concept.hpp>
+#include <string>
+#include <vector>
 
-/*! The sixteen letter nucleotide alphabet
- * \ingroup alphabet
- */
+#include <seqan3/alphabet/nucleotide/concept.hpp>
+
+// ------------------------------------------------------------------
+// nucl16
+// ------------------------------------------------------------------
 
 namespace seqan3
 {
-/*! The sixteen letter nucleotide alphabet of A, B, C, D, G, H, K, M, N, R, S, T, U, V, W, Y
+
+/*!\brief The 16 letter DNA alphabet, containing all IUPAC smybols.
+ * \ingroup alphabet
  *
- * The alphabet may be brace initialized from the static letter members (see above). Note that you cannot assign
- * regular characters, but additional functions for this are available.
+ * \details
+ * Note that in contrast to seqan3::dna4, seqan3::rna4, seqan3::dna5 and seqan3::rna5
+ * the letters 'T' and 'U' are distinct values in this alphabet.
  *
- * ~~~~~~~~~~~~~~~{.cpp}
+ *~~~~~~~~~~~~~~~{.cpp}
  *     nucl16 my_letter{nucl16::A};
  *     // doesn't work:
  *     // nucl16 my_letter{'A'};
  *
  *     my_letter.from_char('C'); // <- this does!
  *
- *     my_letter.from_char('E'); // converted to N internally
+ *     my_letter.from_char('F'); // converted to N internally
  *     if (my_letter.to_char() == 'N')
  *        std::cout << "yeah\n"; // "yeah";
- * ~~~~~~~~~~~~~~~
+ *~~~~~~~~~~~~~~~
  */
 
 struct nucl16
 {
-    //! the type of the alphabet when converted to char (e.g. via @link to_char @endlink)
+    //!\brief The type of the alphabet when converted to char (e.g. via to_char()).
     using char_type = char;
-    //! the type of the alphabet when represented as a number (e.g. via @link to_integral @endlink)
+    //!\brief The type of the alphabet when represented as a number (e.g. via to_integral()).
     using integral_type = uint8_t;
 
-    // strictly typed enum, unfortunately with scope
-    //! \privatesection
+    /*!\name Letter values
+     * \brief Static member "letters" that can be assigned to the alphabet or used in aggregate initialization.
+     * \details Similar to an Enum interface . *Don't worry about the `internal_type`.*
+     */
+    //!\{
+    static const nucl16 A;
+    static const nucl16 B;
+    static const nucl16 C;
+    static const nucl16 D;
+    static const nucl16 G;
+    static const nucl16 H;
+    static const nucl16 K;
+    static const nucl16 M;
+    static const nucl16 N;
+    static const nucl16 R;
+    static const nucl16 S;
+    static const nucl16 T;
+    static const nucl16 U;
+    static const nucl16 V;
+    static const nucl16 W;
+    static const nucl16 Y;
+    static const nucl16 UNKNOWN;
+    //!\}
+
+    /*!\name Read functions
+     * \{
+     */
+    //!\brief Return the letter as a character of char_type.
+    constexpr char_type to_char() const noexcept
+    {
+        return value_to_char[static_cast<integral_type>(_value)];
+    }
+
+    //!\brief Return the letter's numeric value or rank in the alphabet.
+    constexpr integral_type to_integral() const noexcept
+    {
+        return static_cast<integral_type>(_value);
+    }
+    //!\}
+
+    /*!\name Write functions
+     * \{
+     */
+    //!\brief Assign from a character.
+    constexpr nucl16 & from_char(char_type const c) noexcept
+    {
+        _value = char_to_value[c];
+        return *this;
+    }
+
+    //!\brief Assign from a numeric value.
+    constexpr nucl16 & from_integral(integral_type const c)
+    {
+        assert(c < value_size);
+        _value = static_cast<internal_type>(c);
+        return *this;
+    }
+    //!\}
+
+    //!\brief The size of the alphabet, i.e. the number of different values it can take.
+    static constexpr integral_type value_size{16};
+
+    //!\name Comparison operators
+    //!\{
+    constexpr bool operator==(nucl16 const & rhs) const noexcept
+    {
+        return _value == rhs._value;
+    }
+
+    constexpr bool operator!=(nucl16 const & rhs) const noexcept
+    {
+        return _value != rhs._value;
+    }
+
+    constexpr bool operator<(nucl16 const & rhs) const noexcept
+    {
+        return _value < rhs._value;
+    }
+
+    constexpr bool operator>(nucl16 const & rhs) const noexcept
+    {
+        return _value > rhs._value;
+    }
+
+    constexpr bool operator<=(nucl16 const & rhs) const noexcept
+    {
+        return _value <= rhs._value;
+    }
+
+    constexpr bool operator>=(nucl16 const & rhs) const noexcept
+    {
+        return _value >= rhs._value;
+    }
+    //!\}
+
+protected:
+    //!\privatesection
+    /*!\brief The internal type is a strictly typed enum.
+     *
+     * This is done to prevent aggregate initialization from numbers and/or chars.
+     * It is has the drawback that it also introduces a scope which in turn makes
+     * the static "letter values " members necessary.
+     */
     enum struct internal_type : integral_type
     {
         A,
@@ -93,106 +204,8 @@ struct nucl16
         Y,
         UNKNOWN = N
     };
-    internal_type value;
-    //! \publicsection
 
-    // import internal_types values into local scope:
-
-    /*! @name letter values
-     * Static member "letters" that can be assigned to the alphabet or used in aggregate initialization.
-     * *Don't worry about the `internal_type`.*
-     */
-    //!@{
-    static const nucl16 A;
-    static const nucl16 B;
-    static const nucl16 C;
-    static const nucl16 D;
-    static const nucl16 G;
-    static const nucl16 H;
-    static const nucl16 K;
-    static const nucl16 M;
-    static const nucl16 N;
-    static const nucl16 R;
-    static const nucl16 S;
-    static const nucl16 T;
-    static const nucl16 U;
-    static const nucl16 V;
-    static const nucl16 W;
-    static const nucl16 Y;
-    static const nucl16 UNKNOWN;
-    //!@}
-
-    //! ability to cast to @link char_type @endlink **explicitly**.
-    explicit constexpr operator char_type() const
-    {
-        return to_char();
-    }
-
-    //! return the letter as a character of @link char_type @endlink.
-    constexpr char_type to_char() const
-    {
-        return value_to_char[static_cast<integral_type>(value)];
-    }
-
-    //! assign from a character
-    constexpr nucl16 from_char(char_type const c)
-    {
-        value = char_to_value[c];
-        return *this;
-    }
-
-    //! return the letter's numeric value or rank in the alphabet
-    constexpr integral_type to_integral() const
-    {
-        return static_cast<integral_type>(value);
-    }
-
-    //! assign from a numeric value
-    constexpr nucl16 from_integral(integral_type const c)
-    {
-        assert(c < value_size);
-        value = static_cast<internal_type>(c);
-        return *this;
-    }
-
-    //! The size of the alphabet, i.e. the number of different values it can take.
-    static constexpr integral_type value_size{16};
-
-    //! @name comparison operators
-    //!@{
-    constexpr bool operator==(nucl16 const & rhs) const
-    {
-        return value == rhs.value;
-    }
-
-    constexpr bool operator!=(nucl16 const & rhs) const
-    {
-        return value != rhs.value;
-    }
-
-    constexpr bool operator<(nucl16 const & rhs) const
-    {
-        return value < rhs.value;
-    }
-
-    constexpr bool operator>(nucl16 const & rhs) const
-    {
-        return value > rhs.value;
-    }
-
-    constexpr bool operator<=(nucl16 const & rhs) const
-    {
-        return value <= rhs.value;
-    }
-
-    constexpr bool operator>=(nucl16 const & rhs) const
-    {
-        return value >= rhs.value;
-    }
-    //!@}
-
-    //! \privatesection
-    // conversion tables
+    //!\brief Value to char conversion table.
     static constexpr char_type value_to_char[value_size]
     {
         'A',
@@ -213,6 +226,7 @@ struct nucl16
         'Y'
     };
 
+    //!\brief Char to value conversion table.
     static constexpr internal_type char_to_value[256]
     {
         internal_type::UNKNOWN, internal_type::UNKNOWN, internal_type::UNKNOWN, internal_type::UNKNOWN,
@@ -294,7 +308,11 @@ struct nucl16
         internal_type::UNKNOWN, internal_type::UNKNOWN, internal_type::UNKNOWN, internal_type::UNKNOWN,
         internal_type::UNKNOWN, internal_type::UNKNOWN, internal_type::UNKNOWN, internal_type::UNKNOWN
     };
-
+public:
+    //!\privatesection
+    //!\brief The data member.
+    internal_type _value;
+    //!\publicsection
 };
 
 constexpr nucl16 nucl16::A{internal_type::A};
@@ -315,9 +333,143 @@ constexpr nucl16 nucl16::W{internal_type::W};
 constexpr nucl16 nucl16::Y{internal_type::Y};
 constexpr nucl16 nucl16::UNKNOWN{nucl16::N};
 
+} // namespace seqan3
+
+namespace seqan3::detail
+{
+
+//!\brief seqan3::nucl16 is defined as being a nucleotide alphabet.
+template <>
+struct is_nucleotide<nucl16> : public std::true_type
+{};
+
+} // namespace seqan3::detail
+
 #ifndef NDEBUG
-static_assert(alphabet_concept<nucl16>);
-static_assert(detail::internal_alphabet_concept<nucl16>);
+static_assert(seqan3::alphabet_concept<seqan3::nucl16>);
+static_assert(seqan3::detail::internal_alphabet_concept<seqan3::nucl16>);
+static_assert(seqan3::nucleotide_concept<seqan3::nucl16>);
 #endif
 
+// ------------------------------------------------------------------
+// containers
+// ------------------------------------------------------------------
+
+namespace seqan3
+{
+    /*!\name Alphabet aliases
+     * \{
+     * \brief Other names (typedefs) for seqan3::nucl16
+     * \relates nucl16
+     */
+    using dna16 = nucl16;
+    using rna16 = nucl16;
+    using dna = nucl16;
+    using rna = nucl16;
+    //!\}
+
+} // namespace seqan3
+
+
+// ------------------------------------------------------------------
+// containers
+// ------------------------------------------------------------------
+
+namespace seqan3
+{
+
+//!\brief Alias for an std::vector of seqan3::nucl16.
+//!\relates nucl16
+using nucl16_vector = std::vector<nucl16>;
+
+
+/*!\brief Alias for an std::basic_string of seqan3::nucl16.
+ * \relates nucl16
+ *
+ * \attention
+ * Note that we recommend using seqan3::nucl16_vector instead of nucl16_string in almost all situations.
+ * While the C++ style operations on the string are well supported, you should not access the internal c-string
+ * and should not use C-Style operations on it, e.g. the `char_traits::strlen` function will not return the
+ * correct length of the string (while the `.size()` returns the correct value).
+ */
+using nucl16_string = std::basic_string<nucl16, std::char_traits<nucl16>>;
+
+} // namespace seqan3
+
+// ------------------------------------------------------------------
+// literals
+// ------------------------------------------------------------------
+
+namespace seqan3::literal
+{
+
+/*!\brief nucl16 literal
+ * \relates seqan3::nucl16
+ * \returns seqan3::nucl16_vector
+ *
+ * You can use this string literal to easily assign to nucl16_vector:
+ *
+ *~~~~~~~~~~~~~~~{.cpp}
+ *     // these don't work:
+ *     // nucl16_vector foo{"ACGTTA"};
+ *     // nucl16_vector bar = "ACGTTA";
+ *
+ *     // but these do:
+ *     using namespace seqan3::literal;
+ *     nucl16_vector foo{"ACGTTA"_nucl16};
+ *     nucl16_vector bar = "ACGTTA"_nucl16;
+ *     auto bax = "ACGTTA"_nucl16;
+ *~~~~~~~~~~~~~~~
+ *
+ * \attention
+ * All seqan3 literals are in the namespace seqan3::literal!
+ */
+
+inline nucl16_vector operator "" _nucl16(const char * s, std::size_t n)
+{
+    nucl16_vector r;
+    r.resize(n);
+
+    for (size_t i = 0; i < n; ++i)
+        r[i].from_char(s[i]);
+
+    return r;
 }
+
+/*!\brief nucl16 string literal
+ * \relates seqan3::nucl16
+ * \returns seqan3::nucl16_string
+ *
+ * You can use this string literal to easily assign to nucl16_vector:
+ *
+ *~~~~~~~~~~~~~~~{.cpp}
+ *     // these don't work:
+ *     // nucl16_string foo{"ACGTTA"};
+ *     // nucl16_string bar = "ACGTTA";
+ *
+ *     // but these do:
+ *     using namespace seqan3::literal;
+ *     nucl16_string foo{"ACGTTA"_nucl16s};
+ *     nucl16_string bar = "ACGTTA"_nucl16s;
+ *     auto bax = "ACGTTA"_nucl16s;
+ *~~~~~~~~~~~~~~~
+ *
+ * Please note the limitations of seqan3::nucl16_string and consider using the \link operator""_nucl16 \endlink instead.
+ *
+ * \attention
+ * All seqan3 literals are in the namespace seqan3::literal!
+ */
+
+inline nucl16_string operator "" _nucl16s(const char * s, std::size_t n)
+{
+    nucl16_string r;
+    r.resize(n);
+
+    for (size_t i = 0; i < n; ++i)
+        r[i].from_char(s[i]);
+
+    return r;
+}
+
+} // namespace seqan3::literal
+
