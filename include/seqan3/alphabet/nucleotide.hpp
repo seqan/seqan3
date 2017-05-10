b8743b11449189b51b31fb797bbf04b17abedff3
Hannes Hauswedell <h2+github.com@fsfe.org>
2017-05-10 20:37:09 +0200

    [DOC] greatly improve nucleotide documentation

--- include/seqan3/alphabet/nucleotide.hpp
+++ include/seqan3/alphabet/nucleotide.hpp
@@ -49,6 +49,49 @@
  * \brief Contains the different DNA and RNA alphabet types.
  * \ingroup alphabet
  *
+ * \par Introduction
+ *
+ * Nucleotide sequences are at the core of most bioinformatic data processing and while it is possible
+ * to represent them in a regular std::string, it makes sense to have specialised data structures in most cases.
+ * This sub-module offers multiple nucleotide alphabets that can be used with regular container and ranges.
+ *
+ * | Letter   | Description     | seqan3::nucl16 | seqan3::dna5 | seqan3::dna4 | seqan3::rna5 | seqan3::rna4 |
+ * |:--------:|-----------------|:--------------:|:------------:|:------------:|:------------:|:------------:|
+ * |   A      | Adenine         |      ☑         |      ☑       |      ☑       |      ☑       |      ☑       |
+ * |   C      | Cytosine        |      ☑         |      ☑       |      ☑       |      ☑       |      ☑       |
+ * |   G      | Guanine         |      ☑         |      ☑       |      ☑       |      ☑       |      ☑       |
+ * |   T      | Thymine (DNA)   |      ☑         |      ☑       |      ☑       |      ☐       |      ☐       |
+ * |   U      | Uracil (RNA)    |      ☑         |      ☐       |      ☐       |      ☑       |      ☑       |
+ * |   R      | A *or* G        |      ☑         |      ☐       |      ☐       |      ☐       |      ☐       |
+ * |   Y      | C *or* T        |      ☑         |      ☐       |      ☐       |      ☐       |      ☐       |
+ * |   S      | G *or* C        |      ☑         |      ☐       |      ☐       |      ☐       |      ☐       |
+ * |   W      | A *or* T        |      ☑         |      ☐       |      ☐       |      ☐       |      ☐       |
+ * |   K      | G *or* T        |      ☑         |      ☐       |      ☐       |      ☐       |      ☐       |
+ * |   M      | A *or* C        |      ☑         |      ☐       |      ☐       |      ☐       |      ☐       |
+ * |   B      | C *or* G *or* T |      ☑         |      ☐       |      ☐       |      ☐       |      ☐       |
+ * |   D      | A *or* G *or* T |      ☑         |      ☐       |      ☐       |      ☐       |      ☐       |
+ * |   H      | A *or* C *or* T |      ☑         |      ☐       |      ☐       |      ☐       |      ☐       |
+ * |   V      | A *or* C *or* G |      ☑         |      ☐       |      ☐       |      ☐       |      ☐       |
+ * |   N      | any base        |      ☑         |      ☑       |      ☐       |      ☑       |      ☐       |
+ * | **Size** |                 |     16         |      5       |      4       |      5       |      4       |
+ *
+ * Keep in mind, that while we think of "the nucleotide alphabet" as consisting of four bases, there are indeed
+ * more characters defined with different levels of ambiguity. Depending on your application it will make sense
+ * to preserve this ambiguity or to discard it to save space and/or optimise computations.
+ * SeqAn offers five distinct nucleotide alphabet types to accommodate for this.
+ *
+ * For seqan3::nucl16 there are also the following aliases (typedefs): `dna16, rna16, dna, rna`
+ *
+ * The specialised RNA alphabets are provided for convenience, however the DNA alphabets provide a `::U` member
+ * and can handle being assigned and `'U'` character, as well. See below for the details.
+ *
+ * Which alphabet to chose?
+ *   1. in most cases, take seqan3::nucl16
+ *   2. if you are memory constrained and sequence data is actually the main memory consumer, use seqan3::dna5
+ *   3. if you use specialised algorithms that profit from a 2-bit representation, use seqan3::dna4
+ *   4. if 2. or 3. and you are doing only RNA input/output, use seqan3::rna5 or seqan3::rna4
+ *   5. to actually save space from using smaller alphabets, you need a compressed container (TODO link)
+ *
  * \par Concept
  *
  * The nucleotide submodule defines seqan3::nucleotide_concept which encompasses all the alphabets defined in the
@@ -64,29 +107,53 @@
  *     // ...
  * ~~~
  *
- * \par Alphabets
+ * All alphabets define at least the letters `::A`, `::C`, `::G`, `::T`, `::U`, `::UNKNOWN` (although some might be
+ * aliases of others).
  *
- * The nucleotide module contains alphabet data types for nucleic acids. The types differ mainly
- * in their size and how they treat the T and U bases:
+ * \par Printing and conversion to char
  *
- * | Alphabet       | Aliases                | Size | 'T' | 'U' | `UNKNOWN` |
- * |----------------|------------------------|------|-----|-----|-----------|
- * |   seqan3::dna4 |                        |    4 |  ☑  |  ☐  |       'A' |
- * |   seqan3::rna4 |                        |    4 |  ☐  |  ☑  |       'A' |
- * |   seqan3::dna5 |                        |    5 |  ☑  |  ☐  |       'N' |
- * |   seqan3::rna5 |                        |    5 |  ☐  |  ☑  |       'N' |
- * | seqan3::nucl16 | dna16, rna16, dna, rna |   16 |  ☑  |  ☑  |       'N' |
  *
- * The default and recommended alphabet is seqan3::nucl16. It can contain all the IUPAC symbols for nucleic acid data.
- * The other alphabets are smaller representations that are especially useful inside compressed containers (TODO link).
+ * | to_char()      | seqan3::nucl16 | seqan3::dna5 | seqan3::dna4 | seqan3::rna5 | seqan3::rna4 |
+ * |:--------------:|:--------------:|:------------:|:------------:|:------------:|:------------:|
+ * | type::A        |    `'A'`       |    `'A'`     |    `'A'`     |    `'A'`     |    `'A'`     |
+ * |   ...          |    ...         |    ...       |    ...       |    ...       |    ...       |
+ * | type::T        |    `'T'`       |    `'T'`     |    `'T'`     |    `'U'`     |    `'U'`     |
+ * | type::U        |    `'U'`       |    `'T'`     |    `'T'`     |    `'U'`     |    `'U'`     |
+ * | type::R        |    `'R'`       |     ---      |    ---       |     ---      |     ---      |
+ * |   ...          |    ...         |     ---      |    ---       |     ---      |     ---      |
+ * | type::N        |    `'N'`       |    `'N'`     |    ---       |    `'N'`     |     ---      |
+ * | type::UNKNOWN  |    `'N'`       |    `'N'`     |    `'A'`     |    `'N'`     |    `'A'`     |
  *
- * In the smaller alphabets `T` and `U` are represented by the same rank and you cannot differentiate between them,
- * they are, however, converted into each other and not to the `UNKNOWN` character. i.e. you can use seqan3::dna5 to
- * represent RNA data, as well, the only difference is the output when calling to_char().
+ * In the smaller alphabets `T` and `U` are represented by the same rank and you cannot differentiate between them
+ * (the static members `::T` and `::U` are synonymous). The only difference between e.g. seqan3::dna4 and
+ * seqan3::rna4 is the output when calling to_char().
  *
- * \par Conversion
+ * \par Assignment and Conversion
  *
  *   * seqan3::dna4 ↔ seqan3::rna4, as well as seqan3::dna5 ↔ seqan3::rna5 are directly assignable to each other.
- *   * All nucleotide alphabets are convertible to each other via convert().
+ *   * All nucleotide alphabets are convertible to each other, as well as from their `char` and rank types via
+ *     seqan3::convert(), although the behaviour depends on the type, see below.
  *   * All ranges of nucleotide alphabets are convertible to each other via seqan3::view::convert.
+ *
+ * | assign_char() | seqan3::nucl16 | seqan3::dna5  | seqan3::dna4  | seqan3::rna5 | seqan3::rna4  |
+ * |:-------------:|:--------------:|:-------------:|:-------------:|:------------:|:-------------:|
+ * |   `'A'`       |  nucl16::A     |  dna5::A      | dna4::A       |  rna5::A     | rna4::A       |
+ * |   ...         |    ...         |    ...        | ...           |    ...       |     ...       |
+ * |   `'T'`       |  nucl16::T     |  dna5::T ¹    | dna4::T ¹     |  rna5::T ¹   | rna4::T ¹     |
+ * |   `'U'`       |  nucl16::U     |  dna5::T ¹    | dna4::T ¹     |  rna5::T ¹   | rna4::T ¹     |
+ * |   `'R'`       |  nucl16::R     |  dna5::N ²    | dna4::A ³     |  rna5::N ²   | rna4::A ³     |
+ * |   `'Y'`       |  nucl16::Y     |  dna5::N ²    | dna4::C ³     |  rna5::N ²   | rna4::C ³     |
+ * |   ...         |    ...         |  dna5::N ²    | ... ³         |  rna5::N ²   | ... ³         |
+ * |   `'N'`       |  nucl16::N     |  dna5::N ²    | dna4::A ³     |  rna5::N ²   | rna4::A ³     |
+ * | e.g. `'!'`    |  nucl16::N     |  dna5::N ²    | dna4::A ²     |  rna5::N ²   | rna4::A ²     |
+ *
+ * ¹ same as `type::U`<br>
+ * ² same as `type::UNKNOWN`<br>
+ * ³ the first character in the ambiguous set
+ *
+ * When converting from `char` or a larger nucleotide alphabet to smaller one, *loss of information* can occur since
+ * obviously some bases are not available. When converting to seqan3::dna5 or seqan3::rna5, non-canonical
+ * bases (letters other than A, C, G, T, U) are converted to `'N'` to preserve ambiguity at that position, while
+ * for seqan3::dna4 and seqan3::rna4 they are converted to the first of the possibilities they represent (because
+ * there is no letter `'N'` to represent ambiguity).
  */
