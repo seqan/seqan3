8cff07daf50b21121319df492035d072d16d2d5f
Temesgen H. Dadi <temehi@gmail.com>
2017-08-01 13:38:00 +0200

    [FEATURE] read fasta records

--- include/seqan3/io/sequence/sequence_file_format_fasta.hpp
+++ include/seqan3/io/sequence/sequence_file_format_fasta.hpp
@@ -53,122 +53,167 @@
 
 namespace seqan3
 {
-
+/*!\brief defines FASTA format specific read and write functions
+ * \ingroup sequence
+ *
+ * \details
+ * FASTA format
+ * ------------
+ * FASTA format is a plain text format used mainly to sore a sequence of nucleotides or amino acids.
+ * A FASTA formated file can have one or more entries. Each entry represents a contiguous sequence and is composed
+ * of a sequence identifier (id) and the sequence itself. The first line of an entry starts with '>' character.
+ * The rest of this line is the id of the sequence that follows. The actual sequence is written starting from a newline
+ * and continues until another line starting with '>' appears or until the end of the file. Unlike the id a single
+ * sequence could span multiple lines. (These lines are usually of the same length with an exception to the last line).
+ *
+ * ### Example #######
+ *
+ * > &gt;gi|186681228|ref|YP_001864424.1| phycoerythrobilin:ferredoxin oxidoreductase<br>
+ * > MNSERSDVTLYQPFLDYAIAYMRSRLDLEPYPIPTGFESNSAVVGKGKNQEEVVTTSYAFQTAKLRQIRA<br>
+ * > AHVQGGNSLQVLNFVIFPHLNYDLPFFGADLVTLPGGHLIALDMQPLFRDDSAYQAKYTEPILPIFHAHQ<br>
+ * > QHLSWGGDFPEEAQPFFSPAFLWTRPQETAVVETQVFAAFKDYLKAYLDFVEQAEAVTDSQNLVAIKQAQ<br>
+ * > LRYLRYRAEKDPARGMFKRFYGAEWTEEYIHGFLFDLERKLTVVK
+ *
+ *
+ */
 struct sequence_file_format_fasta
 {
 public:
     static inline std::vector<std::string> file_extensions {{".fasta"},{".fa"},{".fna"}};
 
-    //TODO make the requirements stricter
+    /*!\name read functions
+     * \{
+     */
+     /*!\brief reads the next FASTA record from a file.
+     * \param[out] seq a container where the fetched sequence will be written to.
+     * \param[out] meta a container where the fetched sequence-id will be written to.
+     * \param[in] stream an input stream to read from.
+     * \param[in] options different options to be applied while reading. E.g. filters on the sequence(-id) etc.
+     *
+     */
     template <typename sequence_type,
               typename meta_type,
               typename stream_type,
               typename options_type>
+    //!\cond
         requires sequence_concept<std::decay_t<sequence_type>> &&
                sequence_concept<std::decay_t<meta_type>>
+    //!\endcond
     void read(sequence_type && seq,
               meta_type && meta,
               stream_type & stream,
-              options_type const & options);
-
-    template <typename seqs_type,
-              typename metas_type,
-              typename stream_type,
-              typename options_type>
-        requires sequence_of_sequence_concept<std::decay_t<seqs_type>> &&
-                 sequence_of_sequence_concept<std::decay_t<metas_type>>
-    void read(seqs_type && seqs,
-              metas_type && metas,
-              stream_type & stream,
-              options_type const & options,
-              size_t max_records = 0);
-
-    template <typename sequence_type,
-              typename meta_type,
-              typename stream_type,
-              typename options_type>
-        requires sequence_concept<std::decay_t<sequence_type>> &&
-                 sequence_concept<std::decay_t<meta_type>>
-    void write(sequence_type && seq,
-              meta_type && meta,
-              stream_type & stream,
-              options_type const & options);
-
-    template <typename seqs_type,
-              typename metas_type,
-              typename stream_type,
-              typename options_type>
-              requires sequence_of_sequence_concept<std::decay_t<seqs_type>> &&
-                       sequence_of_sequence_concept<std::decay_t<metas_type>>
-    void write(seqs_type && seqs,
-               metas_type && metas,
-               stream_type & stream,
-               options_type const & options,
-               size_t max_records = 0);
-};
-
-template <typename sequence_type,
-          typename meta_type,
-          typename stream_type,
-          typename options_type>
-     requires sequence_concept<std::decay_t<sequence_type>> &&
-              sequence_concept<std::decay_t<meta_type>>
-void sequence_file_format_fasta::read(sequence_type && seq,
-                                      meta_type && meta,
-                                      stream_type & stream,
-                                      options_type const & options)
-{
-    using namespace seqan3::detail;
-    auto [stream_begin, stream_end] = make_preferred_input_iterator_range(stream);
-
-    // check if we are at > and consume it
-    assert(*stream_begin == '>');
-    read_one(stream_begin, stream_end, std::ignore);
-
-    // read sequence identifier
-    auto meta_iter = make_preferred_output_iterator(meta);
-    read_line(stream_begin, stream_end, meta_iter);
-
-    // read sequence
-    auto seq_iter = make_preferred_output_iterator(seq);
-    read_until(stream_begin, stream_end, seq_iter, is_whitespace());
-    read_line(stream_begin, stream_end, std::ignore);
-}
-
-template <typename seqs_type,
-          typename metas_type,
-          typename stream_type,
-          typename options_type>
-    requires sequence_of_sequence_concept<std::decay_t<seqs_type>> &&
-             sequence_of_sequence_concept<std::decay_t<metas_type>>
-void sequence_file_format_fasta::read(seqs_type && seqs,
-                                      metas_type && metas,
-                                      stream_type & stream,
-                                      options_type const & options,
-                                      size_t max_records)
-{
-    using namespace seqan3::detail;
-    auto [stream_begin, stream_end] = make_preferred_input_iterator_range(stream);
-
-    if (seqs.size() < max_records)
-        seqs.resize(max_records);
-    if (metas.size() < max_records)
-        metas.resize(max_records);
-
-    for (size_t i = 0; ( i < max_records && stream_begin != stream_end); ++i)
+              options_type const & options)
     {
+        using namespace seqan3::detail;
+        auto [stream_begin, stream_end] = make_preferred_input_iterator_range(stream);
+
         // check if we are at > and consume it
         assert(*stream_begin == '>');
         read_one(stream_begin, stream_end, std::ignore);
 
         // read sequence identifier
-        auto meta_iter = make_preferred_output_iterator(metas[i]);
+        auto meta_iter = make_preferred_output_iterator(meta);
         read_line(stream_begin, stream_end, meta_iter);
 
         // read sequence
-        auto seq_iter = make_preferred_output_iterator(seqs[i]);
+        auto seq_iter = make_preferred_output_iterator(seq);
         read_until(stream_begin, stream_end, seq_iter, is_whitespace());
         read_line(stream_begin, stream_end, std::ignore);
     }
-}
+
+     /*!\brief reads the next 'n' FASTA records from a file.
+     * \param[out] seqs a container of containers where the fetched sequences will be written to.
+     * \param[out] metas a container of containers where the fetched sequence-ids will be written to.
+     * \param[in] stream an input stream to read from.
+     * \param[in] options different options to be applied while reading. E.g. filters on the sequence(-id) etc.
+     * \param[in] max_records the number of records to read.
+     *
+     */
+    template <typename seqs_type,
+              typename metas_type,
+              typename stream_type,
+              typename options_type>
+    //!\cond
+        requires sequence_of_sequence_concept<std::decay_t<seqs_type>> &&
+                 sequence_of_sequence_concept<std::decay_t<metas_type>>
+    //!\endcond
+    void read(seqs_type && seqs,
+              metas_type && metas,
+              stream_type & stream,
+              options_type const & options,
+              size_t max_records = 0)
+    {
+        using namespace seqan3::detail;
+        auto [stream_begin, stream_end] = make_preferred_input_iterator_range(stream);
+
+        if (seqs.size() < max_records)
+            seqs.resize(max_records);
+        if (metas.size() < max_records)
+            metas.resize(max_records);
+
+        for (size_t i = 0; ( i < max_records && stream_begin != stream_end); ++i)
+        {
+            // check if we are at > and consume it
+            assert(*stream_begin == '>');
+            read_one(stream_begin, stream_end, std::ignore);
+
+            // read sequence identifier
+            auto meta_iter = make_preferred_output_iterator(metas[i]);
+            read_line(stream_begin, stream_end, meta_iter);
+
+            // read sequence
+            auto seq_iter = make_preferred_output_iterator(seqs[i]);
+            read_until(stream_begin, stream_end, seq_iter, is_whitespace());
+            read_line(stream_begin, stream_end, std::ignore);
+        }
+    }
+    //!\}
+
+    /*!\name write functions
+     * \{
+     */
+     /*!\brief writes a FASTA record to a file.
+     * \param[in] seq a container with a sequence to be written
+     * \param[in] meta a container with a sequence-id to be written
+     * \param[out] stream an output stream to write to
+     * \param[in] options different options to be applied before writing. E.g. line wrapping of the sequence etc.
+     *
+     */
+    template <typename sequence_type,
+              typename meta_type,
+              typename stream_type,
+              typename options_type>
+    //!\cond
+        requires sequence_concept<std::decay_t<sequence_type>> &&
+                 sequence_concept<std::decay_t<meta_type>>
+    //!\endcond
+    void write(sequence_type && seq,
+              meta_type && meta,
+              stream_type & stream,
+              options_type const & options);
+
+     /*!\brief writes a batch of 'n' FASTA records to a file.
+     * \param[in] seqs a container of containers with sequences to be written
+     * \param[in] metas a container of containers with sequence-ids to be written
+     * \param[out] stream an output stream to write to
+     * \param[in] options different options to be applied before writing. E.g. line wrapping of the sequence etc.
+     * \param[in] max_records the number of records to read.
+     *
+     */
+    template <typename seqs_type,
+              typename metas_type,
+              typename stream_type,
+              typename options_type>
+    //!\cond
+              requires sequence_of_sequence_concept<std::decay_t<seqs_type>> &&
+                       sequence_of_sequence_concept<std::decay_t<metas_type>>
+    //!\endcond
+    void write(seqs_type && seqs,
+               metas_type && metas,
+               stream_type & stream,
+               options_type const & options,
+               size_t max_records = 0);
+    //!\}
+};
+
 } // namespace seqan3
