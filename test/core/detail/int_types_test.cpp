510c8657682762975ef5e735d7b9b3fb98cae777
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2018-02-20 11:18:06 +0100

    [FEATURE] added: min_viable_uint_v

--- test/core/detail/int_types_test.cpp
+++ test/core/detail/int_types_test.cpp
@@ -61,3 +61,39 @@ TEST(int_types_test, min_viable_uint_t)
     EXPECT_TRUE((std::is_same_v<uint64_1_t, uint64_t>));
     EXPECT_TRUE((std::is_same_v<uint64_2_t, uint64_t>));
 }
+
+TEST(int_types_test, min_viable_uint_v)
+{
+    auto bool_1_v = detail::min_viable_uint_v<0ull>;
+    auto bool_2_v = detail::min_viable_uint_v<1ull>;
+    auto uint8_1_v = detail::min_viable_uint_v<2ull>;
+    auto uint8_2_v = detail::min_viable_uint_v<0xFFull>;
+    auto uint16_1_v = detail::min_viable_uint_v<0x100ull>;
+    auto uint16_2_v = detail::min_viable_uint_v<0xFFFFull>;
+    auto uint32_1_v = detail::min_viable_uint_v<0x10000ull>;
+    auto uint32_2_v = detail::min_viable_uint_v<0xFFFFFFFFull>;
+    auto uint64_1_v = detail::min_viable_uint_v<0x100000000ull>;
+    auto uint64_2_v = detail::min_viable_uint_v<0xFFFFFFFFFFFFFFFFull>;
+
+    EXPECT_EQ(static_cast<uint64_t>(bool_1_v), 0ull);
+    EXPECT_EQ(static_cast<uint64_t>(bool_2_v), 1ull);
+    EXPECT_EQ(static_cast<uint64_t>(uint8_1_v), 2ull);
+    EXPECT_EQ(static_cast<uint64_t>(uint8_2_v), 0xFFull);
+    EXPECT_EQ(static_cast<uint64_t>(uint16_1_v), 0x100ull);
+    EXPECT_EQ(static_cast<uint64_t>(uint16_2_v), 0xFFFFull);
+    EXPECT_EQ(static_cast<uint64_t>(uint32_1_v), 0x10000ull);
+    EXPECT_EQ(static_cast<uint64_t>(uint32_2_v), 0xFFFFFFFFull);
+    EXPECT_EQ(static_cast<uint64_t>(uint64_1_v), 0x100000000ull);
+    EXPECT_EQ(static_cast<uint64_t>(uint64_2_v), 0xFFFFFFFFFFFFFFFFull);
+
+    EXPECT_TRUE((std::is_same_v<decltype(bool_1_v), bool>));
+    EXPECT_TRUE((std::is_same_v<decltype(bool_2_v), bool>));
+    EXPECT_TRUE((std::is_same_v<decltype(uint8_1_v), uint8_t>));
+    EXPECT_TRUE((std::is_same_v<decltype(uint8_2_v), uint8_t>));
+    EXPECT_TRUE((std::is_same_v<decltype(uint16_1_v), uint16_t>));
+    EXPECT_TRUE((std::is_same_v<decltype(uint16_2_v), uint16_t>));
+    EXPECT_TRUE((std::is_same_v<decltype(uint32_1_v), uint32_t>));
+    EXPECT_TRUE((std::is_same_v<decltype(uint32_2_v), uint32_t>));
+    EXPECT_TRUE((std::is_same_v<decltype(uint64_1_v), uint64_t>));
+    EXPECT_TRUE((std::is_same_v<decltype(uint64_2_v), uint64_t>));
+}
