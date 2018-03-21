338ffe40a3efac76db2628f6a0c207964bd7945c
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2018-03-21 22:23:08 +0100

    [FEATURE] cmake: Use INTERFACE libraries to expose seqan3 to users
    cmake:
     * add pthread as dependency for gtest and benchmark
     * make gtest and gbenchmark variable names consistent
     * use add_library(... INTERFACE IMPORTED) instead of add_library(... INTERFACE)
     * name test libraries seqan3::test::*
     * rename seqan::seqan3 into seqan3::seqan3
    
    update .travis.yml to reflect target changes

--- test/seqan3-test.cmake
+++ test/seqan3-test.cmake
@@ -48,22 +48,31 @@ include (FindPackageMessage)
 set (SEQAN3_BENCHMARK_CLONE_DIR "${PROJECT_BINARY_DIR}/vendor/benchmark")
 set (SEQAN3_TEST_CLONE_DIR "${PROJECT_BINARY_DIR}/vendor/googletest")
 
-# required flags, includes, definitions and libraries for seqan3
-set (SEQAN3_STRICT_CXX_FLAGS "-pedantic" "-Werror" "-Wall" "-Wextra")
+# seqan3::test exposes a base set of required flags, includes, definitions and
+# libraries qhich are in common for **all** seqan3 tests
+add_library (seqan3::test INTERFACE IMPORTED)
+set_property (TARGET seqan3::test APPEND PROPERTY INTERFACE_COMPILE_OPTIONS "-pedantic"  "-Wall" "-Wextra" "-Werror")
+set_property (TARGET seqan3::test APPEND PROPERTY INTERFACE_LINK_LIBRARIES "seqan3::seqan3" "pthread")
+set_property (TARGET seqan3::test APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${SEQAN3_CLONE_DIR}/test/include/")
 
-# required flags, includes and libraries for seqan3/test/performance
-set (SEQAN3_BENCHMARK_CXX_FLAGS ${SEQAN3_STRICT_CXX_FLAGS})
-set (SEQAN3_BENCHMARK_INCLUDE_DIRS "")
-set (SEQAN3_BENCHMARK_LIBRARIES "")
-list (APPEND SEQAN3_BENCHMARK_INCLUDE_DIRS "${SEQAN3_BENCHMARK_CLONE_DIR}/include/")
-list (APPEND SEQAN3_BENCHMARK_INCLUDE_DIRS "${SEQAN3_CLONE_DIR}/test/include/")
+# seqan3::test::performance specifies required flags, includes and libraries
+# needed for performance test cases in seqan3/test/performance
+add_library (seqan3::test::performance INTERFACE IMPORTED)
+set_property (TARGET seqan3::test::performance APPEND PROPERTY INTERFACE_LINK_LIBRARIES "seqan3::test" "gbenchmark")
+set_property (TARGET seqan3::test::performance APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${SEQAN3_BENCHMARK_CLONE_DIR}/include/")
+file(MAKE_DIRECTORY ${SEQAN3_BENCHMARK_CLONE_DIR}/include/) # see cmake bug https://gitlab.kitware.com/cmake/cmake/issues/15052
 
-# required flags, includes and libraries for seqan3/test/unit
-set (SEQAN3_TEST_CXX_FLAGS ${SEQAN3_STRICT_CXX_FLAGS})
-set (SEQAN3_TEST_INCLUDE_DIRS "")
-set (SEQAN3_TEST_LIBRARIES "")
-list (APPEND SEQAN3_TEST_INCLUDE_DIRS "${SEQAN3_TEST_CLONE_DIR}/googletest/include/")
-list (APPEND SEQAN3_TEST_INCLUDE_DIRS "${SEQAN3_CLONE_DIR}/test/include/")
+# seqan3::test::unit specifies required flags, includes and libraries
+# needed for unit test cases in seqan3/test/unit
+add_library (seqan3::test::unit INTERFACE IMPORTED)
+set_property (TARGET seqan3::test::unit APPEND PROPERTY INTERFACE_LINK_LIBRARIES "seqan3::test" "gtest_main" "gtest")
+set_property (TARGET seqan3::test::unit APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES "${SEQAN3_TEST_CLONE_DIR}/googletest/include/")
+file(MAKE_DIRECTORY ${SEQAN3_TEST_CLONE_DIR}/googletest/include/) # see cmake bug https://gitlab.kitware.com/cmake/cmake/issues/15052
+
+# seqan3::test::header specifies required flags, includes and libraries
+# needed for header test cases in seqan3/test/header
+add_library (seqan3::test::header INTERFACE IMPORTED)
+set_property (TARGET seqan3::test::header APPEND PROPERTY INTERFACE_LINK_LIBRARIES "seqan3::test::unit")
 
 # commonly shared options:
 set (SEQAN3_EXTERNAL_PROJECT_CMAKE_ARGS "")
@@ -97,75 +106,75 @@ endmacro ()
 macro (seqan3_require_benchmark)
     enable_testing ()
 
-    set (google_benchmark_args ${SEQAN3_EXTERNAL_PROJECT_CMAKE_ARGS})
-    list (APPEND google_benchmark_args "-DBENCHMARK_ENABLE_TESTING=false")
-    # list (APPEND google_benchmark_args "-DBENCHMARK_ENABLE_LTO=true")
+    set (gbenchmark_project_args ${SEQAN3_EXTERNAL_PROJECT_CMAKE_ARGS})
+    list (APPEND gbenchmark_project_args "-DBENCHMARK_ENABLE_TESTING=false")
+    # list (APPEND gbenchmark_project_args "-DBENCHMARK_ENABLE_LTO=true")
 
     include (ExternalProject)
     ExternalProject_Add (
-        google_benchmark
-        PREFIX google_benchmark
+        gbenchmark_project
+        PREFIX gbenchmark_project
         GIT_REPOSITORY "https://github.com/google/benchmark.git"
         SOURCE_DIR "${SEQAN3_BENCHMARK_CLONE_DIR}"
-        CMAKE_ARGS "${google_benchmark_args}"
+        CMAKE_ARGS "${gbenchmark_project_args}"
         UPDATE_DISCONNECTED yes
     )
-    unset (google_benchmark_args)
+    unset (gbenchmark_project_args)
 
-    set (benchmark_library "${PROJECT_BINARY_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}benchmark${CMAKE_STATIC_LIBRARY_SUFFIX}")
+    set (gbenchmark_path "${PROJECT_BINARY_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}benchmark${CMAKE_STATIC_LIBRARY_SUFFIX}")
 
-    add_library (benchmark STATIC IMPORTED)
-    add_dependencies (benchmark google_benchmark)
-    set_target_properties (benchmark PROPERTIES IMPORTED_LOCATION "${benchmark_library}")
+    add_library (gbenchmark STATIC IMPORTED)
+    add_dependencies (gbenchmark gbenchmark_project)
+    set_target_properties (gbenchmark PROPERTIES IMPORTED_LOCATION "${gbenchmark_path}")
+    set_property (TARGET gbenchmark APPEND PROPERTY INTERFACE_LINK_LIBRARIES "pthread")
 
-    list (APPEND SEQAN3_BENCHMARK_LIBRARIES "benchmark")
-    list (APPEND SEQAN3_BENCHMARK_LIBRARIES "pthread")
+    unset (gbenchmark_path)
 endmacro ()
 
 macro (seqan3_require_test)
     enable_testing ()
 
-    set (google_test_args ${SEQAN3_EXTERNAL_PROJECT_CMAKE_ARGS})
-    list (APPEND google_test_args "-DBUILD_GTEST=1")
-    list (APPEND google_test_args "-DBUILD_GMOCK=0")
+    set (gtest_project_args ${SEQAN3_EXTERNAL_PROJECT_CMAKE_ARGS})
+    list (APPEND gtest_project_args "-DBUILD_GTEST=1")
+    list (APPEND gtest_project_args "-DBUILD_GMOCK=0")
 
     # force that libraries are installed to `lib/`, because GNUInstallDirs might install it into `lib64/`
-    list (APPEND google_test_args "-DCMAKE_INSTALL_LIBDIR=${PROJECT_BINARY_DIR}/lib/")
+    list (APPEND gtest_project_args "-DCMAKE_INSTALL_LIBDIR=${PROJECT_BINARY_DIR}/lib/")
 
     include (ExternalProject)
     ExternalProject_Add (
-        googletest
-        PREFIX googletest
+        gtest_project
+        PREFIX gtest_project
         GIT_REPOSITORY "https://github.com/google/googletest.git"
         GIT_TAG "15392f1a38fa0b8c3f13a9732e94b209069efa1c"
         SOURCE_DIR "${SEQAN3_TEST_CLONE_DIR}"
-        CMAKE_ARGS "${google_test_args}"
+        CMAKE_ARGS "${gtest_project_args}"
         UPDATE_DISCONNECTED yes
     )
-    unset (google_test_args)
+    unset (gtest_project_args)
 
     # google sets CMAKE_DEBUG_POSTFIX = "d"
-    set (gtest_main_library "${PROJECT_BINARY_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gtest_main${CMAKE_STATIC_LIBRARY_SUFFIX}")
+    set (gtest_main_path "${PROJECT_BINARY_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gtest_main${CMAKE_STATIC_LIBRARY_SUFFIX}")
     if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-        set (gtest_main_library "${PROJECT_BINARY_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gtest_maind${CMAKE_STATIC_LIBRARY_SUFFIX}")
+        set (gtest_main_path "${PROJECT_BINARY_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gtest_maind${CMAKE_STATIC_LIBRARY_SUFFIX}")
     endif ()
 
     add_library (gtest_main STATIC IMPORTED)
-    add_dependencies (gtest_main googletest)
-    set_target_properties (gtest_main PROPERTIES IMPORTED_LOCATION "${gtest_main_library}")
+    add_dependencies (gtest_main gtest_project)
+    set_target_properties (gtest_main PROPERTIES IMPORTED_LOCATION "${gtest_main_path}")
 
-    set (gtest_library "${PROJECT_BINARY_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}")
+    set (gtest_path "${PROJECT_BINARY_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gtest${CMAKE_STATIC_LIBRARY_SUFFIX}")
     if (CMAKE_BUILD_TYPE STREQUAL "Debug")
-        set (gtest_library "${PROJECT_BINARY_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gtestd${CMAKE_STATIC_LIBRARY_SUFFIX}")
+        set (gtest_path "${PROJECT_BINARY_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}gtestd${CMAKE_STATIC_LIBRARY_SUFFIX}")
     endif ()
 
     add_library (gtest STATIC IMPORTED)
     add_dependencies (gtest gtest_main)
-    set_target_properties (gtest PROPERTIES IMPORTED_LOCATION "${gtest_library}")
+    set_target_properties (gtest PROPERTIES IMPORTED_LOCATION "${gtest_path}")
+    set_property (TARGET gtest APPEND PROPERTY INTERFACE_LINK_LIBRARIES "pthread")
 
-    list (APPEND SEQAN3_TEST_LIBRARIES "gtest_main")
-    list (APPEND SEQAN3_TEST_LIBRARIES "gtest")
-    list (APPEND SEQAN3_TEST_LIBRARIES "pthread")
+    unset(gtest_main_path)
+    unset(gtest_path)
 endmacro ()
 
 macro (add_subdirectories)
