025f29c6a1085dc0297a57d76d37cd330df1079f
sarahet <sara_hetzel@yahoo.de>
2018-06-21 15:21:08 +0200

    [DOC] Add explicit explanation for deep view behaviour in case of single input ranges.

--- include/seqan3/range/view/all.hpp
+++ include/seqan3/range/view/all.hpp
@@ -150,7 +150,8 @@
  * **Deep views:** Some views are declared as "deeps views". This means, that in case they are given a range-of-range
  * as input (as opposed to just a range), they will apply their transformation on the innermost range (instead of
  * the outermost range which would be default). This is handy especially for alphabet-based transformations that you
- * wish to apply to a collection of sequences.
+ * wish to apply to a collection of sequences. Note that in case of a single-dimensional range deep views work the
+ * same way as non-deep views.
  *
  * **For all views the following are documented:**
  *
