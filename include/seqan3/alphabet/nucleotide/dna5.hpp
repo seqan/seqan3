838412b7fa6a5c27e37fe486d2f25cbc6c501b1d
Joshua Kim <joshua.kim@fu-berlin.de>
2018-07-05 16:53:24 +0200

    [FIX] Changed all occurrences of _value = char_to_value[c]; where c is a char_type to _value = char_to_value[static_cast<index_t>(c)];, where index_t = std::make_unsigned_t<char_type>
    Prevents indexing negative values.

--- include/seqan3/alphabet/nucleotide/dna5.hpp
+++ include/seqan3/alphabet/nucleotide/dna5.hpp
@@ -124,7 +124,8 @@ struct dna5
     //!\copydoc seqan3::dna4::assign_char
     constexpr dna5 & assign_char(char_type const c) noexcept
     {
-        _value = char_to_value[c];
+        using index_t = std::make_unsigned_t<char_type>;
+        _value = char_to_value[static_cast<index_t>(c)];
         return *this;
     }
 
