b6d2cdb5389d3021b77f771952346d4739cf1bbb
smehringer <svenja.mehringer@gmail.com>
2018-09-18 11:48:41 +0200

    [INTERNAL] Addinig get_or metafunction.

--- include/seqan3/io/detail/record.hpp
+++ include/seqan3/io/detail/record.hpp
@@ -189,6 +189,55 @@ auto const & get_or_ignore(std::tuple<types...> const & t)
 }
 //!\}
 
+
+// ----------------------------------------------------------------------------
+// get_or
+// ----------------------------------------------------------------------------
+
+/*!\addtogroup io
+ *!\{
+ */
+//!\brief Access an element in a std::tuple or seqan3::record; return or_value if not contained.
+template <field f, typename field_types, typename field_ids, typename or_type>
+decltype(auto) get_or(record<field_types, field_ids> & r, or_type && or_value)
+{
+    if constexpr (field_ids::contains(f))
+        return std::get<field_ids::index_of(f)>(r);
+    else
+        return std::forward<or_type>(or_value);
+}
+
+//!\copydoc seqan3::detail::get_or
+template <field f, typename field_types, typename field_ids, typename or_type>
+decltype(auto) const get_or(record<field_types, field_ids> const & r, or_type && or_value)
+{
+    if constexpr (field_ids::contains(f))
+        return std::get<field_ids::index_of(f)>(r);
+    else
+        return std::forward<or_type>(or_value);
+}
+
+//!\copydoc seqan3::detail::get_or
+template <size_t i, typename or_type, typename ... types>
+decltype(auto) get_or(std::tuple<types...> & t, or_type && or_value)
+{
+    if constexpr (i < sizeof...(types))
+        return std::get<i>(t);
+    else
+        return std::forward<or_type>(or_value);
+}
+
+//!\copydoc seqan3::detail::get_or
+template <size_t i, typename or_type, typename ... types>
+decltype(auto) const get_or(std::tuple<types...> const & t, or_type && or_value)
+{
+    if constexpr (i < sizeof...(types))
+        return std::get<i>(t);
+    else
+        return std::forward<or_type>(or_value);
+}
+//!\}
+
 // ----------------------------------------------------------------------------
 // range_wrap_ignore
 // ----------------------------------------------------------------------------
