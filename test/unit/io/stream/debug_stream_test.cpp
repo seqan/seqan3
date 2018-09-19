90381f51a7e46668859a34eb447d3f7bb6bd3b82
Hannes Hauswedell <hannes.hauswedell@fu-berlin.de>
2018-09-19 13:11:19 +0200

    [TEST] add debug_stream tests

---
+++ test/unit/io/stream/debug_stream_test.cpp
@@ -0,0 +1,143 @@
+// ==========================================================================
+//                 SeqAn - The Library for Sequence Analysis
+// ==========================================================================
+//
+// Copyright (c) 2006-2018, Knut Reinert, FU Berlin
+// Copyright (c) 2016-2018, Knut Reinert & MPI Molekulare Genetik
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright
+//       notice, this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright
+//       notice, this list of conditions and the following disclaimer in the
+//       documentation and/or other materials provided with the distribution.
+//     * Neither the name of Knut Reinert or the FU Berlin nor the names of
+//       its contributors may be used to endorse or promote products derived
+//       from this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL KNUT REINERT OR THE FU BERLIN BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+// LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+// OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
+//
+// ==========================================================================
+
+#include <gtest/gtest.h>
+
+#include <iostream>
+#include <string>
+
+#include <seqan3/alphabet/nucleotide/all.hpp>
+#include <seqan3/io/stream/debug_stream.hpp>
+#include <seqan3/range/container/bitcompressed_vector.hpp>
+#include <seqan3/range/container/concatenated_sequences.hpp>
+
+using namespace seqan3;
+using namespace seqan3::literal;
+
+TEST(debug_stream, basic)
+{
+    std::ostringstream o;
+    debug_stream_type my_stream{o};
+
+    my_stream << 'a';
+    o.flush();
+    EXPECT_EQ(o.str(), "a");
+
+    my_stream << "AGA";
+    o.flush();
+    EXPECT_EQ(o.str(), "aAGA");
+
+    my_stream << 42;
+    o.flush();
+    EXPECT_EQ(o.str(), "aAGA42");
+
+    int const i = 7;
+    my_stream << i;
+    o.flush();
+    EXPECT_EQ(o.str(), "aAGA427");
+}
+
+TEST(debug_stream, range)
+{
+    std::ostringstream o;
+    debug_stream_type my_stream{o};
+
+    std::vector<int> vec{1, 4, 5, 7, 32, 321};
+    my_stream << vec;
+    o.flush();
+    EXPECT_EQ(o.str(), "[1,4,5,7,32,321]");
+
+    std::vector<std::vector<int>> const vec2 = {{1, 2, 33}, {22,11}};
+    my_stream << vec2;
+    o.flush();
+    EXPECT_EQ(o.str(), "[1,4,5,7,32,321][[1,2,33],[22,11]]");
+}
+
+TEST(debug_stream, alphabet)
+{
+    std::ostringstream o;
+    debug_stream_type my_stream{o};
+
+    my_stream << dna4::A;
+    o.flush();
+    EXPECT_EQ(o.str(), "A");
+
+    dna5 d = dna5::N;
+    my_stream << d;
+    o.flush();
+    EXPECT_EQ(o.str(), "AN");
+
+    dna5 const d2 = dna5::N;
+    my_stream << d2;
+    o.flush();
+    EXPECT_EQ(o.str(), "ANN");
+}
+
+TEST(debug_stream, range_of_alphabet)
+{
+    std::ostringstream o;
+    debug_stream_type my_stream{o};
+
+    // temporary
+    my_stream << "AGGATAC"_dna5;
+    o.flush();
+    EXPECT_EQ(o.str(), "AGGATAC");
+
+    // lvalue ref
+    auto d = "AGGATAC"_dna5;
+    my_stream << d;
+    o.flush();
+    EXPECT_EQ(o.str(), "AGGATACAGGATAC");
+
+    // lvalue const ref
+    auto const d2 = "AGGATAC"_dna5;
+    my_stream << d2;
+    o.flush();
+    EXPECT_EQ(o.str(), "AGGATACAGGATACAGGATAC");
+
+    concatenated_sequences<bitcompressed_vector<dna5>> const vec2 = {"ACGT"_dna5, "GAGGA"_dna5};
+    my_stream << vec2;
+    o.flush();
+    EXPECT_EQ(o.str(), "AGGATACAGGATACAGGATAC[ACGT,GAGGA]");
+}
+
+TEST(debug_stream, std_endl)
+{
+    std::ostringstream o;
+    debug_stream_type my_stream{o};
+
+    my_stream << "foo" << std::endl << "bar";
+    o.flush();
+    EXPECT_EQ(o.str(), "foo\nbar");
+}
