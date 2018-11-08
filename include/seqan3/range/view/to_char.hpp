5c90a56b0ba1d39a5b5da335718bb99e45d5ee1f
smehringer <svenja.mehringer@gmail.com>
2018-11-08 13:31:24 +0100

    Update include/seqan3/range/view/to_char.hpp
    Co-Authored-By: marehr <marehr@users.noreply.github.com>

--- include/seqan3/range/view/to_char.hpp
+++ include/seqan3/range/view/to_char.hpp
@@ -87,7 +87,7 @@ namespace seqan3::view
  */
 inline auto const to_char = deep{view::transform([] (auto const in)
 {
-    static_assert(alphabet_concept<remove_cvref_t<decltype(in)>>, "value_type of seqan3::view::to_char should be an alphabet.");
+    static_assert(alphabet_concept<remove_cvref_t<decltype(in)>>, "The value type of seqan3::view::to_char must model the seqan3::alphabet_concept.");
     return seqan3::to_char(in);
 })};
 
