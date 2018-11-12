a8c88a5d70364f3c2bfb5310ee9705b69eaecb10
Marcel <marehr@users.noreply.github.com>
2018-11-12 17:40:42 +0100

    [fix] clang: 'decltype(auto)' cannot be combined with other type specifiers (#544)
    * [fix] clang: 'decltype(auto)' cannot be combined with other type specifiers
    
    /seqan3/include/seqan3/io/detail/record.hpp:212:1: error: 'decltype(auto)' cannot be combined with other type specifiers
    decltype(auto) const get_or(record<field_types, field_ids> const & r, or_type && or_value)
    ^
    
    * review: make them decltype(auto) again, drop const

--- include/seqan3/io/detail/record.hpp
+++ include/seqan3/io/detail/record.hpp
@@ -209,7 +209,7 @@ decltype(auto) get_or(record<field_types, field_ids> & r, or_type && or_value)
 
 //!\copydoc seqan3::detail::get_or
 template <field f, typename field_types, typename field_ids, typename or_type>
-decltype(auto) const get_or(record<field_types, field_ids> const & r, or_type && or_value)
+decltype(auto) get_or(record<field_types, field_ids> const & r, or_type && or_value)
 {
     if constexpr (field_ids::contains(f))
         return std::get<field_ids::index_of(f)>(r);
@@ -229,7 +229,7 @@ decltype(auto) get_or(std::tuple<types...> & t, or_type && or_value)
 
 //!\copydoc seqan3::detail::get_or
 template <size_t i, typename or_type, typename ... types>
-decltype(auto) const get_or(std::tuple<types...> const & t, or_type && or_value)
+decltype(auto) get_or(std::tuple<types...> const & t, or_type && or_value)
 {
     if constexpr (i < sizeof...(types))
         return std::get<i>(t);
