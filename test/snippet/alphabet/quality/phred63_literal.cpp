98aea555d7c5049a436fa549bc244e2f145df851
smehringer <svenja.mehringer@gmail.com>
2019-01-08 15:36:25 +0100

    [FEATURE] String literal for phred63.

---
+++ test/snippet/alphabet/quality/phred63_literal.cpp
@@ -0,0 +1,15 @@
+#include <seqan3/alphabet/quality/phred63.hpp>
+#include <seqan3/io/stream/debug_stream.hpp>
+
+using namespace seqan3;
+
+int main()
+{
+    // directly assign to a std::vector<phred63> using a string literal
+    std::vector<phred63> qual_vec = "###!"_phred63;
+
+    // This is the same as a sequence of char literals:
+    std::vector<phred63> qual_vec2 = {'#'_phred63, '#'_phred63, '#'_phred63, '!'_phred63};
+
+    debug_stream << ranges::equal(qual_vec, qual_vec2) << std::endl; // prints 1 (true)
+}
