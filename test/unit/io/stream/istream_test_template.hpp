c262d905f0916c371896acd87a32664cedd4450f
Enrico Seiler <enrico.seiler@hotmail.de>
2019-01-14 14:48:06 +0100

    [MISC] Change copyright year of existing short header

--- test/unit/io/stream/istream_test_template.hpp
+++ test/unit/io/stream/istream_test_template.hpp
@@ -1,7 +1,7 @@
 // -----------------------------------------------------------------------------------------------------
-// Copyright (c) 2006-2018, Knut Reinert & Freie Universität Berlin
-// Copyright (c) 2016-2018, Knut Reinert & MPI für molekulare Genetik
-// This file may be used, modified and/or redistributed under the terms fi the 3-clause BSD-License
+// Copyright (c) 2006-2019, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2019, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
 // shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE
 // -----------------------------------------------------------------------------------------------------
 
