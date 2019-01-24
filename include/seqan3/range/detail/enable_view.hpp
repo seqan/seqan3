4c4c72bfd4815b061c78b28d7f719272884f7c1a
smehringer <svenja.mehringer@gmail.com>
2019-01-24 14:57:00 +0100

    [MISC] std::ranges::enable_view alias.

---
+++ include/seqan3/range/detail/enable_view.hpp
@@ -0,0 +1,24 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2019, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2019, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \brief Adaptations of concepts from the Ranges TS
+ * \author Svenja Mehringer <svenja.mehringer AT fu-berlin.de>
+ */
+
+#pragma once
+
+#include <seqan3/core/platform.hpp>
+
+namespace seqan3::detail
+{
+
+//!\brief Type trait that indicates whether `type` is a view [default: -1 = undefined].
+template <typename type>
+constexpr int enable_view = -1;
+
+} // namespace seqan3::detail
