4e3bc3fb255c097566fecaa0ae96740eaf74b93f
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2019-02-15 13:58:21 +0100

    [feature] display CUPs in alignment benchmarks

--- test/include/seqan3/test/performance/units.hpp
+++ test/include/seqan3/test/performance/units.hpp
@@ -14,7 +14,10 @@
 
 #include <benchmark/benchmark.h>
 
+#include <seqan3/core/concept/tuple.hpp>
+#include <seqan3/core/metafunction/range.hpp>
 #include <seqan3/core/platform.hpp>
+#include <seqan3/std/ranges>
 
 namespace seqan3::test
 {
@@ -29,6 +32,28 @@ inline benchmark::Counter bytes_per_second(size_t bytes)
     return benchmark::Counter(bytes,
                               benchmark::Counter::kIsIterationInvariantRate,
                               benchmark::Counter::OneK::kIs1024);
+}
+
+//!\brief Calculates the number of cell updates for given sequences for a specific alignment config.
+template <typename sequences_range_t>
+inline size_t pairwise_cell_updates(sequences_range_t const & sequences_range, auto && /*align_cfg*/)
+{
+    size_t matrix_cells = 0u;
+    for (auto && [seq1, seq2]: sequences_range)
+        matrix_cells += (std::ranges::size(seq1) + 1) * (std::ranges::size(seq2) + 1);
+    return matrix_cells;
+}
+
+/*!\brief This returns a counter which represents how many cell updates were done for a matrix.
+ *
+ * \param  cells The total number of cells processed of a complete benchmark run.
+ * \return       Returns a benchmark Counter which represents CUPS (cell updates per second).
+ */
+inline benchmark::Counter cell_updates_per_second(size_t cells)
+{
+    return benchmark::Counter(cells,
+                              benchmark::Counter::kIsIterationInvariantRate,
+                              benchmark::Counter::OneK::kIs1000);
 }
 
 } // namespace seqan3::test
---
+++ test/unit/test/seqan2_test.cpp
@@ -0,0 +1,122 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2019, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2019, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE
+// -----------------------------------------------------------------------------------------------------
+
+#include <gtest/gtest.h>
+
+#include <seqan3/core/concept/tuple.hpp>
+#include <seqan3/core/metafunction/iterator.hpp>
+#include <seqan3/core/metafunction/range.hpp>
+#include <seqan3/test/seqan2.hpp>
+#include <seqan3/std/ranges>
+
+// Note: this file will only test regressions encountered with seqan2 compatibility and has no claim to be complete
+
+#ifdef SEQAN3_HAS_SEQAN2
+#include <seqan/sequence.h>
+
+template <typename T>
+class seqan2_container : public ::testing::Test
+{};
+
+using seqan2_container_types = ::testing::Types<seqan::String<int>, seqan::StringSet<int>>;
+TYPED_TEST_CASE(seqan2_container, seqan2_container_types);
+
+template <typename container_t>
+container_t construct_iota(int n)
+{
+    container_t container{};
+
+    for (int i = 0; i < n; ++i)
+        seqan::appendValue(container, i);
+
+    return container;
+}
+
+TYPED_TEST(seqan2_container, std_ranges_size)
+{
+    using container_t = TypeParam;
+    container_t container = construct_iota<container_t>(5u);
+
+    EXPECT_EQ(5u, std::ranges::size(container));
+}
+
+TYPED_TEST(seqan2_container, std_ranges_begin_end)
+{
+    using container_t = TypeParam;
+    container_t container = construct_iota<container_t>(5u);
+
+    auto it = std::ranges::begin(container);
+    auto it_end = std::ranges::end(container);
+
+    EXPECT_TRUE((std::Same<decltype(it), decltype(seqan::begin(container))>));
+    EXPECT_TRUE((std::Same<decltype(it_end), decltype(seqan::end(container))>));
+
+    for (int i = 0; it != it_end; ++it, ++i)
+    {
+        EXPECT_EQ(i, *it);
+    }
+}
+
+TYPED_TEST(seqan2_container, std_ranges_iterator)
+{
+    using container_t = TypeParam;
+    using iterator_t = decltype(std::ranges::begin(std::declval<container_t &>()));
+    using const_iterator_t = decltype(std::ranges::begin(std::declval<container_t const &>()));
+
+    EXPECT_TRUE((std::Same<std::ranges::iterator_t<container_t>, iterator_t>));
+    EXPECT_TRUE((std::Same<std::ranges::iterator_t<container_t &>, iterator_t>));
+    EXPECT_TRUE((std::Same<std::ranges::iterator_t<container_t const>, const_iterator_t>));
+    EXPECT_TRUE((std::Same<std::ranges::iterator_t<container_t const &>, const_iterator_t>));
+}
+
+TYPED_TEST(seqan2_container, std_iterator_traits)
+{
+    using container_t = TypeParam;
+    using iterator_t = std::ranges::iterator_t<container_t>;
+    using value_type = typename std::iterator_traits<iterator_t>::value_type;
+    EXPECT_TRUE((std::Same<value_type, int>));
+}
+
+TYPED_TEST(seqan2_container, std_iterator)
+{
+    using container_t = TypeParam;
+    using iterator_t = std::ranges::iterator_t<container_t>;
+    EXPECT_FALSE(std::Iterator<container_t>);
+    EXPECT_TRUE(std::Iterator<iterator_t>);
+}
+
+template <typename range_t>
+SEQAN3_CONCEPT SeqAn2Range = requires(range_t range)
+{
+    {seqan::begin(range)} -> std::ranges::iterator_t<range_t>;
+    {seqan::end(range)} -> std::ranges::iterator_t<range_t>;
+};
+
+TYPED_TEST(seqan2_container, seqan_range_concept)
+{
+    using container_t = TypeParam;
+    using iterator_t = std::ranges::iterator_t<container_t>;
+    EXPECT_TRUE(SeqAn2Range<container_t>);
+    EXPECT_FALSE(SeqAn2Range<iterator_t>);
+}
+
+TYPED_TEST(seqan2_container, std_ranges_range)
+{
+    using container_t = TypeParam;
+    using iterator_t = std::ranges::iterator_t<container_t>;
+    EXPECT_TRUE(std::ranges::Range<container_t>);
+    EXPECT_FALSE(std::ranges::Range<iterator_t>);
+}
+
+TYPED_TEST(seqan2_container, seqan3_value_type)
+{
+    using container_t = TypeParam;
+    using value_type = seqan3::value_type_t<container_t>;
+    EXPECT_TRUE((std::Same<value_type, int>));
+}
+
+#endif
