14819dcd85734c27fa6f321196f58f8c76fa1eb0
smehringer <svenja.mehringer@gmail.com>
2019-02-20 10:51:25 +0100

    [DOC] Argument Parser Tutorial.

---
+++ doc/tutorial/argument_parser/small_snippets.cpp
@@ -0,0 +1,81 @@
+#include <seqan3/argument_parser/all.hpp>
+#include <seqan3/std/filesystem>
+
+//![validator_include]
+#include <seqan3/argument_parser/validators.hpp>
+//![validator_include]
+
+using namespace seqan3;
+
+struct cmd_arguments
+{
+    std::filesystem::path file_path{};
+    std::vector<uint8_t> seasons{};
+    std::string aggregate_by{"mean"};
+    bool header_is_set{};
+};
+
+cmd_arguments args{};
+
+int main(int argc, char ** argv)
+{
+
+{
+argument_parser parser("Example Parser", argc, argv);
+//![add_positional_option]
+size_t variable{};
+parser.add_positional_option(variable, "This is a description.");
+//![add_positional_option]
+}
+
+{
+argument_parser parser("Example Parser", argc, argv);
+//![add_option]
+size_t variable{};
+parser.add_option(variable, 'n', "my-number", "This is a description.");
+//![add_option]
+}
+
+{
+argument_parser parser("Example Parser", argc, argv);
+//![add_flag]
+bool variable{};
+parser.add_flag(variable, 'f', "my_flag", "This is a description.");
+//![add_flag]
+}
+
+{
+argument_parser parser("Example Parser", argc, argv);
+//![option_list]
+std::vector<std::string> list_variable{};
+parser.add_option(list_variable, 'n', "names", "Give me some names.");
+//![option_list]
+}
+
+{
+argument_parser parser("Example Parser", argc, argv);
+//![positional_option_list]
+std::string variable1{};
+std::vector<std::string> list_variable{};
+parser.add_positional_option(list_variable, "Give me a single variable.");
+parser.add_positional_option(list_variable, "Give me one or more variables!.");
+//![positional_option_list]
+}
+
+{
+argument_parser parser("Example Parser", argc, argv);
+//![required_option]
+std::string required_variable{};
+parser.add_option(required_variable, 'n', "name", "I really need a name.", option_spec::REQUIRED);
+//![required_option]
+}
+
+{
+argument_parser parser("Example Parser", argc, argv);
+//![file_existence_validator]
+parser.add_positional_option(args.file_path, "Please provide a tab separated data file.",
+                             file_existance_validator());
+//![file_existence_validator]
+}
+
+}
--- test/snippet/argument_parser/validators_4.cpp
+++ test/snippet/argument_parser/validators_4.cpp
@@ -6,11 +6,13 @@ int main(int argc, const char ** argv)
 {
     seqan3::argument_parser myparser("Test", argc, argv); // initialize
 
+    //![validator_call]
     std::string my_string;
     seqan3::regex_validator my_validator{"[a-zA-Z]+@[a-zA-Z]+\\.com"};
 
     myparser.add_option(my_string,'s',"str","Give me a string.",
                         seqan3::option_spec::DEFAULT, my_validator);
+    //![validator_call]
 
     // an exception will be thrown if the user specifies a string
     // that is no email address ending on .com
