96c7da2356383b89766ee4d6818f27d239129a73
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2019-02-20 01:46:43 +0100

    [infra] dry cmake tests in header tests

--- test/seqan3-test.cmake
+++ test/seqan3-test.cmake
@@ -95,41 +95,48 @@ list (APPEND SEQAN3_EXTERNAL_PROJECT_CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${PROJEC
 # e.g. target_source_file = "range/view/take.cpp"
 # component:
 #  * TARGET_NAME - the target name (e.g. "take")
+#  * TARGET_UNIQUE_NAME - the target name which includes the target_path (e.g. range-view-take)
 #  * TEST_NAME - the test name which includes the target_path (e.g. "range/view/take")
 #  * TARGET_PATH - the path to the target source (e.g. "range/view")
 macro (seqan3_test_component VAR target_source_file component_name_)
     string (TOUPPER "${component_name_}" component_name)
 
     get_filename_component (target_relative_path "${target_source_file}" DIRECTORY)
-    get_filename_component (target_cpp "${target_source_file}" NAME)
-    string (REGEX REPLACE ".cpp$" "" target_name ${target_cpp})
+    get_filename_component (target_name "${target_source_file}" NAME_WE)
 
     if (component_name STREQUAL "TARGET_NAME")
-        set (${VAR} ${target_name})
-    elseif (component_name STREQUAL "TEST_NAME")
+        set (${VAR} "${target_name}")
+    elseif (component_name MATCHES "TEST_NAME|TARGET_UNIQUE_NAME")
         if (target_relative_path)
             set (${VAR} "${target_relative_path}/${target_name}")
         else ()
             set (${VAR} "${target_name}")
+        endif ()
+
+        if (component_name STREQUAL "TARGET_UNIQUE_NAME")
+            string (REPLACE "/" "-" ${VAR} "${${VAR}}")
         endif ()
     elseif (component_name STREQUAL "TARGET_PATH")
         set (${VAR} "${target_relative_path}")
     endif ()
 
     unset (target_name)
-    unset (target_cpp)
     unset (target_relative_path)
 endmacro ()
 
-macro (seqan3_test_files VAR test_base_path_)
+macro (seqan3_test_files VAR test_base_path_ extension_wildcards)
     # test_base_path is /home/.../seqan3/test/
     get_filename_component(test_base_path "${test_base_path_}" ABSOLUTE)
     file (RELATIVE_PATH test_base_path_relative "${CMAKE_SOURCE_DIR}" "${test_base_path}")
     # ./ is a hack to deal with empty test_base_path_relative
     set (test_base_path_relative "./${test_base_path_relative}")
     # collect all cpp files
-    file (GLOB_RECURSE ${VAR} RELATIVE "${test_base_path}"
-          "${test_base_path_relative}/*.cpp")
+    set (${VAR} "")
+    foreach (extension_wildcard ${extension_wildcards})
+        file (GLOB_RECURSE test_files RELATIVE "${test_base_path}"
+        "${test_base_path_relative}/${extension_wildcard}")
+        list (APPEND ${VAR} ${test_files})
+    endforeach ()
 
     unset (test_base_path)
     unset (test_base_path_relative)
