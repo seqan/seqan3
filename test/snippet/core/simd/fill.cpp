186421c059a9a3e0475d591d6eb536d13dc4392f
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2019-03-10 02:39:42 +0100

    [fix] type names in simd snippets

--- test/snippet/core/simd/fill.cpp
+++ test/snippet/core/simd/fill.cpp
@@ -2,16 +2,16 @@
 
 using namespace seqan3;
 
-using uint8x16_t = simd_type_t<uint16_t, 8>;
+using uint16x8_t = simd_type_t<uint16_t, 8>;
 
 int main()
 {
-    uint8x16_t a = fill<uint8x16_t>(4);
+    uint16x8_t a = fill<uint16x8_t>(4);
     debug_stream << a << "\n"; // [4,4,4,4,4,4,4,4]
 
-    //or:
+    // or:
 
-    uint8x16_t b = simd::fill<uint8x16_t>(4);
+    uint16x8_t b = simd::fill<uint16x8_t>(4);
     debug_stream << b << "\n"; // [4,4,4,4,4,4,4,4]
     return 0;
 }
--- test/snippet/core/simd/simd_test_utility.cpp
+++ test/snippet/core/simd/simd_test_utility.cpp
@@ -5,8 +5,8 @@
 
 TEST(simd, simd_eq)
 {
-    using int16x4_t = seqan3::simd::simd_type_t<int16_t, 8u>;
-    int16x4_t a{4, 3, 2, 1, 0, -1, -2, -3};
+    using int16x8_t = seqan3::simd::simd_type_t<int16_t, 8u>;
+    int16x8_t a{4, 3, 2, 1, 0, -1, -2, -3};
 
-    SIMD_EQ(a, int16x4_t{4, 3, 2, 1, 0, -1, -2, -3});
+    SIMD_EQ(a, int16x8_t{4, 3, 2, 1, 0, -1, -2, -3});
 }
