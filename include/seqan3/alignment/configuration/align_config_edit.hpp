ff03879c6aff0ba55e3742c023cfff3bf6f00c77
rrahn <rene.rahn@fu-berlin.de>
2019-03-13 00:49:58 +0100

    [DOC] Update API documentation for edit distance shortcut.

--- include/seqan3/alignment/configuration/align_config_edit.hpp
+++ include/seqan3/alignment/configuration/align_config_edit.hpp
@@ -22,7 +22,37 @@
 namespace seqan3::align_cfg
 {
 
-//!\brief Shortcut for edit distance configuration.
+/*!\brief Shortcut for edit distance configuration.
+ * \ingroup configuration
+ *
+ * \details
+ *
+ * The edit distance computation is a specific sub-problem of the alignment computation with the aim to count the number
+ * of edits to transform one sequence into another. An edit operation can be a substitution, an insertion, or a
+ * deletion. Accordingly, this algorithm uses a predefined scoring scheme as well as a gap scheme, where the score for
+ * a match is 0, for a mismatch -1, for a gap -1, and for a gap open 0.
+ *
+ * ### Performance
+ *
+ * Under the hood SeqAn uses a [fast bit-vector algorithm](https://dl.acm.org/citation.cfm?id=316550) to compute the
+ * edit distance whenever possible. This depends on the final alignment configuration. Currently, the fast
+ * edit distance algorithm is only triggered for \ref seqan3::align_cfg::global_alignment "global alignments" and
+ * \ref seqan3::align_cfg::end_gaps::seq1_ends_free "semi-global alignments" with free ends in the first sequence.
+ *
+ * \snippet snippet/alignment/configuration/align_cfg_edit_example.cpp semi_global
+ *
+ * The performance of the algorithm can further be improved if the number of maximal errors (edits) is known by using
+ * the align_cfg::max_error configuration.
+ *
+ * \snippet snippet/alignment/configuration/align_cfg_edit_example.cpp max_error
+ *
+ * \todo banded section
+ *
+ * \attention If the edit distance configuration is combined with any other configuration element or setting, the
+ * algorithm falls back to the slower standard pairwise algorithm. For example the `cfg_slow` in the above example will
+ * trigger the slower algorithm which can handle the case if the ends are free in the second sequence instead of the
+ * first sequence.
+ */
 inline constexpr configuration edit = mode{global_alignment} | scoring{nucleotide_scoring_scheme{}} |
                                       gap{gap_scheme{gap_score{-1}}};
 
