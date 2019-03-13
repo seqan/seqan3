82883178ddc369e2984411b6cc3ad200670bd223
smehringer <svenja.mehringer@gmail.com>
2019-03-13 09:59:29 +0100

    [DOC] Sequence IO tutorial

---
+++ doc/tutorial/sequence_file/sequence_file_solution1.cpp
@@ -0,0 +1,53 @@
+#include <fstream>
+
+#include <seqan3/std/filesystem>
+
+struct write_file_dummy_struct
+{
+    write_file_dummy_struct()
+    {
+
+auto file_raw = R"//![fastq_file](
+@seq1
+AGCTAGCAGCGATCG
++
+IIIIIHIIIIIIIII
+@seq2
+CGATCGATC
++
+IIIIIIIII
+@seq3
+AGCGATCGAGGAATATAT
++
+IIIIHHGIIIIHHGIIIH
+)//![fastq_file]";
+
+        std::ofstream file{std::filesystem::temp_directory_path()/"my.fastq"};
+        std::string str{file_raw};
+        file << str.substr(1); // skip first newline
+    }
+};
+
+write_file_dummy_struct go{};
+
+//![solution]
+#include <seqan3/io/sequence_file/all.hpp>
+#include <seqan3/io/stream/debug_stream.hpp>
+#include <seqan3/std/filesystem>
+
+using namespace seqan3;
+
+int main()
+{
+    std::filesystem::path tmp_dir = std::filesystem::temp_directory_path(); // get the temp directory
+
+    sequence_file_input fin{tmp_dir/"my.fastq"};
+
+    for (auto & rec : fin)
+    {
+        debug_stream << "ID:  "  << get<field::ID>(rec) << '\n';
+        debug_stream << "SEQ: "  << get<field::SEQ>(rec) << '\n';
+        debug_stream << "QUAL: " << get<field::QUAL>(rec) << '\n';
+    }
+}
+//![solution]
---
+++ doc/tutorial/sequence_file/sequence_file_solution5.cpp
@@ -0,0 +1,57 @@
+#include <fstream>
+
+#include <seqan3/std/filesystem>
+
+struct write_file_dummy_struct
+{
+    write_file_dummy_struct()
+    {
+
+auto file_raw = R"//![fastq_file](
+@seq1
+CGATCGATC
++
+IIIIII!!!
+@seq2
+AGCGATCGAGGAATATAT
++
+IIIIHHGIII!!!!!!!!
+@seq3
+AGCTAGCAGCGATCG
++
+IIIIIHII!!!!!!!
+)//![fastq_file]";
+
+        std::ofstream file{std::filesystem::temp_directory_path()/"my.fastq"};
+        std::string str{file_raw};
+        file << str.substr(1); // skip first newline
+    }
+};
+
+write_file_dummy_struct go{};
+
+//![solution]
+#include <seqan3/io/sequence_file/all.hpp>
+#include <seqan3/io/stream/debug_stream.hpp>
+#include <seqan3/range/view/take_until.hpp>  // view::take_until
+#include <seqan3/std/filesystem>
+
+using namespace seqan3;
+
+int main()
+{
+    std::filesystem::path tmp_dir = std::filesystem::temp_directory_path();
+
+    sequence_file_input fin{tmp_dir/"my.fastq", fields<field::ID, field::SEQ_QUAL>{}};
+    sequence_file_output fout{tmp_dir/"trimmed.fastq", fields<field::ID, field::SEQ_QUAL>{}};
+
+    auto trimming = view::transform([] (auto & rec)
+    {
+        get<field::SEQ_QUAL>(rec) = (get<field::SEQ_QUAL>(rec)
+                                  | view::take_until([] (auto chr) { return chr.to_phred() <= 10; }));
+        return rec;
+    });
+
+    fout = fin | trimming;
+}
+//![solution]
