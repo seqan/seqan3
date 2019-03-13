101920e8fa5b46173615f5b1a5c1a91b81a1d75d
rrahn <rene.rahn@fu-berlin.de>
2019-03-13 12:52:30 +0100

    [DOC] adds first version of alignment tutorial.

---
+++ doc/tutorial/pairwise_alignment/simple_global_alignment_solution_1.cpp
@@ -0,0 +1,34 @@
+#include <utility>
+#include <vector>
+
+#include <seqan3/alignment/scoring/nucleotide_scoring_scheme.hpp>
+#include <seqan3/alignment/pairwise/align_pairwise.hpp>
+#include <seqan3/alphabet/nucleotide/dna4.hpp>
+#include <seqan3/io/stream/debug_stream.hpp>
+
+using namespace seqan3;
+
+int main()
+{
+    std::vector vec{"ACGTGAACTGACT"_dna4,
+                    "ACGAAGACCGAT"_dna4,
+                    "ACGTGACTGACT"_dna4,
+                    "AGGTACGAGCGACACT"_dna4};
+
+    using pair_t = decltype(std::tie(vec[0], vec[0]));
+    std::vector<pair_t> source;
+    for (unsigned i = 0; i < vec.size(); ++i)
+    {
+        for (unsigned j = i + 1; j < vec.size(); ++j)
+        {
+            source.push_back(std::tie(vec[i], vec[j]));
+        }
+    }
+
+    // Configure the alignment kernel.
+    auto config = align_cfg::mode{align_cfg::global_alignment} |
+                  align_cfg::scoring{nucleotide_scoring_scheme{}};
+
+    for (auto const & res : align_pairwise(source, config))
+        debug_stream << "Score: " << res.get_score() << '\n';
+}
