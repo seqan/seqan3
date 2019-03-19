15995bf9920ab7597ff513857813068baf377dcb
Hannes Hauswedell <h2+github.com@fsfe.org>
2019-03-19 17:30:13 +0100

    [FEATURE] add seqan3::view::slice, including benchmarks and tests

---
+++ test/snippet/range/view/slice.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <seqan3/io/stream/debug_stream.hpp>
+#include <seqan3/range/view/slice.hpp>              // provides view::slice
+#include <seqan3/std/ranges>                        // provides std::view::reverse
+
+using namespace seqan3;
+
+int main()
+{
+    std::string s{"foobar"};
+    auto v = s | view::slice(1,4);
+    debug_stream << v << '\n';      // "oob"
+
+    auto v2 = s | std::view::reverse | view::slice(1, 4);
+    debug_stream << v2 << '\n';     // "abo"
+
+}
