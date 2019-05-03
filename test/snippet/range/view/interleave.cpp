ee1f50153584840e7df95af5d8b3442dbc5ee540
Joshua Kim <joshua.kim@fu-berlin.de>
2019-05-03 21:40:54 +0100

    [Feature] View::interleave (#742)
    * View::interleave implementation.
    
    * Tests for view::interleave
    Add test for ForwardRange.
    
    * [FIX] New and improved pipe adaptor implementation.
    
    * Fix size() function for urange size % step_size == 0.
    
    * Formatting for interleave.hpp

---
+++ test/snippet/range/view/interleave.cpp
@@ -0,0 +1,15 @@
+#include <range/v3/algorithm/equal.hpp>
+
+#include <seqan3/io/stream/debug_stream.hpp>
+#include <seqan3/range/view/interleave.hpp>
+
+int main()
+{
+    std::string u{"FOOBARBAXBAT"};
+    std::string i{"in"};
+    size_t s = 3;
+
+    auto v = u | seqan3::view::interleave(s, i);
+
+    seqan3::debug_stream << v << '\n'; // prints FOOinBARinBAXinBAT
+}
