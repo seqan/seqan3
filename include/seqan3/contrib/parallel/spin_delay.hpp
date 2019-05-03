94f1d2778cbfe4aeef2af311e2ff01563eed0854
rrahn <rene.rahn@fu-berlin.de>
2019-05-03 15:04:05 +0200

    [FEATURE] Ports concurrent queue of SeqAn2.

---
+++ include/seqan3/contrib/parallel/spin_delay.hpp
@@ -0,0 +1,68 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2019, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2019, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \brief Provides seqan3::spin_delay.
+ * \author Rene Rahn <rene.rahn AT fu-berlin.de>
+ */
+
+#pragma once
+
+#include <thread>
+#include <xmmintrin.h>
+
+#include <seqan3/core/platform.hpp>
+
+namespace seqan3
+{
+//!\cond
+class spin_delay
+{
+public:
+    /*!\name Constructors, destructor and assignment
+     * \{
+     */
+    constexpr spin_delay()                               noexcept = default;  //!< Defaulted.
+    constexpr spin_delay(spin_delay const &)             noexcept = default;  //!< Defaulted.
+    constexpr spin_delay(spin_delay &&)                  noexcept = default;  //!< Defaulted.
+    constexpr spin_delay & operator=(spin_delay const &) noexcept = default;  //!< Defaulted.
+    constexpr spin_delay & operator=(spin_delay &&)      noexcept = default;  //!< Defaulted.
+    ~spin_delay()                                        noexcept = default;  //!< Defaulted.
+    //!\}
+
+    void wait()
+    {
+        if (current <= max_repetitions)  // Start active spinning phase
+        {
+            for (int_fast32_t i = 0; i < current; ++i)
+                pause_processor();
+            current <<= 1;  // double the amount of active CPU waiting.
+        }
+        else  // Start passive spinning phase
+        {
+            std::this_thread::yield();
+        }
+    }
+
+private:
+
+    void pause_processor()
+    {
+        #if defined(__SSE2__)  // AMD and Intel
+            _mm_pause();
+        #elif defined(__ia64__)  // IA64
+            __asm__ __volatile__ ("hint @pause");
+        #else  // everything else.
+            asm volatile ("nop" ::: "memory");  // default operation - does nothing => Might lead to passive spinning.
+        #endif
+    }
+
+    static constexpr int_fast32_t max_repetitions{16};
+    int_fast32_t                  current{1};
+};
+//!\endcond
+} // namespace seqan3
---
+++ test/unit/contrib/parallel/concurrent_queue_test.cpp
@@ -0,0 +1,99 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2019, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2019, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+#include <gtest/gtest.h>
+
+#include <atomic>
+#include <numeric>
+#include <thread>
+
+#include <seqan3/contrib/parallel/concurrent_queue.hpp>
+#include <seqan3/std/ranges>
+
+using namespace seqan3;
+
+void test_concurrent_queue(size_t num_producer, size_t num_consumer)
+{
+    constexpr size_t size_v = 10000;
+    concurrent_queue<uint32_t> queue{100};
+
+    std::atomic<uint32_t> cnt{0};
+
+    // Define the producer section
+    auto produce = [&]()
+    {
+        while (true)
+        {
+            // Load atomic
+            uint32_t intermediate = cnt.fetch_add(1);
+
+            if (intermediate >= size_v)
+                return;
+
+            // wait semantics
+            queue_op_status status = queue.wait_push(intermediate);
+            if (status != queue_op_status::success)
+                return;
+        }
+    };
+
+    // Define the consumer section
+    std::atomic<uint32_t> sum{0};
+    auto consume = [&]() mutable
+    {
+        uint32_t i = 0;
+        while (queue.wait_pop(i) != queue_op_status::closed)
+            sum.fetch_add(i, std::memory_order_relaxed);
+    };
+
+    // Create producer pool
+    std::vector<std::thread> producer_pool;
+    for (size_t n = 0; n < num_producer; ++n)
+        producer_pool.emplace_back(produce);
+
+    // Create consumer pool
+    std::vector<std::thread> consumer_pool;
+    for (size_t n = 0; n < num_consumer; ++n)
+        consumer_pool.emplace_back(consume);
+
+    for (auto & t : producer_pool)
+    {
+        if (t.joinable())
+            t.join();
+    }
+    // Notify queue that no more work is going to be added.
+    queue.close();
+
+    for (auto & t : consumer_pool)
+    {
+        if (t.joinable())
+            t.join();
+    }
+
+    auto v = std::view::iota(0, size_v) | std::view::common;
+    EXPECT_EQ(sum.load(), (std::accumulate(v.begin(), v.end(), 0u)));
+}
+
+TEST(buffer_queue, single_producer_single_consumer)
+{
+    test_concurrent_queue(1, 1);
+}
+
+TEST(buffer_queue, single_producer_multiple_consumer)
+{
+    test_concurrent_queue(1, 4);
+}
+
+TEST(buffer_queue, multiple_producer_single_consumer)
+{
+    test_concurrent_queue(4, 1);
+}
+
+TEST(buffer_queue, multiple_producer_multiple_consumer)
+{
+    test_concurrent_queue(4, 4);
+}
