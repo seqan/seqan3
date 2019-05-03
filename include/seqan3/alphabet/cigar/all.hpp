874865dd1603e235cd3210831247b9defbd79542
Joshua Kim <joshua.kim@fu-berlin.de>
2019-05-03 21:58:17 +0100

    [FEATURE] Cigar alphabet (#861)
    * Introduce the cigar operation alphabet.
    
    * fix tabs and documentation.
    
    * Change snippet to use debug_stream and make it CopynPastable. Add gtest into the unit test.

---
+++ include/seqan3/alphabet/cigar/all.hpp
@@ -0,0 +1,43 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2019, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2019, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \author Joshua Kim <joshua.kim AT fu-berlin.de>
+ * \brief Meta-header for the cigar submodule; includes all headers from alphabet/cigar/.
+ */
+
+ #pragma once
+
+ #include <seqan3/alphabet/cigar/cigar_op.hpp>
+
+ /*!\defgroup cigar CIGAR
+  * \brief Contains the CIGAR operation alphabet, along with the CIGAR cartesian composition.
+  * \ingroup alphabet
+  *
+  * \par Introduction
+  *
+  * CIGAR strings are combinations of count values and CIGAR operations, representing an alignment as a sequence of
+  * edit operations. This submodule has two different
+  * alphabets. One is the seqan3::cigar_op alphabet, which is a base seqan3::alphabet implementation. This
+  * contains all valid symbols contained in CIGAR strings. The other alphabet is the seqan3::cigar alphabet, which
+  * is a seqan3::cartesian_composition implementation. This combines the seqan3::cigar_op alphabet with a count value,
+  * such that one can represent an entire CIGAR string with a std::vector of seqan3::cigar values.
+  *
+  * The following table outlines the valid characters in the seqan3::cigar_op alphabet.
+  *
+  * | Letter | Description                                                                                     |
+  * |--------|-------------------------------------------------------------------------------------------------|
+  * | M      | Alignment match (can be a sequence match or mismatch, used only in basic CIGAR representations) |
+  * | I      | Insertion to the reference                                                                      |
+  * | D      | Deletion from the reference                                                                     |
+  * | N      | Skipped region from the reference                                                               |
+  * | S      | Soft clipping (clipped sequences present in SEQ)                                                |
+  * | H      | Hard clipping (clipped sequences NOT present in SEQ)                                            |
+  * | P      | Padding (silent deletion from padded reference)                                                 |
+  * | =      | Sequence match                                                                                  |
+  * | X      | Sequence mismatch                                                                               |
+  */
---
+++ test/snippet/alphabet/cigar/cigar_op.cpp
@@ -0,0 +1,13 @@
+#include <seqan3/alphabet/cigar/cigar_op.hpp>
+#include <seqan3/io/stream/debug_stream.hpp>
+
+using seqan3::operator""_cigar_op;
+
+int main()
+{
+// Initialze an seqan3::cigar_op:
+seqan3::cigar_op match{'M'_cigar_op};
+
+// you can print cigar_op values:
+seqan3::debug_stream << match << std::endl;          // M
+}
