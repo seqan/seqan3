90ed76b945d053a7fc720457179bff8c04f74bbb
rrahn <rene.rahn@fu-berlin.de>
2019-05-24 15:21:39 +0200

    [TEST] Adds test for unpack_hi and unpack_lo.

--- test/unit/core/simd/simd_algorithm_test.cpp
+++ test/unit/core/simd/simd_algorithm_test.cpp
@@ -12,6 +12,15 @@
 #include <seqan3/test/simd_utility.hpp>
 
 #include <iostream>
+template <typename t>
+class simd_algorithm_generic : public ::testing::Test
+{
+public:
+};
+
+using test_types = ::testing::Types<int8_t, uint8_t, int16_t, uint16_t, int32_t, uint32_t, int64_t, uint64_t>;
+
+TYPED_TEST_CASE(simd_algorithm_generic, test_types);
 
 using namespace seqan3;
 
@@ -37,4 +46,64 @@ TEST(simd_algorithm, iota)
 
     constexpr simd_type result = iota<simd_type>(0);
     SIMD_EQ(result, expect);
+}
+
+TYPED_TEST(simd_algorithm_generic, unpack_hi)
+{
+    using simd_t = typename simd_type<TypeParam>::type;
+
+    // Nothing to test if the simd type is just scalar.
+    // TODO: report gcc9.1 fails for intrinsic _mm512_permutex2var_epi8 on mac: g++-9 (Homebrew GCC 9.1.0) 9.1.0
+    if constexpr (simd_traits<simd_t>::length == 1 || simd_traits<simd_t>::length == 64)
+    {
+        return;
+    }
+    else
+    {
+        simd_t lhs = iota<simd_t>(1);
+        simd_t rhs = iota<simd_t>(1);
+
+        simd_t res = unpack_hi(lhs, rhs);
+
+        simd_t cmp{};
+
+        uint32_t j = simd_traits<simd_t>::length / 2;
+        for (uint32_t i = 0; i < simd_traits<simd_t>::length; i += 2, ++j)
+        {
+            cmp[i]     = lhs[j];
+            cmp[i + 1] = rhs[j];
+        }
+
+        SIMD_EQ(res, cmp);
+    }
+}
+
+TYPED_TEST(simd_algorithm_generic, unpack_lo)
+{
+    using simd_t = typename simd_type<TypeParam>::type;
+
+    // Nothing to test if the simd type is just scalar.
+    // TODO: report gcc9.1 fails for intrinsic _mm512_permutex2var_epi8 on mac: g++-9 (Homebrew GCC 9.1.0) 9.1.0
+    if constexpr (simd_traits<simd_t>::length == 1 || simd_traits<simd_t>::length == 64)
+    {
+        return;
+    }
+    else
+    {
+
+        simd_t lhs = iota<simd_t>(1);
+        simd_t rhs = iota<simd_t>(1);
+
+        simd_t res = unpack_lo(lhs, rhs);
+
+        simd_t cmp{};
+
+        for (uint32_t i = 0, j = 0; i < simd_traits<simd_t>::length; i += 2, ++j)
+        {
+            cmp[i]     = lhs[j];
+            cmp[i + 1] = rhs[j];
+        }
+
+        SIMD_EQ(res, cmp);
+    }
 }
