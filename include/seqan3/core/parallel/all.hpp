93752d0ba087afb7803ea9cdc78ccdf006b2ddae
rrahn <rene.rahn@fu-berlin.de>
2019-06-05 00:42:01 +0200

    [FEATURE] Adopting std execution policies.

--- include/seqan3/core/parallel/all.hpp
+++ include/seqan3/core/parallel/all.hpp
@@ -12,12 +12,27 @@
 
 #pragma once
 
-//!\cond DEV
 /*!\defgroup parallel Parallel
- * \brief The parallel module contains concurrent data structures, synchronisation objects and execution policies and
- *        executors for the parallel execution of algorithms.
+ * \brief This module contains types and utilities for concurrent execution of algorithms in SeqAn.
  * \ingroup core
+ *
+ * \details
+ *
+ * ### Execution policies
+ *
+ * Since C++17/20 the standard defines execution policies for sequential, parallel and vectorised execution of
+ * algorithms. These policies are not yet fully supported by the gcc compilers or only with a hard dependency on
+ * external libraries. Thus, we define our own execution policies which adopt the behaviour of the policies defined
+ * in the standard library. Once they are fully supported by the compilers our policies will merely alias the standard
+ * policies.
+ *
+ * \if DEV
+ * ### Concurrency support
+ *
+ * This module contains helper classes to synchronise threads in concurrent environments.
+ *
+ * \endif
  */
-//!\endcond
 
+#include <seqan3/core/parallel/execution.hpp>
 #include <seqan3/core/parallel/detail/all.hpp>
