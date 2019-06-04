bef5da51efe8fc87a4f8875a50ba0738f4ff9ad5
rrahn <rene.rahn@fu-berlin.de>
2019-06-05 00:47:29 +0200

    [TEST] Refactor global affine tests to use test template.

--- test/unit/alignment/pairwise/global_affine_banded_test.cpp
+++ test/unit/alignment/pairwise/global_affine_banded_test.cpp
@@ -7,116 +7,15 @@
 
 #include <gtest/gtest.h>
 
-
-#include <seqan3/alignment/matrix/alignment_score_matrix.hpp>
-#include <seqan3/alignment/matrix/alignment_trace_matrix.hpp>
 #include <seqan3/alignment/pairwise/align_pairwise.hpp>
 
-#include <seqan3/range/view/to_char.hpp>
-
 #include "fixture/global_affine_banded.hpp"
-#include "fixture/semi_global_affine_banded.hpp"
+#include "pairwise_alignment_single_test_template.hpp"
 
-using namespace seqan3;
-using namespace seqan3::detail;
-using namespace seqan3::test::alignment::fixture;
-
-template <auto _fixture>
-struct param : public ::testing::Test
-{
-    auto fixture() -> decltype(alignment_fixture{*_fixture}) const &
-    {
-        return *_fixture;
-    }
-};
-
-template <typename param_t>
-class global_affine_banded : public param_t
-{};
-
-TYPED_TEST_CASE_P(global_affine_banded);
-
-using global_affine_banded_types
-    = ::testing::Types<
-        param<&global::affine::banded::dna4_01>
+using pairwise_global_affine_unbanded_testing_types = ::testing::Types<
+        pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::banded::dna4_01>
     >;
 
-using semi_global_affine_banded_types
-    = ::testing::Types<
-        param<&semi_global::affine::banded::dna4_semi_seq1_a>,
-        //param<&semi_global::affine::banded::dna4_semi_seq1_b>,
-        param<&semi_global::affine::banded::dna4_semi_seq2_a>,
-        param<&semi_global::affine::banded::dna4_semi_seq2_b>
-    >;
-
-TYPED_TEST_P(global_affine_banded, score)
-{
-    auto const & fixture = this->fixture();
-    // We only compute the score.
-    auto align_cfg = fixture.config | align_cfg::result{with_score};
-
-    std::vector database = fixture.sequence1;
-    std::vector query = fixture.sequence2;
-
-    // TODO Make test work with ranges.
-    auto alignment = align_pairwise(std::tie(database, query), align_cfg);
-
-    EXPECT_EQ((*std::ranges::begin(alignment)).score(), fixture.score);
-}
-
-TYPED_TEST_P(global_affine_banded, end_position)
-{
-    auto const & fixture = this->fixture();
-    auto align_cfg = fixture.config | align_cfg::result{with_back_coordinate};
-
-    std::vector database = fixture.sequence1;
-    std::vector query = fixture.sequence2;
-
-    auto alignment = align_pairwise(std::tie(database, query), align_cfg);
-
-    auto res = *std::ranges::begin(alignment);
-    EXPECT_EQ(res.score(), fixture.score);
-
-    EXPECT_EQ(res.back_coordinate(), fixture.back_coordinate);
-}
-
-TYPED_TEST_P(global_affine_banded, begin_position)
-{
-    auto const & fixture = this->fixture();
-    auto align_cfg = fixture.config | align_cfg::result{with_front_coordinate};
-
-    std::vector database = fixture.sequence1;
-    std::vector query = fixture.sequence2;
-
-    auto alignment = align_pairwise(std::tie(database, query), align_cfg);
-
-    auto res = *std::ranges::begin(alignment);
-    EXPECT_EQ(res.score(), fixture.score);
-
-    EXPECT_EQ(res.front_coordinate(), fixture.front_coordinate);
-    EXPECT_EQ(res.back_coordinate(), fixture.back_coordinate);
-}
-
-TYPED_TEST_P(global_affine_banded, trace)
-{
-    auto const & fixture = this->fixture();
-    auto align_cfg = fixture.config | align_cfg::result{with_alignment};
-
-    std::vector database = fixture.sequence1;
-    std::vector query = fixture.sequence2;
-
-    auto alignment = align_pairwise(std::tie(database, query), align_cfg);
-
-    auto res = *std::ranges::begin(alignment);
-    EXPECT_EQ(res.score(), fixture.score);
-    EXPECT_EQ(res.front_coordinate(), fixture.front_coordinate);
-    EXPECT_EQ(res.back_coordinate(), fixture.back_coordinate);
-    EXPECT_TRUE(ranges::equal(get<0>(res.alignment()) | view::to_char, fixture.aligned_sequence1));
-    EXPECT_TRUE(ranges::equal(get<1>(res.alignment()) | view::to_char, fixture.aligned_sequence2));
-}
-
-REGISTER_TYPED_TEST_CASE_P(global_affine_banded, score, end_position, begin_position, trace);
-
-// work around a bug that you can't specify more than 50 template arguments to ::testing::types
-INSTANTIATE_TYPED_TEST_CASE_P(global, global_affine_banded, global_affine_banded_types);
-INSTANTIATE_TYPED_TEST_CASE_P(semi_global, global_affine_banded, semi_global_affine_banded_types);
+INSTANTIATE_TYPED_TEST_CASE_P(pairwise_global_affine_unbanded,
+                              pairwise_alignment_test,
+                              pairwise_global_affine_unbanded_testing_types);
--- test/unit/alignment/pairwise/global_affine_unbanded_test.cpp
+++ test/unit/alignment/pairwise/global_affine_unbanded_test.cpp
@@ -7,112 +7,16 @@
 
 #include <gtest/gtest.h>
 
-
-#include <seqan3/alignment/matrix/alignment_score_matrix.hpp>
-#include <seqan3/alignment/matrix/alignment_trace_matrix.hpp>
 #include <seqan3/alignment/pairwise/align_pairwise.hpp>
 
-#include <seqan3/range/view/to_char.hpp>
-
 #include "fixture/global_affine_unbanded.hpp"
-#include "fixture/semi_global_affine_unbanded.hpp"
+#include "pairwise_alignment_single_test_template.hpp"
 
-using namespace seqan3;
-using namespace seqan3::detail;
-using namespace seqan3::test::alignment::fixture;
-
-template <auto _fixture>
-struct param : public ::testing::Test
-{
-    auto fixture() -> decltype(alignment_fixture{*_fixture}) const &
-    {
-        return *_fixture;
-    }
-};
-
-template <typename param_t>
-class global_affine_unbanded : public param_t
-{};
-
-TYPED_TEST_CASE_P(global_affine_unbanded);
-
-using global_affine_unbanded_types
-    = ::testing::Types<
-        param<&global::affine::unbanded::dna4_01>,
-        param<&global::affine::unbanded::dna4_02>
+using pairwise_global_affine_unbanded_testing_types = ::testing::Types<
+        pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::unbanded::dna4_01>,
+        pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::unbanded::dna4_02>
     >;
 
-using semi_global_affine_unbanded_types
-    = ::testing::Types<
-        param<&semi_global::affine::unbanded::dna4_01>,
-        param<&semi_global::affine::unbanded::dna4_02>,
-        param<&semi_global::affine::unbanded::dna4_03>,
-        param<&semi_global::affine::unbanded::dna4_04>
-    >;
-
-TYPED_TEST_P(global_affine_unbanded, score)
-{
-    auto const & fixture = this->fixture();
-    auto align_cfg = fixture.config | align_cfg::result{with_score};
-
-    std::vector database = fixture.sequence1;
-    std::vector query = fixture.sequence2;
-
-    auto alignment = align_pairwise(std::tie(database, query), align_cfg);
-
-    EXPECT_EQ((*std::ranges::begin(alignment)).score(), fixture.score);
-}
-
-TYPED_TEST_P(global_affine_unbanded, end_position)
-{
-    auto const & fixture = this->fixture();
-    auto align_cfg = fixture.config | align_cfg::result{with_back_coordinate};
-
-    std::vector database = fixture.sequence1;
-    std::vector query = fixture.sequence2;
-
-    auto alignment = align_pairwise(std::tie(database, query), align_cfg);
-
-    auto res = *std::ranges::begin(alignment);
-    EXPECT_EQ(res.score(), fixture.score);
-    EXPECT_EQ(res.back_coordinate(), fixture.back_coordinate);
-}
-
-TYPED_TEST_P(global_affine_unbanded, begin_position)
-{
-    auto const & fixture = this->fixture();
-    auto align_cfg = fixture.config | align_cfg::result{with_front_coordinate};
-
-    std::vector database = fixture.sequence1;
-    std::vector query = fixture.sequence2;
-
-    auto alignment = align_pairwise(std::tie(database, query), align_cfg);
-
-    auto res = *std::ranges::begin(alignment);
-    EXPECT_EQ(res.score(), fixture.score);
-    EXPECT_EQ(res.front_coordinate(), fixture.front_coordinate);
-    EXPECT_EQ(res.back_coordinate(), fixture.back_coordinate);
-}
-
-TYPED_TEST_P(global_affine_unbanded, trace)
-{
-    auto const & fixture = this->fixture();
-    auto align_cfg = fixture.config | align_cfg::result{with_alignment};
-
-    std::vector database = fixture.sequence1;
-    std::vector query = fixture.sequence2;
-
-    auto alignment = align_pairwise(std::tie(database, query), align_cfg);
-
-    auto res = *std::ranges::begin(alignment);
-    EXPECT_EQ(res.score(), fixture.score);
-    EXPECT_EQ(res.front_coordinate(), fixture.front_coordinate);
-    EXPECT_EQ(res.back_coordinate(), fixture.back_coordinate);
-    EXPECT_TRUE(ranges::equal(get<0>(res.alignment()) | view::to_char, fixture.aligned_sequence1));
-    EXPECT_TRUE(ranges::equal(get<1>(res.alignment()) | view::to_char, fixture.aligned_sequence2));
-}
-
-REGISTER_TYPED_TEST_CASE_P(global_affine_unbanded, score, end_position, begin_position, trace);
-
-INSTANTIATE_TYPED_TEST_CASE_P(global, global_affine_unbanded, global_affine_unbanded_types);
-INSTANTIATE_TYPED_TEST_CASE_P(semi_global, global_affine_unbanded, semi_global_affine_unbanded_types);
+INSTANTIATE_TYPED_TEST_CASE_P(pairwise_global_affine_unbanded,
+                              pairwise_alignment_test,
+                              pairwise_global_affine_unbanded_testing_types);
