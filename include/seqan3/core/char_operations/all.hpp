748631fe700fa9037f0f54799d6098ea7778ed39
Hannes Hauswedell <h2+github.com@fsfe.org>
2019-06-06 11:49:15 +0200

    [DOC] files forgotten for char_operations

---
+++ include/seqan3/core/char_operations/all.hpp
@@ -0,0 +1,23 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2019, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2019, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \brief Provides Character predicates for tokenization.
+ * \author Rene Rahn <rene.rahn AT fu-berlin.de>
+ * \author Hannes Hauswedell <hannes.hauswedell AT fu-berlin.de>
+ */
+
+#pragma once
+
+#include <seqan3/core/char_operations/predicate.hpp>
+#include <seqan3/core/char_operations/pretty_print.hpp>
+#include <seqan3/core/char_operations/transform.hpp>
+
+/*!\defgroup char_operations Char operations
+ * \brief Provides various operations on character types.
+ * \ingroup core
+ */
