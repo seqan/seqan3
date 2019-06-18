04f3044b3593d816186e6a21abce624994e56cf6
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2019-06-18 11:01:26 +0200

    [MISC #1117] deprecate translate_triplet overloads

--- test/unit/alphabet/aminoacid/aminoacid_translation_test.cpp
+++ test/unit/alphabet/aminoacid/aminoacid_translation_test.cpp
@@ -12,31 +12,62 @@
 #include <range/v3/view/concat.hpp>
 #include <range/v3/view/single.hpp>
 
-#include <seqan3/alphabet/aminoacid/all.hpp>
+#include <seqan3/alphabet/nucleotide/dna4.hpp>
+#include <seqan3/alphabet/nucleotide/dna15.hpp>
+#include <seqan3/alphabet/aminoacid/aa27.hpp>
+#include <seqan3/alphabet/aminoacid/translation.hpp>
 
 using namespace seqan3;
 
-TEST(translation, translate_triplets)
+TEST(translate_triplets, dna4)
+{
+    dna4 n1{'C'_dna4};
+    dna4 n2{'T'_dna4};
+    dna4 n3{'A'_dna4};
+    aa27 c{'L'_aa27};
+
+    aa27 t1{translate_triplet<genetic_code::CANONICAL>(n1, n2, n3)};
+
+    EXPECT_EQ(t1, c);
+}
+
+TEST(translate_triplets, dna15)
 {
     dna15 n1{'C'_dna15};
     dna15 n2{'T'_dna15};
     dna15 n3{'A'_dna15};
     aa27 c{'L'_aa27};
 
-    // Nucleotide interface
     aa27 t1{translate_triplet<genetic_code::CANONICAL, dna15>(n1, n2, n3)};
 
     EXPECT_EQ(t1, c);
+}
 
-    // Range interface
+TEST(translate_triplets, RandomAccessRange)
+{
+    dna15 n1{'C'_dna15};
+    dna15 n2{'T'_dna15};
+    dna15 n3{'A'_dna15};
+    aa27 c{'L'_aa27};
+
     auto range_triplet = ranges::view::concat(ranges::view::single(n1), ranges::view::single(n2),
                                               ranges::view::single(n3));
+    #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
     aa27 t2{translate_triplet(range_triplet)};
 
+    EXPECT_TRUE(std::ranges::RandomAccessRange<decltype(range_triplet)>);
     EXPECT_EQ(t2, c);
+}
 
-    // Tuple interface
+TEST(translate_triplets, tuple)
+{
+    dna15 n1{'C'_dna15};
+    dna15 n2{'T'_dna15};
+    dna15 n3{'A'_dna15};
+    aa27 c{'L'_aa27};
+
     std::tuple tuple_triplet{n1, n2, n3};
+    #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
     aa27 t3{translate_triplet(tuple_triplet)};
 
     EXPECT_EQ(t3, c);
