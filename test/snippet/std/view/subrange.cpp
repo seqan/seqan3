9bc373e3d5c4012a55461cbd5a99f250d642118e
Josh Kim <33100114+joshuak94@users.noreply.github.com>
2019-06-24 15:55:00 +0100

    [TEST] Make std/view snippets copy&pastable.

--- test/snippet/std/view/subrange.cpp
+++ test/snippet/std/view/subrange.cpp
@@ -3,15 +3,13 @@
 #include <seqan3/range/view/to_char.hpp>
 #include <seqan3/std/ranges>
 
-using namespace seqan3;
-
 int main()
 {
-//! [example]
-dna4_vector s{"ACTTTGATAA"_dna4};
-using iterator = dna4_vector::iterator;
-auto v1 = std::ranges::subrange<iterator, iterator>{begin(s) + 2, end(s)} | view::to_char; // == "TTTGATAA"
-//! [example]
+    using seqan3::operator""_dna4;
+    seqan3::dna4_vector s{"ACTTTGATAA"_dna4};
+    using iterator = seqan3::dna4_vector::iterator;
+    auto v1 = std::ranges::subrange<iterator, iterator>{std::ranges::begin(s) + 2, std::ranges::end(s)}
+            | seqan3::view::to_char; // == "TTTGATAA"
 
-debug_stream << v1 << '\n';
+    seqan3::debug_stream << v1 << '\n';
 }
