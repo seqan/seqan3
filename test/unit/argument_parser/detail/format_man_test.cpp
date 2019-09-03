b4c03b16c81fd148f49f3a65ed150b13b69d21bf
Joshua Kim <joshua.kim@fu-berlin.de>
2019-09-03 12:17:55 +0200

    [TEST] Split a test into multiple, reduce redundancies

--- test/unit/argument_parser/detail/format_man_test.cpp
+++ test/unit/argument_parser/detail/format_man_test.cpp
@@ -15,65 +15,75 @@
 
 using namespace seqan3;
 
-TEST(format_man, empty_information)
+// Reused global variables
+int option_value{5};
+bool flag_value{};
+int8_t non_list_pos_opt_value{1};
+std::vector<std::string> list_pos_opt_value{};
+const char * argv[] = {"./format_man_test --version-check 0", "--export-help", "man"};
+std::string my_stdout{};
+std::string expected{".TH DEFAULT 1 \"December 01, 1994\" \"default 01.01.01\" \"default_man_page_title\"\n"
+                     ".SH NAME\n"
+                     "default \\- A short description here.\n"
+                     ".SH SYNOPSIS\n"
+                     "\\fB./format_man_test\\fP synopsis\n"
+                     ".br\n"
+                     "\\fB./format_man_test\\fP synopsis2\n"
+                     ".SH DESCRIPTION\n"
+                     "description\n"
+                     ".sp\n"
+                     "description2\n"
+                     ".SH POSITIONAL ARGUMENTS\n"
+                     ".TP\n"
+                     "\\fBARGUMENT-1\\fP (\\fIsigned 8 bit integer\\fP)\n"
+                     "this is a positional option. \n"
+                     ".TP\n"
+                     "\\fBARGUMENT-2\\fP (\\fIList\\fP of \\fIstd::string\\fP's)\n"
+                     "this is a positional option. Default: []. \n"
+                     ".SH OPTIONS\n"
+                     ".SS Basic options:\n"
+                     ".TP\n"
+                     "\\fB-h\\fP, \\fB--help\\fP\n"
+                     "Prints the help page.\n"
+                     ".TP\n"
+                     "\\fB-hh\\fP, \\fB--advanced-help\\fP\n"
+                     "Prints the help page including advanced options.\n"
+                     ".TP\n"
+                     "\\fB--version\\fP\n"
+                     "Prints the version information.\n"
+                     ".TP\n"
+                     "\\fB--copyright\\fP\n"
+                     "Prints the copyright/license information.\n"
+                     ".TP\n"
+                     "\\fB--export-help\\fP (std::string)\n"
+                     "Export the help page information. Value must be one of [html, man].\n"
+                     ".TP\n"
+                     "\\fB--version-check\\fP (bool)\n"
+                     "Whether to to check for the newest app version. Default: 1.\n"
+                     ".SS \n"
+                     ".TP\n"
+                     "\\fB-i\\fP, \\fB--int\\fP (\\fIsigned 32 bit integer\\fP)\n"
+                     "this is a int option. Default: 5. \n"
+                     ".TP\n"
+                     "\\fB-j\\fP, \\fB--jint\\fP (\\fIsigned 32 bit integer\\fP)\n"
+                     "this is a required int option. \n"
+                     ".SH FLAGS\n"
+                     ".SS SubFlags\n"
+                     "here come all the flags\n"
+                     ".TP\n"
+                     "\\fB-f\\fP, \\fB--flag\\fP\n"
+                     "this is a flag.\n"
+                     ".TP\n"
+                     "\\fB-k\\fP, \\fB--kflag\\fP\n"
+                     "this is a flag.\n"
+                     ".SH EXAMPLES\n"
+                     "example\n"
+                     ".sp\n"
+                     "example2\n"};
+
+// Full info parser initialisation
+void dummy_init(argument_parser & parser)
 {
-    std::string my_stdout{};
-    std::string expected{};
-    // Create the dummy parser.
-    const char * argv[] = {"./format_man_test --version-check 0", "--export-help", "man"};
-    argument_parser parser{"default", 3, argv};
-    parser.info.date = "December 01, 1994";
-    parser.info.version = "01.01.01";
-    parser.info.man_page_title = "default_man_page_title";
-    parser.info.short_description = "A short description here.";
-
-    expected = std::string{".TH DEFAULT 1 \"December 01, 1994\" \"default 01.01.01\" \"default_man_page_title\"\n"
-                           ".SH NAME\n"
-                           "default \\- A short description here.\n"
-                           ".SH OPTIONS\n"
-                           ".SS Basic options:\n"
-                           ".TP\n"
-                           "\\fB-h\\fP, \\fB--help\\fP\n"
-                           "Prints the help page.\n"
-                           ".TP\n"
-                           "\\fB-hh\\fP, \\fB--advanced-help\\fP\n"
-                           "Prints the help page including advanced options.\n"
-                           ".TP\n"
-                           "\\fB--version\\fP\n"
-                           "Prints the version information.\n"
-                           ".TP\n"
-                           "\\fB--copyright\\fP\n"
-                           "Prints the copyright/license information.\n"
-                           ".TP\n"
-                           "\\fB--export-help\\fP (std::string)\n"
-                           "Export the help page information. Value must be one of [html, man].\n"
-                           ".TP\n"
-                           "\\fB--version-check\\fP (bool)\n"
-                           "Whether to to check for the newest app version. Default: 1.\n"
-                           ".SS \n"};
-
-    // Test the dummy parser with minimal information.
-    testing::internal::CaptureStdout();
-    EXPECT_EXIT(parser.parse(), ::testing::ExitedWithCode(EXIT_SUCCESS), "");
-
-    my_stdout = testing::internal::GetCapturedStdout();
-    EXPECT_EQ(my_stdout, expected);
-}
-
-TEST(format_man, full_information)
-{
-    std::string my_stdout{};
-    std::string expected{};
-    int option_value{5};
-    bool flag_value{};
-    int8_t non_list_pos_opt_value{1};
-    std::vector<std::string> list_pos_opt_value{};
-
-    // Create the dummy parser.
-    const char * argv[] = {"./format_man_test --version-check 0", "--export-help", "man"};
-    argument_parser parser{"default", 3, argv};
-
-    // Fill out the dummy parser with options and flags and sections and subsections.
     parser.info.date = "December 01, 1994";
     parser.info.version = "01.01.01";
     parser.info.man_page_title = "default_man_page_title";
@@ -93,105 +103,120 @@ TEST(format_man, full_information)
     parser.add_positional_option(list_pos_opt_value, "this is a positional option.");
     parser.info.examples.push_back("example");
     parser.info.examples.push_back("example2");
-    expected = std::string{".TH DEFAULT 1 \"December 01, 1994\" \"default 01.01.01\" \"default_man_page_title\"\n"
-                           ".SH NAME\n"
-                           "default \\- A short description here.\n"
-                           ".SH SYNOPSIS\n"
-                           "\\fB./format_man_test\\fP synopsis\n"
-                           ".br\n"
-                           "\\fB./format_man_test\\fP synopsis2\n"
-                           ".SH DESCRIPTION\n"
-                           "description\n"
-                           ".sp\n"
-                           "description2\n"
-                           ".SH POSITIONAL ARGUMENTS\n"
-                           ".TP\n"
-                           "\\fBARGUMENT-1\\fP (\\fIsigned 8 bit integer\\fP)\n"
-                           "this is a positional option. \n"
-                           ".TP\n"
-                           "\\fBARGUMENT-2\\fP (\\fIList\\fP of \\fIstd::string\\fP's)\n"
-                           "this is a positional option. Default: []. \n"
-                           ".SH OPTIONS\n"
-                           ".SS Basic options:\n"
-                           ".TP\n"
-                           "\\fB-h\\fP, \\fB--help\\fP\n"
-                           "Prints the help page.\n"
-                           ".TP\n"
-                           "\\fB-hh\\fP, \\fB--advanced-help\\fP\n"
-                           "Prints the help page including advanced options.\n"
-                           ".TP\n"
-                           "\\fB--version\\fP\n"
-                           "Prints the version information.\n"
-                           ".TP\n"
-                           "\\fB--copyright\\fP\n"
-                           "Prints the copyright/license information.\n"
-                           ".TP\n"
-                           "\\fB--export-help\\fP (std::string)\n"
-                           "Export the help page information. Value must be one of [html, man].\n"
-                           ".TP\n"
-                           "\\fB--version-check\\fP (bool)\n"
-                           "Whether to to check for the newest app version. Default: 1.\n"
-                           ".SS \n"
-                           ".TP\n"
-                           "\\fB-i\\fP, \\fB--int\\fP (\\fIsigned 32 bit integer\\fP)\n"
-                           "this is a int option. Default: 5. \n"
-                           ".TP\n"
-                           "\\fB-j\\fP, \\fB--jint\\fP (\\fIsigned 32 bit integer\\fP)\n"
-                           "this is a required int option. \n"
-                           ".SH FLAGS\n"
-                           ".SS SubFlags\n"
-                           "here come all the flags\n"
-                           ".TP\n"
-                           "\\fB-f\\fP, \\fB--flag\\fP\n"
-                           "this is a flag.\n"
-                           ".TP\n"
-                           "\\fB-k\\fP, \\fB--kflag\\fP\n"
-                           "this is a flag.\n"
-                           ".SH EXAMPLES\n"
-                           "example\n"
-                           ".sp\n"
-                           "example2\n"};
-    {
-        // Test the dummy parser without any copyright or citations.
-        testing::internal::CaptureStdout();
-        EXPECT_EXIT(parser.parse(), ::testing::ExitedWithCode(EXIT_SUCCESS), "");
+}
 
-        my_stdout = testing::internal::GetCapturedStdout();
-        EXPECT_EQ(my_stdout, expected);
-    }
+TEST(format_man, empty_information)
+{
+    // Create the dummy parser.
+    argument_parser parser{"default", 3, argv};
+    parser.info.date = "December 01, 1994";
+    parser.info.version = "01.01.01";
+    parser.info.man_page_title = "default_man_page_title";
+    parser.info.short_description = "A short description here.";
+
+    std::string expected_short{".TH DEFAULT 1 \"December 01, 1994\" \"default 01.01.01\" \"default_man_page_title\"\n"
+                               ".SH NAME\n"
+                               "default \\- A short description here.\n"
+                               ".SH OPTIONS\n"
+                               ".SS Basic options:\n"
+                               ".TP\n"
+                               "\\fB-h\\fP, \\fB--help\\fP\n"
+                               "Prints the help page.\n"
+                               ".TP\n"
+                               "\\fB-hh\\fP, \\fB--advanced-help\\fP\n"
+                               "Prints the help page including advanced options.\n"
+                               ".TP\n"
+                               "\\fB--version\\fP\n"
+                               "Prints the version information.\n"
+                               ".TP\n"
+                               "\\fB--copyright\\fP\n"
+                               "Prints the copyright/license information.\n"
+                               ".TP\n"
+                               "\\fB--export-help\\fP (std::string)\n"
+                               "Export the help page information. Value must be one of [html, man].\n"
+                               ".TP\n"
+                               "\\fB--version-check\\fP (bool)\n"
+                               "Whether to to check for the newest app version. Default: 1.\n"
+                               ".SS \n"};
+
+    // Test the dummy parser with minimal information.
+    testing::internal::CaptureStdout();
+    EXPECT_EXIT(parser.parse(), ::testing::ExitedWithCode(EXIT_SUCCESS), "");
+
+    my_stdout = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(my_stdout, expected_short);
+}
+
+TEST(format_man, full_information)
+{
+    // Create the dummy parser.
+    argument_parser parser{"default", 3, argv};
+
+    // Fill out the dummy parser with options and flags and sections and subsections.
+    dummy_init(parser);
+    // Test the dummy parser without any copyright or citations.
+    testing::internal::CaptureStdout();
+    EXPECT_EXIT(parser.parse(), ::testing::ExitedWithCode(EXIT_SUCCESS), "");
+
+    my_stdout = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(my_stdout, expected);
+}
+
+TEST(format_man, full_info_short_copyright)
+{
+    // Create the dummy parser.
+    argument_parser parser{"default", 3, argv};
+
+    // Fill out the dummy parser with options and flags and sections and subsections.
+    dummy_init(parser);
     // Add a short copyright and test the dummy parser.
     parser.info.short_copyright = "short copyright";
-    {
-        expected += std::string{".SH LEGAL\n"
-                                "\\fBdefault Copyright:\\fR short copyright\n"
-                                ".br\n"
-                                "\\fBSeqAn Copyright:\\fR 2006-2015 Knut Reinert, FU-Berlin; released under the 3-clause BSDL.\n"
-                                ".br\n"};
-        testing::internal::CaptureStdout();
-        EXPECT_EXIT(parser.parse(), ::testing::ExitedWithCode(EXIT_SUCCESS), "");
+    expected += std::string{".SH LEGAL\n"
+                            "\\fBdefault Copyright:\\fR short copyright\n"
+                            ".br\n"
+                            "\\fBSeqAn Copyright:\\fR 2006-2015 Knut Reinert, FU-Berlin; released under the 3-clause BSDL.\n"
+                            ".br\n"};
+    testing::internal::CaptureStdout();
+    EXPECT_EXIT(parser.parse(), ::testing::ExitedWithCode(EXIT_SUCCESS), "");
 
-        my_stdout = testing::internal::GetCapturedStdout();
-        EXPECT_EQ(my_stdout, expected);
-    }
-    // Now add a citation and test it again.
+    my_stdout = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(my_stdout, expected);
+}
+
+TEST(format_man, full_info_short_and_citation)
+{
+    // Create the dummy parser.
+    argument_parser parser{"default", 3, argv};
+
+    // Fill out the dummy parser with options and flags and sections and subsections.
+    dummy_init(parser);
+    // Add a short copyright & citation and test the dummy parser.
+    parser.info.short_copyright = "short copyright";
     parser.info.citation = "citation";
-    {
-        expected += std::string{"\\fBIn your academic works please cite:\\fR citation\n"
-                                ".br\n"};
-        testing::internal::CaptureStdout();
-        EXPECT_EXIT(parser.parse(), ::testing::ExitedWithCode(EXIT_SUCCESS), "");
+    expected += std::string{"\\fBIn your academic works please cite:\\fR citation\n"
+                            ".br\n"};
+    testing::internal::CaptureStdout();
+    EXPECT_EXIT(parser.parse(), ::testing::ExitedWithCode(EXIT_SUCCESS), "");
 
-        my_stdout = testing::internal::GetCapturedStdout();
-        EXPECT_EQ(my_stdout, expected);
-    }
-    // Lastly, add a long copyright and test it one more time.
+    my_stdout = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(my_stdout, expected);
+}
+
+TEST(format_man, full_info_short_long_and_citation)
+{
+    // Create the dummy parser.
+    argument_parser parser{"default", 3, argv};
+
+    // Fill out the dummy parser with options and flags and sections and subsections.
+    dummy_init(parser);
+    // Add a short copyright & citation & long copyright and test the dummy parser.
+    parser.info.short_copyright = "short copyright";
+    parser.info.citation = "citation";
     parser.info.long_copyright = "looong copyright";
-    {
-        expected += std::string{"For full copyright and/or warranty information see \\fB--copyright\\fR.\n"};
-        testing::internal::CaptureStdout();
-        EXPECT_EXIT(parser.parse(), ::testing::ExitedWithCode(EXIT_SUCCESS), "");
+    expected += std::string{"For full copyright and/or warranty information see \\fB--copyright\\fR.\n"};
+    testing::internal::CaptureStdout();
+    EXPECT_EXIT(parser.parse(), ::testing::ExitedWithCode(EXIT_SUCCESS), "");
 
-        my_stdout = testing::internal::GetCapturedStdout();
-        EXPECT_EQ(my_stdout, expected);
-    }
+    my_stdout = testing::internal::GetCapturedStdout();
+    EXPECT_EQ(my_stdout, expected);
 }
