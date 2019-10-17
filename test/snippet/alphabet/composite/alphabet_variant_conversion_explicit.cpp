80cd30886b42108079cf9c9f1b15a87bf418a6c8
Hannes Hauswedell <h2+github.com@fsfe.org>
2019-10-17 15:37:20 +0200

    [MISC] add/update snippets for alphabet_variant

---
+++ test/snippet/alphabet/composite/alphabet_variant_conversion_explicit.cpp
@@ -0,0 +1,13 @@
+#include <seqan3/alphabet/composite/alphabet_variant.hpp>
+#include <seqan3/alphabet/gap/gap.hpp>
+#include <seqan3/alphabet/nucleotide/all.hpp>
+
+int main()
+{
+    using seqan3::operator""_dna5;
+
+    // possible:
+    seqan3::alphabet_variant<seqan3::dna4, seqan3::gap> letter1{'C'_dna5};
+    // not possible:
+    // seqan3::alphabet_variant<seqan3::dna4, seqan3::gap> letter2 = 'C'_dna5;
+}
