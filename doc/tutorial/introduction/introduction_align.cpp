b6a98ffc9ca9cc1e372053575f702fbf57e5b56c
Lydia Buntrock <lydia.buntrock@fu-berlin.de>
2019-12-04 17:05:51 +0100

    [DOC] remove 'using namespace seqan3' from introduction ('First steps with SeqAn')

--- doc/tutorial/introduction/introduction_align.cpp
+++ doc/tutorial/introduction/introduction_align.cpp
@@ -11,41 +11,41 @@
 #include <seqan3/alignment/pairwise/align_pairwise.hpp>                   // for align_pairwise
 //! [alignment_include]
 
-using namespace seqan3;
-
 int main()
 {
     auto tmp_dir = std::filesystem::temp_directory_path();
     std::string filename{tmp_dir/"seq.fasta"};
     {
         // Create a /tmp/my.fasta file.
-        sequence_file_output file_out{filename};
+        seqan3::sequence_file_output file_out{filename};
+        using seqan3::operator""_dna4;
         file_out.emplace_back("ACGTGATG"_dna4, std::string{"seq1"});
         file_out.emplace_back("AGTGATACT"_dna4, std::string{"seq2"});
     }
 
 //! [sequence_input]
     // Initialise a file input object with a FastA file.
-    sequence_file_input file_in{filename}; // filename: "seq.fasta"
+    seqan3::sequence_file_input file_in{filename}; // filename: "seq.fasta"
 
     // Retrieve the sequences and ids.
     for (auto &[seq, id, qual] : file_in)
     {
-        debug_stream << "ID:  " << id << '\n';
-        debug_stream << "SEQ: " << seq << '\n';
-        debug_stream << "EMPTY QUAL." << qual << '\n'; // qual is empty for FastA files
+        seqan3::debug_stream << "ID:  " << id << '\n';
+        seqan3::debug_stream << "SEQ: " << seq << '\n';
+        seqan3::debug_stream << "EMPTY QUAL." << qual << '\n'; // qual is empty for FastA files
     }
 //! [sequence_input]
 
+    using seqan3::operator""_dna5;
     std::vector<seqan3::dna5_vector> sequences{"ACGTGATG"_dna5, "AGTGATACT"_dna5};
 //! [alignment]
     // Call a pairwise alignment with edit distance and traceback.
     for (auto && res : align_pairwise(std::tie(sequences[0], sequences[1]),
-                                      align_cfg::edit | align_cfg::result{with_alignment}))
+                                      seqan3::align_cfg::edit | seqan3::align_cfg::result{seqan3::with_alignment}))
     {
         // Print the resulting score and the alignment.
-        debug_stream << res.score() << '\n';              // => -4
-        debug_stream << res.alignment() << '\n';          // =>       0     .    :
+        seqan3::debug_stream << res.score() << '\n';      // => -4
+        seqan3::debug_stream << res.alignment() << '\n';  // =>       0     .    :
                                                           //            ACGTGATG--
                                                           //            | |||||
                                                           //            A-GTGATACT
--- doc/tutorial/introduction/introduction_hello_world.cpp
+++ doc/tutorial/introduction/introduction_hello_world.cpp
@@ -1,10 +1,8 @@
 //! [hello]
 #include <seqan3/core/debug_stream.hpp> // for debug_stream
 
-using namespace seqan3;
-
 int main()
 {
-    debug_stream << "Hello World!\n";
+    seqan3::debug_stream << "Hello World!\n";
 }
 //! [hello]
