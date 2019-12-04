32ef9581fbb8a080037bbd9a844e15c04623729c
Lydia Buntrock <lydia.buntrock@fu-berlin.de>
2019-12-04 18:10:32 +0100

    [DOC] remove 'using namespace seqan3' from c++ concepts

--- doc/tutorial/concepts/overloading_solution1.cpp
+++ doc/tutorial/concepts/overloading_solution1.cpp
@@ -1,19 +1,20 @@
 #include <iostream>                     // for std::cout
 #include <seqan3/alphabet/all.hpp>      // include all alphabet headers
 
-using namespace seqan3;
-
-template <alphabet t>
+template <seqan3::alphabet t>
 void print(t const v)
 {
-    std::cout << "I am an alphabet and my value as char is: " << to_char(v) << '\n';
+    std::cout << "I am an alphabet and my value as char is: " << seqan3::to_char(v) << '\n';
 }
+
+using seqan3::operator""_dna5;
+using seqan3::operator""_aa27;
 
 int main()
 {
     auto d = 'A'_dna5;
     auto a = 'L'_aa27;
-    auto g = gap{};
+    auto g = seqan3::gap{};
 
     print(d);
     print(a);
