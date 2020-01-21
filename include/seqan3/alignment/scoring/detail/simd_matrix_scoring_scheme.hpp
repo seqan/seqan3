2763c900318c548ae824ca352547618ee5e323d4
rrahn <rene.rahn@fu-berlin.de>
2020-01-21 15:27:16 +0100

    [MISC] Update the copyright year. (#1503)
    * [MISC] Update the copyright year.

--- include/seqan3/alignment/scoring/detail/simd_matrix_scoring_scheme.hpp
+++ include/seqan3/alignment/scoring/detail/simd_matrix_scoring_scheme.hpp
@@ -1,6 +1,6 @@
 // -----------------------------------------------------------------------------------------------------
-// Copyright (c) 2006-2019, Knut Reinert & Freie Universität Berlin
-// Copyright (c) 2016-2019, Knut Reinert & MPI für molekulare Genetik
+// Copyright (c) 2006-2020, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2020, Knut Reinert & MPI für molekulare Genetik
 // This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
 // shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE
 // -----------------------------------------------------------------------------------------------------
