f9b4d3a89592825b4fa70c18b7720af77d40839d
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2020-01-28 22:07:53 +0100

    Apply code suggestions

--- include/seqan3/alignment/scoring/scoring_scheme_concept.hpp
+++ include/seqan3/alignment/scoring/scoring_scheme_concept.hpp
@@ -59,8 +59,8 @@ namespace seqan3
 //!\cond
 template <typename t, typename alphabet_t, typename alphabet2_t = alphabet_t>
 SEQAN3_CONCEPT scoring_scheme = requires (t scheme,
-                                                alphabet_t const alph1,
-                                                alphabet2_t const alph2)
+                                          alphabet_t const alph1,
+                                          alphabet2_t const alph2)
 {
     requires alphabet<alphabet_t>;
     requires alphabet<alphabet2_t>;
