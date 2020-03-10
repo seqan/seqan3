043f5dd4b187a2e3fa73d164d0cc0bbd1b24d173
Lydia Buntrock <lydia.buntrock@fu-berlin.de>
2020-03-10 15:05:38 +0100

    [MISC] Remove 'using namespace seqan3' from test/unit/alphabet/quality.
    Signed-off-by: Lydia Buntrock <lydia.buntrock@fu-berlin.de>

--- test/unit/alphabet/quality/phred_test_template.hpp
+++ test/unit/alphabet/quality/phred_test_template.hpp
@@ -9,8 +9,6 @@
 
 #include <seqan3/alphabet/quality/all.hpp>
 
-using namespace seqan3;
-
 template <typename T>
 using phred = ::testing::Test;
 
@@ -19,15 +17,15 @@ TYPED_TEST_SUITE_P(phred);
 // test provision of data type `phred_type` and phred converter.
 TYPED_TEST_P(phred, concept_check)
 {
-    EXPECT_TRUE(quality_alphabet<TypeParam>);
-    EXPECT_TRUE(quality_alphabet<TypeParam &>);
-    EXPECT_TRUE(quality_alphabet<TypeParam const>);
-    EXPECT_TRUE(quality_alphabet<TypeParam const &>);
+    EXPECT_TRUE(seqan3::quality_alphabet<TypeParam>);
+    EXPECT_TRUE(seqan3::quality_alphabet<TypeParam &>);
+    EXPECT_TRUE(seqan3::quality_alphabet<TypeParam const>);
+    EXPECT_TRUE(seqan3::quality_alphabet<TypeParam const &>);
 
-    EXPECT_TRUE(writable_quality_alphabet<TypeParam>);
-    EXPECT_TRUE(writable_quality_alphabet<TypeParam &>);
-    EXPECT_FALSE(writable_quality_alphabet<TypeParam const>);
-    EXPECT_FALSE(writable_quality_alphabet<TypeParam const &>);
+    EXPECT_TRUE(seqan3::writable_quality_alphabet<TypeParam>);
+    EXPECT_TRUE(seqan3::writable_quality_alphabet<TypeParam &>);
+    EXPECT_FALSE(seqan3::writable_quality_alphabet<TypeParam const>);
+    EXPECT_FALSE(seqan3::writable_quality_alphabet<TypeParam const &>);
 }
 
 // more elaborate test of assign_char and to_char, basic test is in alphabet_test.cpp
