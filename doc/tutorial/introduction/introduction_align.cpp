bbdbe2b7daa03ebe4aa413e4ba5fb79bff878ec1
wvandertoorn <31660235+wvandertoorn@users.noreply.github.com>
2020-03-12 14:18:14 +0100

    [DOC] Improve tutorial; section "First steps" (#1626)
    * [DOC] 1.1.0 Fix Note on 'this is a code snippet'
    * [FIX] 1.1.1 More explicit includes, all.hpp where appropriate

--- doc/tutorial/introduction/introduction_align.cpp
+++ doc/tutorial/introduction/introduction_align.cpp
@@ -1,14 +1,15 @@
 //! [alignment]
-#include <vector>                                                           // for std::vector
+#include <vector>                                         // for std::vector
+#include <tuple>                                          // for std::tie
 
-#include <seqan3/alignment/aligned_sequence/aligned_sequence_concept.hpp>   // for alignment stream operator
-#include <seqan3/alignment/pairwise/align_pairwise.hpp>                     // for align_pairwise
-#include <seqan3/alphabet/nucleotide/dna5.hpp>                              // for dna5 datastrucutre
-#include <seqan3/argument_parser/all.hpp>                                   // for argument_parser
-#include <seqan3/core/debug_stream.hpp>                                     // for debug_stream
-#include <seqan3/io/sequence_file/input.hpp>                                // for sequence_file_input
-#include <seqan3/io/sequence_file/output.hpp>                               // for sequence_file_output
-#include <seqan3/std/filesystem>                                            // for tmp_dir
+#include <seqan3/alignment/aligned_sequence/all.hpp>      // for alignment stream operator <<
+#include <seqan3/alignment/configuration/all.hpp>         // for all configs in the seqan3::align_cfg namespace
+#include <seqan3/alignment/pairwise/all.hpp>              // for seqan3::align_pairwise
+#include <seqan3/alphabet/nucleotide/dna5.hpp>            // for dna5 datastrucutre
+#include <seqan3/argument_parser/all.hpp>                 // for argument_parser
+#include <seqan3/core/debug_stream.hpp>                   // for debug_stream
+#include <seqan3/io/sequence_file/all.hpp>                // for sequence_file_input and sequence_file_output
+#include <seqan3/std/filesystem>                          // for tmp_dir
 
 int main()
 {
