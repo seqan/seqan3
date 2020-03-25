7f96e7d2934642b24c23ce1f346cb1ba5b40925f
Marcel <marehr@users.noreply.github.com>
2020-03-25 22:31:19 +0100

    [MISC] io: move include from io/detail/misc to io/alignment_file/format_sam (#1675)
    * [MISC] io: move include from io/detail/misc to io/alignment_file/format_sam
    
    * Who builds snippet and benchmarks?

--- include/seqan3/io/detail/misc.hpp
+++ include/seqan3/io/detail/misc.hpp
@@ -17,7 +17,6 @@
 #include <seqan3/core/detail/pack_algorithm.hpp>
 #include <seqan3/core/type_traits/template_inspection.hpp>
 #include <seqan3/io/exception.hpp>
-#include <seqan3/io/sequence_file/input_format_concept.hpp>
 #include <seqan3/std/algorithm>
 #include <seqan3/std/filesystem>
 #include <seqan3/std/iterator>
