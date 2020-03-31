f96e3b803b27181f3de6ed0fcdef8ae14825fb5c
Mitra Darja Darvish <mitradarvish@ash.imp.fu-berlin.de>
2020-03-31 10:23:54 +0200

    [TEST] Add minimise_hash function tests

--- include/seqan3/range/views/minimiser_hash.hpp
+++ include/seqan3/range/views/minimiser_hash.hpp
@@ -7,26 +7,28 @@
 
 /*!\file
  * \author Mitra Darvish <mitra.darvish AT fu-berlin.de>
- * \brief Provides seqan3::views::minimizer_hash.
+ * \brief Provides seqan3::views::minimiser_hash.
  */
 
 #pragma once
 
 #include <seqan3/range/views/kmer_hash.hpp>
-#include <seqan3/range/views/minimizer.hpp>
+#include <seqan3/range/views/minimiser.hpp>
 #include <seqan3/search/kmer_index/shape.hpp>
+
+#include <seqan3/core/debug_stream.hpp>
 
 namespace seqan3::detail
 {
 // ============================================================================
-//  views::minimizer_hash (adaptor instance definition)
+//  views::minimiser_hash (adaptor instance definition)
 // ============================================================================
 
 //![adaptor_def]
-//!\brief views::minimizer's range adaptor object type (non-closure).
-struct minimizer_hash_fn
+//!\brief views::minimiser's range adaptor object type (non-closure).
+struct minimiser_hash_fn
 {
-    //!\brief Store the shape and the window_size and return a range adaptor closure object.
+    //!\brief Store the shape and return a range adaptor closure object.
     constexpr auto operator()(seqan3::shape const & shape) const
     {
         return seqan3::detail::adaptor_from_functor{*this, shape, shape.size()};
@@ -36,7 +38,7 @@ struct minimizer_hash_fn
     {
         return seqan3::detail::adaptor_from_functor{*this, shape, window_size};
     }
-    //!\brief Store the shape and the window_size and return a range adaptor closure object.
+    //!\brief Store the shape, the window_size and the seed and return a range adaptor closure object.
     constexpr auto operator()(seqan3::shape const & shape, uint32_t const & window_size, uint64_t const & seed) const
     {
         return seqan3::detail::adaptor_from_functor{*this, shape, window_size, seed};
@@ -47,41 +49,26 @@ struct minimizer_hash_fn
      * \param[in] urange The input range to process. Must model std::ranges::viewable_range and the reference type
      *                   of the range must model seqan3::semialphabet.
      * \param[in] shape_ The seqan3::shape to use for hashing.
-     * \throws std::invalid_argument if resulting hash values would be too big for a 64 bit integer.
+     * \throws std::invalid_argument if the size of the shape is greater than the window_size.
      * \returns          A range of converted elements.
      */
-
-    /*!\brief            Call the view's constructor with the underlying view, a seqan3::shape and a window size as
-     *                   argument.
-     * \param[in] urange The input range to process. Must model std::ranges::viewable_range and the reference type
-     *                   of the range must model seqan3::semialphabet.
-     * \param[in] shape_ The seqan3::shape to use for hashing.
-     * \throws std::invalid_argument if resulting hash values would be too big for a 64 bit integer.
-     * \returns          A range of converted elements.
-     */
-    template <std::ranges::range urng_t>
-    constexpr auto operator()(urng_t && urange, seqan3::shape const & shape, uint32_t const & window_size) const
-    {
-        static_assert(std::ranges::viewable_range<urng_t>,
-            "The range parameter to views::minimizer cannot be a temporary of a non-view range.");
-        static_assert(std::ranges::forward_range<urng_t>,
-            "The range parameter to views::minimizer must model std::ranges::forward_range.");
-
-        return std::forward<urng_t>(urange) | seqan3::views::kmer_hash(shape) | seqan3::views::minimizer(shape.size(),
-                                    window_size);
-    }
-
     template <std::ranges::range urng_t>
     constexpr auto operator()(urng_t && urange, seqan3::shape const & shape, uint32_t const & window_size,
-                              uint64_t const & seed) const
+                              uint64_t const & seed = 0x8F3F73B5CF1C9ADE) const
     {
         static_assert(std::ranges::viewable_range<urng_t>,
-            "The range parameter to views::minimizer cannot be a temporary of a non-view range.");
+            "The range parameter to views::minimiser_hash cannot be a temporary of a non-view range.");
         static_assert(std::ranges::forward_range<urng_t>,
-            "The range parameter to views::minimizer must model std::ranges::forward_range.");
+            "The range parameter to views::minimiser_hash must model std::ranges::forward_range.");
+        static_assert(semialphabet<reference_t<urng_t>>,
+            "The range parameter to views::minimiser_hash must be over elements of seqan3::semialphabet.");
 
-        return std::forward<urng_t>(urange) | seqan3::views::kmer_hash(shape) | seqan3::views::minimizer(shape.size(),
-                                    window_size, seed);
+        if (shape.size() > window_size)
+            throw std::invalid_argument{"The size of the shape cannot be greater than the window size."};
+
+        return std::forward<urng_t>(urange) | seqan3::views::kmer_hash(shape)
+                                            | std::views::transform([seed] (int i) { return i ^ seed; })
+                                            | seqan3::views::minimiser(window_size - shape.size() + 1);
     }
 };
 //![adaptor_def]
@@ -95,23 +82,23 @@ namespace seqan3::views
  * \{
  */
 
-/*!\brief                    Computes minimizers for a range with a given shape, window size and seed.
+/*!\brief                    Computes minimisers for a range with a given shape, window size and seed.
  * \tparam urng_t            The type of the range being processed. See below for requirements. [template parameter is
  *                           omitted in pipe notation]
  * \param[in] urange         The range being processed. [parameter is omitted in pipe notation]
  * \param[in] shape          The seqan3::shape that determines how to compute the hash value.
- * \param[in] window_size    The swindow size to use.
+ * \param[in] window_size    The window size to use.
  * \param[in] seed           The seed to use. Default: 0x8F3F73B5CF1C9ADE.
- * \returns                  A range of std::size_t where each value is the hash of the resp. k-mer.
+ * \returns                  A range of std::integral where each value is the minimiser of the resp. window.
  *                           See below for the properties of the returned range.
  * \ingroup views
  *
  * \details
  *
  * \attention
- * For the alphabet size \f$\sigma\f$ of the alphabet of `urange` and the number of 1s \f$s\f$ of `shape` it must hold
- * that \f$s>\frac{64}{\log_2\sigma}\f$, i.e. hashes resulting from the shape/alphabet combination can be represented
- * in an `uint64_t`.
+ * As f the the seqan3::views::kmer_hash the alphabet size \f$\sigma\f$ of the alphabet of `urange` and the number of
+ * 1s \f$s\f$ of `shape` it must hold that \f$s>\frac{64}{\log_2\sigma}\f$, i.e. hashes resulting from the
+ * shape/alphabet combination can be represented in an `uint64_t`.
  *
  * ### View properties
  *
@@ -119,18 +106,18 @@ namespace seqan3::views
  * |----------------------------------|:----------------------------------:|:--------------------------------:|
  * | std::ranges::input_range         | *required*                         | *preserved*                      |
  * | std::ranges::forward_range       | *required*                         | *preserved*                      |
- * | std::ranges::bidirectional_range |                                    | *preserved*                      |
- * | std::ranges::random_access_range |                                    | *preserved*                      |
+ * | std::ranges::bidirectional_range |                                    | *lost*                           |
+ * | std::ranges::random_access_range |                                    | *lost*                           |
  * | std::ranges::contiguous_range    |                                    | *lost*                           |
  * |                                  |                                    |                                  |
  * | std::ranges::viewable_range      | *required*                         | *guaranteed*                     |
  * | std::ranges::view                |                                    | *guaranteed*                     |
- * | std::ranges::sized_range         |                                    | *preserved*                      |
+ * | std::ranges::sized_range         |                                    | *lost*                           |
  * | std::ranges::common_range        |                                    | *lost*                           |
  * | std::ranges::output_range        |                                    | *lost*                           |
  * | seqan3::const_iterable_range     |                                    | *preserved*                      |
  * |                                  |                                    |                                  |
- * | std::ranges::range_reference_t   | seqan3::semialphabet               | std::size_t                      |
+ * | std::ranges::range_reference_t   | seqan3::semialphabet               | std::integral                    |
  *
  * See the \link views views submodule documentation \endlink for detailed descriptions of the view properties.
  *
@@ -140,7 +127,7 @@ namespace seqan3::views
  *
  * \hideinitializer
  */
-inline auto constexpr minimizer_hash = detail::minimizer_hash_fn{};
+inline constexpr auto minimiser_hash = detail::minimiser_hash_fn{};
 
 //!\}
 
---
+++ test/unit/range/views/view_minimiser_hash_test.cpp
@@ -0,0 +1,117 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2020, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2020, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+#include <forward_list>
+#include <list>
+#include <type_traits>
+
+#include <seqan3/alphabet/nucleotide/dna4.hpp>
+#include <seqan3/core/debug_stream.hpp>
+#include <seqan3/range/container/bitcompressed_vector.hpp>
+#include <seqan3/range/views/kmer_hash.hpp>
+#include <seqan3/range/views/minimiser_hash.hpp>
+#include <seqan3/range/views/take_exactly.hpp>
+#include <seqan3/range/views/take_until.hpp>
+#include <seqan3/range/views/to.hpp>
+
+#include <gtest/gtest.h>
+
+using seqan3::operator""_dna4;
+using seqan3::operator""_shape;
+using result_t = std::vector<size_t>;
+
+static constexpr seqan3::shape ungapped_shape = seqan3::ungapped{4};
+static constexpr seqan3::shape gapped_shape = 0b1001_shape;
+static constexpr auto ungapped_view = seqan3::views::minimiser_hash(ungapped_shape, 8, 0); // window_size 5, seed 0
+static constexpr auto gapped_view = seqan3::views::minimiser_hash(gapped_shape, 8, 0);     // window_size 5, seed 0
+
+template <typename T>
+class minimiser_hash_properties_test: public ::testing::Test { };
+
+using underlying_range_types = ::testing::Types<std::vector<seqan3::dna4>,
+                                                std::vector<seqan3::dna4> const,
+                                                seqan3::bitcompressed_vector<seqan3::dna4>,
+                                                seqan3::bitcompressed_vector<seqan3::dna4> const,
+                                                std::list<seqan3::dna4>,
+                                                std::list<seqan3::dna4> const,
+                                                std::forward_list<seqan3::dna4>,
+                                                std::forward_list<seqan3::dna4> const>;
+
+TYPED_TEST_SUITE(minimiser_hash_properties_test, underlying_range_types, );
+
+class minimiser_hash_test : public ::testing::Test
+{
+protected:
+    std::vector<seqan3::dna4> text1{"AAAAAAAAAA"_dna4};
+    std::vector<seqan3::dna4> text1_short{"AAAAAA"_dna4};
+    result_t result1{0, 0, 0}; // Same for ungapped and gapped
+    result_t result1_default_seed{0x8F3F73B5CF1C9ADE, 0x8F3F73B5CF1C9ADE, 0x8F3F73B5CF1C9ADE}; // Default seed used
+
+    std::vector<seqan3::dna4> text2{"AC"_dna4};
+    result_t result2{};
+
+    std::vector<seqan3::dna4> text3{"ACGGCGACGTTTAG"_dna4};
+    result_t ungapped_no_rev3{26, 97}; // ACGG, CGAC
+    result_t gapped_no_rev3{2, 5};     // A--G, C--C- "-" for gap
+};
+
+TYPED_TEST(minimiser_hash_properties_test, different_input_ranges)
+{
+    TypeParam text{'A'_dna4, 'C'_dna4, 'G'_dna4, 'T'_dna4, 'C'_dna4, 'G'_dna4, 'A'_dna4, 'C'_dna4, 'G'_dna4, 'T'_dna4,
+                   'T'_dna4, 'T'_dna4, 'A'_dna4, 'G'_dna4}; // ACGTCGACGTTTAG
+    result_t ungapped_no_rev{27, 97, 27}; // ACGT, CGAC, ACGT
+    result_t gapped_no_rev{3, 5, 3};      // A--T, C--C, A--T - "-" for gap
+    EXPECT_EQ(ungapped_no_rev, text | ungapped_view | seqan3::views::to<result_t>);
+    EXPECT_EQ(gapped_no_rev, text | gapped_view | seqan3::views::to<result_t>);
+}
+
+TEST_F(minimiser_hash_test, ungapped)
+{
+    EXPECT_EQ(result1, text1 | ungapped_view | seqan3::views::to<result_t>);
+    EXPECT_EQ(result2, text2 | ungapped_view | seqan3::views::to<result_t>);
+
+    auto stop_at_t = seqan3::views::take_until([] (seqan3::dna4 const x) { return x == 'T'_dna4; });
+    EXPECT_EQ(ungapped_no_rev3, text3 | stop_at_t | seqan3::views::minimiser_hash(seqan3::ungapped{4}, 8, 0)
+                                      | seqan3::views::to<result_t>);
+}
+
+TEST_F(minimiser_hash_test, gapped)
+{
+    EXPECT_EQ(result1, text1 | gapped_view | seqan3::views::to<result_t>);
+    EXPECT_EQ(result2, text2 | gapped_view | seqan3::views::to<result_t>);
+
+    auto stop_at_t = seqan3::views::take_until([] (seqan3::dna4 const x) { return x == 'T'_dna4; });
+    EXPECT_EQ(gapped_no_rev3, text3 | stop_at_t | gapped_view | seqan3::views::to<result_t>);
+}
+
+TEST_F(minimiser_hash_test, default_seed)
+{
+    EXPECT_EQ(result1_default_seed, text1 | seqan3::views::minimiser_hash(ungapped_shape, 8)
+                                          | seqan3::views::to<result_t>);
+    EXPECT_EQ(result1_default_seed, text1 | seqan3::views::minimiser_hash(gapped_shape, 8)
+                                          | seqan3::views::to<result_t>);
+}
+
+TEST_F(minimiser_hash_test, shape_size_equal_window_size)
+{
+    auto apply_seed = std::views::transform([] (int i) { return i ^ 0x8F3F73B5CF1C9ADE; });
+    EXPECT_EQ(text1 | seqan3::views::kmer_hash(ungapped_shape)
+                    | apply_seed
+                    | seqan3::views::to<result_t>,
+              text1 | seqan3::views::minimiser_hash(ungapped_shape) | seqan3::views::to<result_t>);
+    EXPECT_EQ(text1 | seqan3::views::kmer_hash(gapped_shape)
+                    | apply_seed
+                    | seqan3::views::to<result_t>,
+              text1 | seqan3::views::minimiser_hash(gapped_shape) | seqan3::views::to<result_t>);
+}
+
+
+TEST_F(minimiser_hash_test, shape_bigger_than_window)
+{
+    EXPECT_THROW(text1 | seqan3::views::minimiser_hash(ungapped_shape, 3), std::invalid_argument);
+    EXPECT_THROW(text1 | seqan3::views::minimiser_hash(gapped_shape, 3), std::invalid_argument);
+}
