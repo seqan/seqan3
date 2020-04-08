a88773069bd8d33e0c85cdfb107a25d3989cc9f3
rrahn <rene.rahn@fu-berlin.de>
2020-04-08 12:05:27 +0200

    [MISC] Add requirements to function traits class

--- include/seqan3/core/type_traits/function.hpp
+++ include/seqan3/core/type_traits/function.hpp
@@ -27,12 +27,18 @@ template <typename function_t>
 struct function_traits;
 //!\endcond
 
-/*!\brief A traits class to provide a uniform interface to the properties of a std::function type.
+/*!\brief A traits class to provide a uniform interface to the properties of a function type.
  * \ingroup type_traits
+ * \tparam return_t The return type of the function.
+ * \tparam args_t A template parameter pack over the argument types of the function.
+ *
+ * \details
  *
  * seqan3::function_traits is the type trait class that provides a uniform interface to the properties of
- * a std::function type.
+ * a std::function type, a lambda type, a function type or a function pointer type.
  * This makes it possible to access the return type and the argument types of the stored target function.
+ * The function types must be complete, i.e. all argument types and the return type must be known, otherwise
+ * this traits class is incomplete.
  *
  * ### Example
  *
@@ -56,6 +62,14 @@ struct function_traits<std::function<return_t(args_t...)>>
     //!\endcond
     using argument_type_at = pack_traits::at<index, args_t...>;
 };
+
+//!\cond
+// Overload for all function types.
+template <typename function_t>
+    requires requires (function_t fn) { {std::function{fn}}; }
+struct function_traits<function_t> : function_traits<decltype(std::function{std::declval<function_t>()})>
+{};
+//!\endcond
 } // namespace seqan3
 
 namespace seqan3::detail
