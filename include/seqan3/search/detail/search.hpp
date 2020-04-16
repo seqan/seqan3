286a2f11512e06475d085cd2f5c791d60a52c3b7
smehringer <svenja.mehringer@gmail.com>
2020-04-16 08:13:25 +0200

    [MISC] Simplify redundant code. (#1715)
    * [MISC] Simplify redundant code.

--- include/seqan3/search/detail/search.hpp
+++ include/seqan3/search/detail/search.hpp
@@ -78,45 +78,39 @@ inline auto search_single(index_t const & index, query_t & query, configuration_
     };
 
     // choose mode
-    if constexpr (search_traits_t::search_best_hits)
+    if constexpr (search_traits_t::search_best_hits ||
+                  search_traits_t::search_all_best_hits ||
+                  search_traits_t::search_strata_hits)
     {
         detail::search_param max_error2{max_error};
         max_error2.total = 0;
         while (internal_hits.empty() && max_error2.total <= max_error.total)
         {
-            detail::search_algo<true>(index, query, max_error2, internal_delegate);
+            // * If you only want the best hit (search_traits_t::search_best_hits), you stop after finding the first hit,
+            //   the hit with the least errors (`abort_on_hit` is true).
+            // * If you are in strata mode (search_traits_t::search_strata_hits), you do the same as with best hits,
+            //   but then do the extra step afterwards (`abort_on_hit` is true).
+            // * If you want all best hits (search_traits_t::search_all_best_hits), you do not stop after the first hit
+            //   but continue the current search algorithm/max_error2 pattern (`abort_on_hit` is true).
+            constexpr bool abort_on_hit = !search_traits_t::search_all_best_hits;
+            detail::search_algo<abort_on_hit>(index, query, max_error2, internal_delegate);
             max_error2.total++;
         }
-    }
-    else if constexpr (search_traits_t::search_all_best_hits)
-    {
-        detail::search_param max_error2{max_error};
-        max_error2.total = 0;
-        while (internal_hits.empty() && max_error2.total <= max_error.total)
+        if constexpr (search_traits_t::search_strata_hits)
         {
-            detail::search_algo<false>(index, query, max_error2, internal_delegate);
-            max_error2.total++;
-        }
-    }
-    else if constexpr (search_traits_t::search_strata_hits)
-    {
-        detail::search_param max_error2{max_error};
-        max_error2.total = 0;
-        while (internal_hits.empty() && max_error2.total <= max_error.total)
-        {
-            detail::search_algo<true>(index, query, max_error2, internal_delegate);
-            max_error2.total++;
-        }
-        if (!internal_hits.empty())
-        {
-            internal_hits.clear(); // TODO: don't clear when using Optimum Search Schemes with lower error bounds
-            uint8_t const s = get<search_cfg::mode>(cfg).value;
-            max_error2.total += s - 1;
-            detail::search_algo<false>(index, query, max_error2, internal_delegate);
+            if (!internal_hits.empty())
+            {
+                internal_hits.clear(); // TODO: don't clear when using Optimum Search Schemes with lower error bounds
+                uint8_t const s = get<search_cfg::mode>(cfg).value;
+                max_error2.total += s - 1;
+                detail::search_algo<false>(index, query, max_error2, internal_delegate);
+            }
         }
     }
     else // detail::search_mode_all
     {
+        // If you want to find all hits, you cannot stop once you found any hit (<false>)
+        // since you have to find all paths in the search tree that satisfy the hit condition.
         detail::search_algo<false>(index, query, max_error, internal_delegate);
     }
 
