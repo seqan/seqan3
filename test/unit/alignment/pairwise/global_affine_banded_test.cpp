b404eaa2038af8dc0a14f500e5c7c16f2dbd1d81
rrahn <rene.rahn@fu-berlin.de>
2020-06-04 23:39:42 +0200

    [TEST] Add more test cases for the banded global alignment.

--- test/unit/alignment/pairwise/global_affine_banded_test.cpp
+++ test/unit/alignment/pairwise/global_affine_banded_test.cpp
@@ -12,10 +12,57 @@
 #include "fixture/global_affine_banded.hpp"
 #include "pairwise_alignment_single_test_template.hpp"
 
-using pairwise_global_affine_unbanded_testing_types = ::testing::Types<
-        pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::banded::dna4_01>
-    >;
+using pairwise_global_affine_banded_testing_types = ::testing::Types<
+    pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::banded::dna4_01>,
+    pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::banded::dna4_same_sequence_upper_diagonal_0>,
+    pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::banded::dna4_same_sequence_lower_diagonal_0>,
+    pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::banded::dna4_small_band>,
+    pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::banded::dna4_single_diagonal>,
+    pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::banded::dna4_large_band>
+>;
 
-INSTANTIATE_TYPED_TEST_SUITE_P(pairwise_global_affine_unbanded,
+INSTANTIATE_TYPED_TEST_SUITE_P(pairwise_global_affine_banded,
                                pairwise_alignment_test,
-                               pairwise_global_affine_unbanded_testing_types, );
+                               pairwise_global_affine_banded_testing_types, );
+
+struct pairwise_global_affine_banded : public ::testing::Test
+{
+    using fixture_t = decltype(seqan3::test::alignment::fixture::global::affine::banded::dna4_01);
+
+    fixture_t fixture{seqan3::test::alignment::fixture::global::affine::banded::dna4_01};
+
+    auto & band()
+    {
+        using seqan3::get;
+        return get<seqan3::align_cfg::band_fixed_size>(fixture.config);
+    }
+};
+
+TEST_F(pairwise_global_affine_banded, invalid_band_lower_diagonal_greater_0)
+{
+    band().lower_diagonal = seqan3::align_cfg::lower_diagonal{1};
+    EXPECT_THROW((align_pairwise(std::tie(fixture.sequence1, fixture.sequence2), fixture.config)),
+                 seqan3::invalid_alignment_configuration);
+}
+
+TEST_F(pairwise_global_affine_banded, invalid_band_upper_diagonal_smaller_0)
+{
+    band().lower_diagonal = seqan3::align_cfg::lower_diagonal{-4};
+    band().upper_diagonal = seqan3::align_cfg::upper_diagonal{-1};
+    EXPECT_THROW((align_pairwise(std::tie(fixture.sequence1, fixture.sequence2), fixture.config)),
+                 seqan3::invalid_alignment_configuration);
+}
+
+TEST_F(pairwise_global_affine_banded, invalid_band_upper_diagonal_smaller_lower_diagonal)
+{
+    band().upper_diagonal = seqan3::align_cfg::upper_diagonal{-6};
+    EXPECT_THROW((align_pairwise(std::tie(fixture.sequence1, fixture.sequence2), fixture.config)),
+                 seqan3::invalid_alignment_configuration);
+}
+
+TEST_F(pairwise_global_affine_banded, invalid_band_last_cell_not_covered)
+{
+    band().upper_diagonal = seqan3::align_cfg::upper_diagonal{5};
+    auto result_range = align_pairwise(std::tie(fixture.sequence1, fixture.sequence2), fixture.config);
+    EXPECT_THROW(result_range.begin(), seqan3::invalid_alignment_configuration);
+}
