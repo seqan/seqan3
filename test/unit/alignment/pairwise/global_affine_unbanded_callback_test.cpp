cc434412d6d16456c63b9e1cb8b2a5692c367ed1
rrahn <rene.rahn@fu-berlin.de>
2020-06-05 21:32:04 +0200

    [TEST] Add tests for invoking the alignment with user callbacks.

---
+++ test/unit/alignment/pairwise/global_affine_unbanded_callback_test.cpp
@@ -0,0 +1,28 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2020, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2020, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+#include <gtest/gtest.h>
+
+#include <seqan3/alignment/pairwise/align_pairwise.hpp>
+
+#include "fixture/global_affine_unbanded.hpp"
+#include "pairwise_alignment_single_callback_test_template.hpp"
+
+using pairwise_global_affine_unbanded_testing_types = ::testing::Types<
+    pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::unbanded::dna4_match_4_mismatch_5_gap_1_open_10_part_01>,
+    pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::unbanded::dna4_match_4_mismatch_5_gap_1_open_10_part_02>,
+    pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::unbanded::dna4_match_4_mismatch_5_gap_1_open_10_part_03>,
+    pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::unbanded::dna4_match_4_mismatch_5_gap_1_open_10_part_04>,
+    pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::unbanded::dna4_match_4_mismatch_5_gap_1_open_10_part_05>,
+    pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::unbanded::dna4_match_4_mismatch_5_gap_1_open_10_seq1_empty>,
+    pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::unbanded::dna4_match_4_mismatch_5_gap_1_open_10_seq2_empty>,
+    pairwise_alignment_fixture<&seqan3::test::alignment::fixture::global::affine::unbanded::dna4_match_4_mismatch_5_gap_1_open_10_both_empty>
+>;
+
+INSTANTIATE_TYPED_TEST_SUITE_P(pairwise_global_affine_unbanded,
+                               pairwise_alignment_callback_test,
+                               pairwise_global_affine_unbanded_testing_types, );
---
+++ test/unit/alignment/pairwise/pairwise_alignment_collection_callback_test_template.hpp
@@ -0,0 +1,111 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2020, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2020, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <seqan3/std/algorithm>
+#include <seqan3/std/ranges>
+
+#include <seqan3/alignment/pairwise/align_pairwise.hpp>
+#include <seqan3/range/views/to_char.hpp>
+#include <seqan3/range/views/zip.hpp>
+#include <seqan3/test/expect_range_eq.hpp>
+
+#include "fixture/alignment_fixture.hpp"
+
+using namespace seqan3::test::alignment::fixture;
+
+template <auto _fixture>
+struct pairwise_alignment_fixture : public ::testing::Test
+{
+    auto fixture() -> decltype(alignment_fixture_collection{*_fixture}) const &
+    {
+        return *_fixture;
+    }
+};
+
+template <typename fixture_t>
+class pairwise_alignment_collection_callback_test : public fixture_t
+{};
+
+TYPED_TEST_SUITE_P(pairwise_alignment_collection_callback_test);
+
+TYPED_TEST_P(pairwise_alignment_collection_callback_test, score)
+{
+    auto const & fixture = this->fixture();
+    seqan3::configuration align_cfg = fixture.config |
+                                      seqan3::align_cfg::result{seqan3::with_score} |
+                                      seqan3::align_cfg::on_result{[&] (auto && result)
+                                      {
+                                          auto id = result.id();
+                                          EXPECT_EQ(result.score(), fixture.get_scores()[id]);
+                                      }};
+    auto [database, query] = fixture.get_sequences();
+    seqan3::align_pairwise(seqan3::views::zip(database, query), align_cfg);
+}
+
+TYPED_TEST_P(pairwise_alignment_collection_callback_test, back_coordinate)
+{
+    auto const & fixture = this->fixture();
+    seqan3::configuration align_cfg = fixture.config |
+                                      seqan3::align_cfg::result{seqan3::with_back_coordinate} |
+                                      seqan3::align_cfg::on_result{[&] (auto && result)
+                                      {
+                                          auto id = result.id();
+                                          EXPECT_EQ(result.score(), fixture.get_scores()[id]);
+                                          EXPECT_EQ(result.back_coordinate(), fixture.get_back_coordinates()[id]);
+                                      }};
+    auto [database, query] = fixture.get_sequences();
+    seqan3::align_pairwise(seqan3::views::zip(database, query), align_cfg);
+}
+
+TYPED_TEST_P(pairwise_alignment_collection_callback_test, front_coordinate)
+{
+    auto const & fixture = this->fixture();
+    seqan3::configuration align_cfg = fixture.config |
+                                      seqan3::align_cfg::result{seqan3::with_front_coordinate} |
+                                      seqan3::align_cfg::on_result{[&] (auto && result)
+                                      {
+                                          auto id = result.id();
+                                          EXPECT_EQ(result.score(), fixture.get_scores()[id]);
+                                          EXPECT_EQ(result.back_coordinate(), fixture.get_back_coordinates()[id]);
+                                          EXPECT_EQ(result.front_coordinate(), fixture.get_front_coordinates()[id]);
+                                      }};
+    auto [database, query] = fixture.get_sequences();
+    seqan3::align_pairwise(seqan3::views::zip(database, query), align_cfg);
+}
+
+TYPED_TEST_P(pairwise_alignment_collection_callback_test, alignment)
+{
+    auto const & fixture = this->fixture();
+    seqan3::configuration align_cfg = fixture.config |
+                                      seqan3::align_cfg::result{seqan3::with_alignment} |
+                                      seqan3::align_cfg::on_result{[&] (auto && result)
+                                      {
+                                          auto id = result.id();
+                                          EXPECT_EQ(result.score(), fixture.get_scores()[id]);
+                                          EXPECT_EQ(result.back_coordinate(), fixture.get_back_coordinates()[id]);
+                                          EXPECT_EQ(result.front_coordinate(), fixture.get_front_coordinates()[id]);
+
+                                          auto [gapped_database, gapped_query] = result.alignment();
+                                          EXPECT_RANGE_EQ(gapped_database | seqan3::views::to_char,
+                                                          fixture.get_aligned_sequences1()[id]);
+                                          EXPECT_RANGE_EQ(gapped_query | seqan3::views::to_char,
+                                                          fixture.get_aligned_sequences2()[id]);
+                                      }};
+
+    auto [database, query] = fixture.get_sequences();
+    seqan3::align_pairwise(seqan3::views::zip(database, query), align_cfg);
+}
+
+REGISTER_TYPED_TEST_SUITE_P(pairwise_alignment_collection_callback_test,
+                            score,
+                            back_coordinate,
+                            front_coordinate,
+                            alignment);
