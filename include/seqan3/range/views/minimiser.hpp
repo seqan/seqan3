4a25f68ddd87ddfb414d8b54c3b9146affe61479
Marcel <marehr@users.noreply.github.com>
2020-06-10 15:28:19 +0200

    [MISC] minimiser view: fixup code-style

--- include/seqan3/range/views/minimiser.hpp
+++ include/seqan3/range/views/minimiser.hpp
@@ -12,6 +12,7 @@
 
 #pragma once
 
+#include <seqan3/std/algorithm>
 #include <deque>
 
 #include <seqan3/range/concept.hpp>
@@ -24,8 +25,8 @@ namespace seqan3::detail
 // ---------------------------------------------------------------------------------------------------------------------
 
 /*!\brief The type returned by seqan3::views::minimiser.
- * \tparam urng_t The type of the underlying ranges, must model std::forward_range, the reference type must model
- *                std::totally_ordered. The typical use case is that the reference type is the result of
+ * \tparam urng_t The type of the underlying ranges, must model std::ranges::forward_range, the reference type must
+ *                model std::totally_ordered. The typical use case is that the reference type is the result of
  *                seqan3::kmer_hash.
  * \implements std::ranges::view
  * \ingroup views
@@ -45,7 +46,7 @@ private:
     urng_t urange;
 
     //!\brief The number of values in one window.
-    uint32_t window_values_size;
+    size_t window_values_size;
 
     template <typename rng_t>
     class window_iterator;
@@ -66,7 +67,7 @@ public:
     *                      std::ranges::forward_range.
     * \param[in] w_        The number of values in one window.
     */
-    minimiser_view(urng_t urange_, uint32_t const w_) : urange{std::move(urange_)}, window_values_size{w_}{}
+    minimiser_view(urng_t urange_, size_t const w_) : urange{std::move(urange_)}, window_values_size{w_}{}
 
     /*!\brief Construct from a non-view that can be view-wrapped and a given number of values in one window.
     * \param[in] urange_   The input range to process. Must model std::ranges::viewable_range and
@@ -78,7 +79,7 @@ public:
      requires std::ranges::viewable_range<rng_t> &&
               std::constructible_from<urng_t, ranges::ref_view<std::remove_reference_t<rng_t>>>
     //!\endcond
-    minimiser_view(rng_t && urange_, uint32_t const w_) :
+    minimiser_view(rng_t && urange_, size_t const w_) :
     urange{std::views::all(std::forward<rng_t>(urange_))}, window_values_size{w_} {}
     //!\}
 
@@ -156,9 +157,9 @@ class minimiser_view<urng_t>::window_iterator
 {
 private:
     //!\brief The iterator type of the underlying range.
-    using it_t = std::ranges::iterator_t<rng_t>;
+    using urng_iterator_t = std::ranges::iterator_t<rng_t>;
     //!\brief The sentinel type of the underlying range.
-    using sentinel_t = std::ranges::sentinel_t<rng_t>;
+    using urng_sentinel_t = std::ranges::sentinel_t<rng_t>;
     //!\brief The sentinel type of the minimiser_view.
     using sentinel_type = std::default_sentinel_t;
 
@@ -170,9 +171,9 @@ public:
      * \{
      */
     //!\brief Type for distances between iterators.
-    using difference_type = typename std::iter_difference_t<it_t>;
+    using difference_type = typename std::ranges::range_difference_t<rng_t>;
     //!\brief Value type of this iterator.
-    using value_type = std::ranges::range_value_t<urng_t>;
+    using value_type = std::ranges::range_value_t<rng_t>;
     //!\brief The pointer type.
     using pointer = void;
     //!\brief Reference to `value_type`.
@@ -205,17 +206,17 @@ public:
         requires std::same_as<std::remove_const_t<urng_t>, urng2_t>
      //!\endcond
     window_iterator(window_iterator<urng2_t> it) :
-         urange_end{std::move(it.urange_end)},
+         urng_sentinel{std::move(it.urng_sentinel)},
          minimiser_value{std::move(it.minimiser_value)},
-         window_right{std::move(it.window_right)},
+         urng_iterator{std::move(it.urng_iterator)},
          window_values{std::move(it.window_values)}
     {}
 
     /*!\brief                              Construct from begin and end iterators of a given range over
     *                                      std::totally_ordered values, and the number of values per window.
-    * /param[in] it_start                  Iterator pointing to the first position of the std::totally_ordered range.
-    * /param[in] it_end                    Iterator pointing to the last position of the std::totally_ordered range.
-    * /param[in] window_values_size        The number of values in one window.
+    * \param[in] urng_iterator             Iterator pointing to the first position of the std::totally_ordered range.
+    * \param[in] urng_sentinel             Iterator pointing to the last position of the std::totally_ordered range.
+    * \param[in] window_values_size        The number of values in one window.
     *
     * \details
     *
@@ -223,13 +224,14 @@ public:
     * this action. If a minimiser in consecutive windows is the same, it is returned only once.
     *
     */
-    window_iterator(it_t it_start, sentinel_t it_end, uint32_t window_values_size) :
-    urange_end{it_end}, window_right{it_start}
+    window_iterator(urng_iterator_t urng_iterator, urng_sentinel_t urng_sentinel, size_t window_values_size) :
+        urng_sentinel{std::move(urng_sentinel)},
+        urng_iterator{std::move(urng_iterator)}
     {
-        if (window_values_size > std::ranges::distance(window_right, urange_end))
-            window_values_size = std::ranges::distance(window_right, urange_end);
-        if (window_right != urange_end)
-            window_first(window_values_size);
+        size_t size = std::ranges::distance(urng_iterator, urng_sentinel);
+        window_values_size = std::min<size_t>(window_values_size, size);
+
+        window_first(window_values_size);
     }
     //!\}
 
@@ -240,7 +242,7 @@ public:
     //!\brief Compare to the sentinel of the underlying range.
     friend bool operator==(window_iterator const & lhs, sentinel_type const &)
     {
-        return lhs.window_right == lhs.urange_end;
+        return lhs.urng_iterator == lhs.urng_sentinel;
     }
 
     //!\brief Compare to the sentinel of the underlying range.
@@ -252,7 +254,7 @@ public:
     //!\brief Compare to another window_iterator.
     friend bool operator==(window_iterator const & lhs, window_iterator const & rhs)
     {
-        return (lhs.window_right == rhs.window_right) &&
+        return (lhs.urng_iterator == rhs.urng_iterator) &&
                (lhs.window_values.size() == rhs.window_values.size());
     }
 
@@ -277,32 +279,32 @@ public:
     //!\brief Compare to another window_iterator.
     friend bool operator<(window_iterator const & lhs, window_iterator const & rhs)
     {
-        return (lhs.window_right < rhs.window_right) && (lhs.window_values.size() < rhs.window_values.size());
+        return (lhs.urng_iterator < rhs.urng_iterator) && (lhs.window_values.size() < rhs.window_values.size());
     }
 
     //!\brief Compare to another window_iterator.
     friend bool operator>(window_iterator const & lhs, window_iterator const & rhs)
     {
-        return (lhs.window_right > rhs.window_right) && (lhs.window_values.size() > rhs.window_values.size());
+        return (lhs.urng_iterator > rhs.urng_iterator) && (lhs.window_values.size() > rhs.window_values.size());
     }
 
     //!\brief Compare to another window_iterator.
     friend bool operator<=(window_iterator const & lhs, window_iterator const & rhs)
     {
-        return (lhs.window_right <= rhs.window_right) && (lhs.window_values.size() <= rhs.window_values.size());
+        return (lhs.urng_iterator <= rhs.urng_iterator) && (lhs.window_values.size() <= rhs.window_values.size());
     }
 
     //!\brief Compare to another window_iterator.
     friend bool operator>=(window_iterator const & lhs, window_iterator const & rhs)
     {
-        return (lhs.window_right >= rhs.window_right) && (lhs.window_values.size() >= rhs.window_values.size());
+        return (lhs.urng_iterator >= rhs.urng_iterator) && (lhs.window_values.size() >= rhs.window_values.size());
     }
     //!\}
 
     //!\brief Pre-increment.
     window_iterator & operator++() noexcept
     {
-        get_minimiser();
+        next_unique_minimiser();
         return *this;
     }
 
@@ -310,7 +312,7 @@ public:
     window_iterator operator++(int) noexcept
     {
         window_iterator tmp{*this};
-        get_minimiser();
+        next_unique_minimiser();
         return tmp;
     }
 
@@ -322,34 +324,37 @@ public:
 
 private:
     //!brief Iterator to last element in range.
-    sentinel_t urange_end;
+    urng_sentinel_t urng_sentinel{};
 
     //!\brief The minimiser value.
     value_type minimiser_value{};
 
     //!\brief Iterator to the rightmost value of one window.
-    it_t window_right;
+    urng_iterator_t urng_iterator{};
 
     //!\brief Stored values per window. It is necessary to store them, because a shift can remove the current minimiser.
     std::deque<value_type> window_values;
 
     //!\brief Increments iterator by 1.
-    void get_minimiser()
+    void next_unique_minimiser()
     {
         // Call next_minimiser until minimiser value changed or end of the underlying range is reached.
         while (!next_minimiser()) {}
     }
 
     //!\brief Calculates minimisers for the first window.
-    void window_first(uint32_t window_values_size)
+    void window_first(size_t const window_values_size)
     {
-        for (uint32_t i = 0; (i < window_values_size - 1) ; i++)
+        if (window_values_size == 0u)
+            return;
+
+        for (size_t i = 0u; i < window_values_size - 1u; ++i)
         {
-            window_values.push_back(*window_right);
-            std::ranges::advance(window_right,  1);
+            window_values.push_back(*urng_iterator);
+            ++urng_iterator;
         }
-        window_values.push_back(*window_right);
-        minimiser_value = *(std::min_element(std::begin(window_values), std::end(window_values)));
+        window_values.push_back(*urng_iterator);
+        minimiser_value = *std::ranges::min_element(window_values);
     }
 
     //!\brief Calculates the next minimiser value.
@@ -357,21 +362,21 @@ private:
     // value that results from the window shifting.
     bool next_minimiser()
     {
-        std::ranges::advance(window_right, 1);
-        if (window_right == urange_end)
+        ++urng_iterator;
+        if (urng_iterator == urng_sentinel)
             return true;
 
-        value_type new_value = *window_right;
-        if (minimiser_value == *(std::begin(window_values)))
-        {
-            window_values.pop_front();
-            window_values.push_back(new_value);
-            minimiser_value = *(std::min_element(std::begin(window_values), std::end(window_values)));
-            return true;
-        }
+        value_type const new_value = *urng_iterator;
+        value_type const first_window_value = window_values.front();
 
         window_values.pop_front();
         window_values.push_back(new_value);
+
+        if (minimiser_value == first_window_value)
+        {
+            minimiser_value = *std::ranges::min_element(window_values);
+            return true;
+        }
 
         if (new_value < minimiser_value)
         {
@@ -385,7 +390,7 @@ private:
 
 //!\brief A deduction guide for the view class template.
 template <std::ranges::viewable_range rng_t>
-minimiser_view(rng_t &&, uint32_t const & window_values_size) -> minimiser_view<std::views::all_t<rng_t>>;
+minimiser_view(rng_t &&, size_t const & window_values_size) -> minimiser_view<std::views::all_t<rng_t>>;
 
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -397,7 +402,7 @@ minimiser_view(rng_t &&, uint32_t const & window_values_size) -> minimiser_view<
 struct minimiser_fn
 {
     //!\brief Store the number of values in one window and return a range adaptor closure object.
-    constexpr auto operator()(uint32_t const & window_values_size) const
+    constexpr auto operator()(size_t const window_values_size) const
     {
         return adaptor_from_functor{*this, window_values_size};
     }
@@ -410,7 +415,7 @@ struct minimiser_fn
      * \returns                         A range of converted values.
      */
     template <std::ranges::range urng_t>
-    constexpr auto operator()(urng_t && urange, uint32_t const & window_values_size) const
+    constexpr auto operator()(urng_t && urange, size_t const window_values_size) const
     {
         static_assert(std::ranges::viewable_range<urng_t>,
             "The range parameter to views::minimiser cannot be a temporary of a non-view range.");
