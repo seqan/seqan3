ab34f564ca7b7ac5431ee3e42587a7bbd977f14c
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2020-06-14 18:12:59 +0200

    [INFRA] add macro benchmarks

---
+++ test/cmake/seqan3_macro_benchmark.cmake
@@ -0,0 +1,68 @@
+# -----------------------------------------------------------------------------------------------------
+# Copyright (c) 2006-2020, Knut Reinert & Freie Universität Berlin
+# Copyright (c) 2016-2020, Knut Reinert & MPI für molekulare Genetik
+# This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+# shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+# -----------------------------------------------------------------------------------------------------
+
+# Adds a macro benchmark target and a test which executes that macro benchmark
+#
+# Example:
+#
+# seqan3_macro_benchmark(
+#     edit_distance_macro_benchmark.cpp
+#     REPETITIONS 3 # optional, default value is 3
+#     META_DATA # optional
+#         foo bar # user-specified data
+#         seqan3_version 3.0.1 # automatically set
+#         seqan3_git_version a1829272d7bafea48580af6eaac2bfbb678c3861 # automatically set
+#         range-v3_version 0.10.0 # automatically set
+#         range-v3_git_version 5daa38e80238a1b50a18c08b4a582178f414404c # automatically set
+#         sdsl_version 3.0.0 # automatically set
+#         sdsl_git_version 9a0d5676fd09fb8b52af214eca2d5809c9a32dbe # automatically set
+# )
+#
+# This produces an executable target with the name `edit_distance_macro_benchmark`.
+#
+# When calling ctest, the executable `edit_distance_macro_benchmark` will be executed and a json file (i.e.
+# `edit_distance_macro_benchmark.json`) with the benchmark results will be generated.
+#
+# Options:
+#
+# * REPETITIONS: [default value: 3]
+#
+#   Repeats the benchmark `REPETITIONS`-many times.
+#
+#   Under the hood executes the target with `--benchmark_repetitions = ${REPETITIONS}`.
+#
+# * META_DATA:
+#
+#   Adds the meta data to the generated json file.
+function (seqan3_macro_benchmark macro_benchmark_cpp)
+    cmake_parse_arguments(
+        "MACRO_BENCHMARK"
+        ""
+        "REPETITIONS"
+        "META_DATA"
+        ${ARGN}
+    )
+
+    if (NOT MACRO_BENCHMARK_REPETITIONS)
+        set (MACRO_BENCHMARK_REPETITIONS "3")
+    endif ()
+
+    file (RELATIVE_PATH macro_benchmark "${CMAKE_SOURCE_DIR}" "${CMAKE_CURRENT_LIST_DIR}/${macro_benchmark_cpp}")
+
+    include (seqan3_test_component)
+    seqan3_test_component (target "${macro_benchmark}" TARGET_NAME)
+    seqan3_test_component (test_name "${macro_benchmark}" TEST_NAME)
+
+    add_executable (${target} ${macro_benchmark_cpp})
+    target_link_libraries (${target} seqan3::test::performance)
+    add_test (NAME "${test_name}"
+              COMMAND ${target}
+                      --benchmark_repetitions=${MACRO_BENCHMARK_REPETITIONS}
+                      --benchmark_min_time=0
+                      --benchmark_out=$<TARGET_FILE_NAME:${target}>.json
+                      --benchmark_out_format=json)
+endfunction ()
--- test/macro_benchmark/example/memcpy_macro_benchmark.cpp
+++ test/macro_benchmark/example/memcpy_macro_benchmark.cpp
@@ -6,19 +6,13 @@
 // -----------------------------------------------------------------------------------------------------
 
 #include <algorithm>
-#include <cstring>
 #include <vector>
 
 #include <benchmark/benchmark.h>
 
 #include <seqan3/test/performance/units.hpp>
 
-static void vector_copy_benchmark(benchmark::State & state) {
-    std::vector<int> x = {15, 13, 12, 10};
-    for (auto _ : state)
-        std::vector<int> copy{x};
-}
-
+// This test allocates 1Gb data with a string and copies it to another memory location.
 static void memcpy_benchmark(benchmark::State & state) {
     size_t size = state.range(0);
     std::vector<char> src(size, '-');
@@ -30,25 +24,7 @@ static void memcpy_benchmark(benchmark::State & state) {
     state.counters["bytes_per_second"] = seqan3::test::bytes_per_second(size);
 }
 
-static void copy_benchmark(benchmark::State & state) {
-    unsigned size = state.range(0);
-    char* src = new char[size];
-    char* dst = new char[size];
-
-    memset(src, '-', size);
-    for (auto _ : state)
-        std::copy_n(src, size, dst);
-
-    state.counters["bytes_per_second"] = seqan3::test::bytes_per_second(size);
-    delete[] src;
-    delete[] dst;
-}
-
 // Register the function as a benchmark
-BENCHMARK(vector_copy_benchmark);
-
-BENCHMARK(memcpy_benchmark)->Arg(8)->Arg(64)->Arg(512);
-BENCHMARK(memcpy_benchmark)->Range(4, 4<<5);
-BENCHMARK(copy_benchmark)->RangeMultiplier(2)->Range(4, 4<<5);
+BENCHMARK(memcpy_benchmark)->Arg(1'000'000'000ull); // 1Gb data
 
 BENCHMARK_MAIN();
