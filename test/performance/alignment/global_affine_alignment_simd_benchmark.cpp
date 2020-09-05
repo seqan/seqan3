1b8039cbc30346a104a319f337fefe0e07854880
rrahn <rene.rahn@fu-berlin.de>
2020-09-05 10:43:07 +0200

    [MISC] Update the simd benchmark to use correct alignment output configuration.

--- test/performance/alignment/global_affine_alignment_simd_benchmark.cpp
+++ test/performance/alignment/global_affine_alignment_simd_benchmark.cpp
@@ -78,13 +78,16 @@ void seqan3_affine_dna4_accelerated(benchmark::State & state, align_configs_t &&
 BENCHMARK_CAPTURE(seqan3_affine_dna4_accelerated,
                   simd_with_score,
                   seqan3::align_cfg::result{seqan3::with_score, seqan3::using_score_type<int16_t>},
+                  seqan3::align_cfg::output_score,
                   seqan3::align_cfg::vectorised)
                         ->UseRealTime()
                         ->DenseRange(deviation_begin, deviation_end, deviation_step);
 
 BENCHMARK_CAPTURE(seqan3_affine_dna4_accelerated,
                   simd_with_end_position,
-                  seqan3::align_cfg::result{seqan3::with_end_positions, seqan3::using_score_type<int16_t>},
+                  seqan3::align_cfg::result{seqan3::with_score, seqan3::using_score_type<int16_t>},
+                  seqan3::align_cfg::output_score,
+                  seqan3::align_cfg::output_end_position,
                   seqan3::align_cfg::vectorised)
                         ->UseRealTime()
                         ->DenseRange(deviation_begin, deviation_end, deviation_step);
@@ -92,6 +95,7 @@ BENCHMARK_CAPTURE(seqan3_affine_dna4_accelerated,
 BENCHMARK_CAPTURE(seqan3_affine_dna4_accelerated,
                   simd_parallel_with_score,
                   seqan3::align_cfg::result{seqan3::with_score, seqan3::using_score_type<int16_t>},
+                  seqan3::align_cfg::output_score,
                   seqan3::align_cfg::vectorised,
                   seqan3::align_cfg::parallel{get_number_of_threads()})
                         ->UseRealTime()
@@ -99,7 +103,9 @@ BENCHMARK_CAPTURE(seqan3_affine_dna4_accelerated,
 
 BENCHMARK_CAPTURE(seqan3_affine_dna4_accelerated,
                   simd_parallel_with_end_position,
-                  seqan3::align_cfg::result{seqan3::with_end_positions, seqan3::using_score_type<int16_t>},
+                  seqan3::align_cfg::result{seqan3::with_score, seqan3::using_score_type<int16_t>},
+                  seqan3::align_cfg::output_score,
+                  seqan3::align_cfg::output_end_position,
                   seqan3::align_cfg::vectorised,
                   seqan3::align_cfg::parallel{get_number_of_threads()})
                         ->UseRealTime()
