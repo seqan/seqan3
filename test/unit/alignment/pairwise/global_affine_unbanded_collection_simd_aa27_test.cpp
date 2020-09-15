0bb310ea649573192555366c74a6c4c6cec97766
rrahn <rene.rahn@fu-berlin.de>
2020-09-15 13:55:16 +0200

    [TEST] More tests for unbanded affine with aa27 alphabet.

--- test/unit/alignment/pairwise/global_affine_unbanded_collection_simd_aa27_test.cpp
+++ test/unit/alignment/pairwise/global_affine_unbanded_collection_simd_aa27_test.cpp
@@ -28,10 +28,36 @@ static auto aa27_all_same = []()
 
     return alignment_fixture_collection{base_fixture.config | seqan3::align_cfg::vectorised{}, data};
 }();
+
+static auto aa27_different_lengths = []()
+{
+
+    auto base_fixture_01 = fixture::global::affine::unbanded::aa27_blosum62_gap_1_open_10;
+    auto base_fixture_02 = fixture::global::affine::unbanded::aa27_blosum62_gap_1_open_10_small;
+    auto base_fixture_03 = fixture::global::affine::unbanded::aa27_blosum62_gap_1_open_10_empty_first;
+    auto base_fixture_04 = fixture::global::affine::unbanded::aa27_blosum62_gap_1_open_10_empty_second;
+    auto base_fixture_05 = fixture::global::affine::unbanded::aa27_blosum62_gap_1_open_10_empty_both;
+
+    using fixture_t = decltype(base_fixture_01);
+
+    std::vector<fixture_t> data{};
+    for (size_t i = 0; i < 25; ++i)
+    {
+        data.push_back(base_fixture_01);
+        data.push_back(base_fixture_02);
+        data.push_back(base_fixture_03);
+        data.push_back(base_fixture_04);
+        data.push_back(base_fixture_05);
+    }
+
+    return alignment_fixture_collection{base_fixture_01.config | seqan3::align_cfg::vectorised{}, data};
+}();
+
 } // namespace seqan3::test::alignment::collection::simd::global::affine::unbanded
 
 using pairwise_collection_simd_global_affine_unbanded_testing_types = ::testing::Types<
-        pairwise_alignment_fixture<&seqan3::test::alignment::collection::simd::global::affine::unbanded::aa27_all_same>
+        pairwise_alignment_fixture<&seqan3::test::alignment::collection::simd::global::affine::unbanded::aa27_all_same>,
+        pairwise_alignment_fixture<&seqan3::test::alignment::collection::simd::global::affine::unbanded::aa27_different_lengths>
     >;
 
 INSTANTIATE_TYPED_TEST_SUITE_P(pairwise_collection_simd_global_affine_unbanded_aa27,
