6d98848a087fae0b2c2e33830cdcf004dc1871df
Jörg Winkler <j.winkler@fu-berlin.de>
2020-11-17 17:11:48 +0100

    [MISC] add deprecation note for type_traits/iterator

---
+++ include/seqan3/core/type_traits/iterator.hpp
@@ -0,0 +1,17 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2020, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2020, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \brief [DEPRECATED] Provides various transformation traits for use on iterators.
+ * \deprecated This header is deprecated and will be removed in SeqAn-3.1.
+ *             Please \#include <seqan3/core/detail/iterator_traits.hpp> instead.
+ */
+
+#include <seqan3/core/detail/iterator_traits.hpp>
+
+SEQAN3_DEPRECATED_HEADER("This header is deprecated and will be removed in SeqAn-3.1. "
+                         "Please #include <seqan3/core/detail/iterator_traits.hpp> instead.")
