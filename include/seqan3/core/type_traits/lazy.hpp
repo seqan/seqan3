05dd69da29b671f66c84af39b9b9a56077d9e0cd
Jörg Winkler <j.winkler@fu-berlin.de>
2020-11-18 15:42:06 +0100

    [MISC] add deprecation note for type_traits/lazy

---
+++ include/seqan3/core/type_traits/lazy.hpp
@@ -0,0 +1,20 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2020, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2020, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \brief [DEPRECATED] Provides lazy template instantiation traits.
+ * \deprecated This header is deprecated and will be removed in SeqAn-3.1.
+ *             Please \#include <seqan3/utility/type_traits/lazy_conditional.hpp>
+ *             or <seqan3/core/detail/is_class_template_declarable.hpp> instead.
+ */
+
+#include <seqan3/core/detail/is_class_template_declarable.hpp>
+#include <seqan3/utility/type_traits/lazy_conditional.hpp>
+
+SEQAN3_DEPRECATED_HEADER("This header is deprecated and will be removed in SeqAn-3.1. "
+                         "Please #include <seqan3/utility/type_traits/lazy_conditional.hpp> "
+                         "or <seqan3/core/detail/is_class_template_declarable.hpp> instead.")
