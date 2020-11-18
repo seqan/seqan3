7feaa77190f2f134d8cbaa210523e7e55d469ae9
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2020-11-18 22:02:33 +0100

    [MISC] add deprecation header for simd module

---
+++ include/seqan3/core/simd/detail/builtin_simd.hpp
@@ -0,0 +1,20 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2020, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2020, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \author [DEPRECATED] Marcel Ehrhardt <marcel.ehrhardt AT fu-berlin.de>
+ * \brief Provides seqan3::detail::builtin_simd, seqan3::detail::is_builtin_simd
+ *        and seqan3::simd::simd_traits<builtin_simd_t>.
+ * \deprecated This header will be removed in SeqAn-3.1.0. Please use seqan3/utility/simd/detail/builtin_simd.hpp instead.
+ */
+
+#pragma once
+
+#include <seqan3/utility/simd/detail/builtin_simd.hpp>
+
+SEQAN3_DEPRECATED_HEADER(
+    "This header is deprecated and will be removed in SeqAn-3.1.0; Please #include <seqan3/utility/simd/detail/builtin_simd.hpp> instead.")
---
+++ include/seqan3/core/simd/simd.hpp
@@ -0,0 +1,19 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2020, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2020, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \brief [DEPRECATED] Provides seqan3::simd::simd_type
+ * \author Marcel Ehrhardt <marcel.ehrhardt AT fu-berlin.de>
+ * \deprecated This header will be removed in SeqAn-3.1.0. Please use seqan3/utility/simd/simd.hpp instead.
+ */
+
+#pragma once
+
+#include <seqan3/utility/simd/simd.hpp>
+
+SEQAN3_DEPRECATED_HEADER(
+   "This header is deprecated and will be removed in SeqAn-3.1.0; Please #include <seqan3/utility/simd/simd.hpp> instead.")
