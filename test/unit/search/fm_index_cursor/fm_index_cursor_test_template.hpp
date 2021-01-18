7da350529f975ecbb98cfcf35751a630bfd7124b
Evelin Aasna <evelin.aasna@fu-berlin.de>
2021-01-18 09:13:35 +0100

    [FIX] removing unused includes

--- test/unit/search/fm_index_cursor/fm_index_cursor_test_template.hpp
+++ test/unit/search/fm_index_cursor/fm_index_cursor_test_template.hpp
@@ -11,7 +11,6 @@
 #include <type_traits>
 
 #include <seqan3/range/views/slice.hpp>
-#include <seqan3/search/fm_index/fm_index_cursor.hpp>
 #include <seqan3/test/cereal.hpp>
 #include <seqan3/test/expect_range_eq.hpp>
 
