56f168c8e649d10fa127d2dc8d3c23f755d7d23e
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2021-02-25 00:57:11 +0100

    [MISC] introduce [rd]na5::char_to_rank and dna5::rank_to_char function

--- include/seqan3/alphabet/nucleotide/rna5.hpp
+++ include/seqan3/alphabet/nucleotide/rna5.hpp
@@ -78,9 +78,7 @@ public:
     }
     //!\}
 
-protected:
-    //!\privatesection
-
+private:
     //!\copydoc seqan3::dna4::rank_to_char
     static constexpr char_type rank_to_char[alphabet_size]
     {
@@ -91,11 +89,14 @@ protected:
         'U'
     };
 
-    //!\copydoc seqan3::dna4::char_to_rank
-    static constexpr std::array<rank_type, 256> char_to_rank = dna5::char_to_rank;
-
     //!\copydoc seqan3::dna4::complement_table
     static const std::array<rna5, alphabet_size> complement_table;
+
+    //!\copydoc seqan3::dna4::char_to_rank
+    static constexpr rank_type char_to_rank(char_type const chr)
+    {
+        return dna5::char_to_rank(chr);
+    }
 };
 
 // ------------------------------------------------------------------
