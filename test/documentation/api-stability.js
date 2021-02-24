671e5462144caf6fd8c5c20976b956ff92d834d2
Enrico Seiler <eseiler@users.noreply.github.com>
2021-02-24 15:56:32 +0100

    [DOC] Add API badge to detailed description (#2384)
    * [DOC] Add API badge to detailed description
    
    * Apply suggestions from code review
    
    Co-authored-by: Marcel <marehr@users.noreply.github.com>

--- test/documentation/api-stability.js
+++ test/documentation/api-stability.js
@@ -2,30 +2,52 @@
 
 $(document).ready(function()
 {
-    $('div.memitem').each(function(index)
+    var add_mlabel = function()
     {
-        var is_no_api = $(this).find('dl.no-api').length;
-        var is_experimental_api = $(this).find('dl.experimental-api').length;
-        var is_stable_api = $(this).find('dl.stable-api').length;
+        var memname_html = $(this).find('table.memname').prop('outerHTML');
+        var new_html = "<table class=\"mlabels\"> <tbody> <tr> <td class=\"mlabels-left\"> " +
+                        memname_html +
+                        " </td> <td class=\"mlabels-right\"> <span class=\"mlabels\"\> " +
+                        "</td> </tr> </tbody> </table>";
+
+        $(this).find('div.memproto').html(new_html);
+    }
+
+    var add_labels = function(api_type, entry_type)
+    {
+        if (entry_type == "member")
+            $(this).find('span.mlabels').prepend('<span class="mlabel ' + api_type + '">' + api_type + '</span>');
+        else if (entry_type == "header")
+            $('h2.groupheader:contains("Detailed Description")').append('<span class="mlabel ' + api_type + ' header">' + api_type + '</span>');
+    }
+
+    var select_and_add_label = function(entry_obj, entry_type)
+    {
+        var is_no_api = $(entry_obj).find('dl.no-api').length;
+        var is_experimental_api = $(entry_obj).find('dl.experimental-api').length;
+        var is_stable_api = $(entry_obj).find('dl.stable-api').length;
         var has_mlabels = $(this).find('span.mlabels').length;
 
         if (!has_mlabels)
-        {
-            var memname_html = $(this).find('table.memname').prop('outerHTML');
-            var new_html = "<table class=\"mlabels\"> <tbody> <tr> <td class=\"mlabels-left\"> " +
-                           memname_html +
-                           " </td> <td class=\"mlabels-right\"> <span class=\"mlabels\"\> " +
-                           "</td> </tr> </tbody> </table>";
-            $(this).find('div.memproto').html(new_html);
-        }
+            add_mlabel.call(this);
 
         if (is_no_api)
-            $(this).find('span.mlabels').prepend('<span class="mlabel no-api">no-api</span>');
+            add_labels.call(this, "no-api", entry_type);
         else if (is_experimental_api)
-            $(this).find('span.mlabels').prepend('<span class="mlabel experimental-api">experimental-api</span>');
+            add_labels.call(this, "experimental-api", entry_type);
         else if (is_stable_api)
-            $(this).find('span.mlabels').prepend('<span class="mlabel stable-api">stable-api</span>');
+            add_labels.call(this, "stable-api", entry_type);
         else
-            $(this).find('span.mlabels').prepend('<span class="mlabel no-api">no-api</span>');
+            add_labels.call(this, "no-api", entry_type);
+    }
+
+    // Badges for members
+    $('div.memitem').each(function(index)
+    {
+        select_and_add_label.call(this, this, "member");
     });
+
+    // Badge for "Detailed Description"
+    select_and_add_label($('div.textblock'), "header");
+
 });
