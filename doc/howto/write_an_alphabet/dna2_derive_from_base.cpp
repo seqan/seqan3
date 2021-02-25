adfa0c8feed5cdc784b2b0e5bcee7406f903972f
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2021-02-25 11:06:15 +0100

    [MISC] introduce snippet::dna2::{rank_to_char, char_to_rank} function

--- doc/howto/write_an_alphabet/dna2_derive_from_base.cpp
+++ doc/howto/write_an_alphabet/dna2_derive_from_base.cpp
@@ -9,15 +9,40 @@
 #include <array>                                         // std::array
 #include <seqan3/alphabet/alphabet_base.hpp>             // alphabet_base
 #include <seqan3/alphabet/concept.hpp>                   // alphabet concept checks
+#include <seqan3/utility/char_operations/transform.hpp>  // seqan3::to_lower
 
 // derive from alphabet_base
 struct dna2 : public seqan3::alphabet_base<dna2, 2>
 {
 private:
-    // map 0 => 'S' and 1 => 'W'
-    static constexpr char_type rank_to_char[alphabet_size] {'S', 'W'};
+    // make the base class a friend so it can access the tables:
+    friend seqan3::alphabet_base<dna2, 2>;
 
-    static constexpr std::array<rank_type, 256> char_to_rank
+    // map 0 => 'S' and 1 => 'W'
+    static constexpr char_type rank_to_char(rank_type const rank)
+    {
+        // via a lookup table
+        return rank_to_char_table[rank];
+        // or via an arithmetic expression
+        return rank == 1 ? 'W' : 'S';
+    }
+
+    static constexpr rank_type char_to_rank(char_type const chr)
+    {
+        // via a lookup table
+        using index_t = std::make_unsigned_t<char_type>;
+        return char_to_rank_table[static_cast<index_t>(chr)];
+        // or via an arithmetic expression
+        return seqan3::to_lower(chr) == 'w' ? 1 : 0;
+    }
+
+private:
+    // === lookup-table implementation detail ===
+
+    // map 0 => 'S' and 1 => 'W'
+    static constexpr char_type rank_to_char_table[alphabet_size] {'S', 'W'};
+
+    static constexpr std::array<rank_type, 256> char_to_rank_table
     {
         // initialise with an immediately evaluated lambda expression:
         [] () constexpr
@@ -28,9 +53,6 @@ private:
             return ret;
         } ()
     };
-
-    // make the base class a friend so it can access the tables:
-    friend seqan3::alphabet_base<dna2, 2>;
 };
 
 // check the concepts
