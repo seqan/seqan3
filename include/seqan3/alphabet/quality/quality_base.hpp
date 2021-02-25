fdf35e500a9a0c57ba7ff7fab48ba52e82b29b24
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2021-02-25 11:09:42 +0100

    [MISC] introduce quality_base::{rank_to_char, char_to_rank} function

--- include/seqan3/alphabet/quality/quality_base.hpp
+++ include/seqan3/alphabet/quality/quality_base.hpp
@@ -39,6 +39,9 @@ private:
     //!\brief The base type.
     using base_t = alphabet_base<derived_type, size, char>;
 
+    //!\brief Befriend the base type so it can access #char_to_rank and #rank_to_char.
+    friend base_t;
+
     /*!\name Constructors, destructor and assignment
      * \{
      */
@@ -119,7 +122,7 @@ public:
     }
     //!\}
 
-protected:
+private:
     //!\brief Phred to rank conversion table.
     static std::array<rank_type, 256> constexpr phred_to_rank
     {
@@ -140,27 +143,6 @@ protected:
         }()
     };
 
-    //!\brief Char to rank conversion table.
-    static std::array<rank_type, 256> constexpr char_to_rank
-    {
-        [] () constexpr
-        {
-            std::array<rank_type, 256> ret{};
-
-            for (int64_t i = std::numeric_limits<char_type>::lowest(); i <= std::numeric_limits<char_type>::max(); ++i)
-            {
-                if (i < derived_type::offset_char)                     // map too-small to smallest possible
-                    ret[static_cast<rank_type>(i)] = 0;
-                else if (i >= derived_type::offset_char + alphabet_size)  // map too-large to highest possible
-                    ret[static_cast<rank_type>(i)] = alphabet_size - 1;
-                else                                                   // map valid range to identity
-                    ret[static_cast<rank_type>(i)] = i - derived_type::offset_char;
-            }
-
-            return ret;
-        }()
-    };
-
     //!\brief Rank to phred conversion table.
     static std::array<phred_type, alphabet_size> constexpr rank_to_phred
     {
@@ -175,19 +157,18 @@ protected:
         }()
     };
 
-    //!\brief Rank to char conversion table.
-    static std::array<char_type, alphabet_size> constexpr rank_to_char
+    //!\copydoc seqan3::dna4::char_to_rank
+    static constexpr rank_type char_to_rank(char_type const chr)
     {
-        [] () constexpr
-        {
-            std::array<char_type, alphabet_size> ret{};
+        int64_t difference = static_cast<int64_t>(chr) - static_cast<int64_t>(derived_type::offset_char);
+        return std::clamp<int64_t>(difference, 0, alphabet_size - 1);
+    }
 
-            for (size_t i = 0; i < alphabet_size; ++i)
-                ret[i] = i + derived_type::offset_char;
-
-            return ret;
-        }()
-    };
+    //!\copydoc seqan3::dna4::rank_to_char
+    static constexpr char_type rank_to_char(rank_type const rank)
+    {
+        return rank + derived_type::offset_char;
+    }
 };
 
 } // namespace seqan3
