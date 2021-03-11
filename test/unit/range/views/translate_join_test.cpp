dcd1436c07b5d8742151fba4103785ee249f8c1c
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2021-03-11 21:30:45 +0100

    [TEST] remove seqan3::views::to in translate_join_test.cpp

--- test/unit/range/views/translate_join_test.cpp
+++ test/unit/range/views/translate_join_test.cpp
@@ -23,7 +23,6 @@
 #include <seqan3/range/concept.hpp>
 #include <seqan3/range/views/char_to.hpp>
 #include <seqan3/range/views/complement.hpp>
-#include <seqan3/range/views/to.hpp>
 #include <seqan3/range/views/translate_join.hpp>
 #include <seqan3/test/expect_range_eq.hpp>
 
@@ -73,116 +72,128 @@ TYPED_TEST_SUITE(nucleotide, nucleotide_types, );
 
 TYPED_TEST(nucleotide, view_translate)
 {
-    std::string const in1{"ACGTACGTACGTA"};
-    std::string const in2{"TCGAGAGCTTTAGC"};
-    std::vector<std::vector<TypeParam> > vec;
-    vec.resize(2);
-    vec[0] = in1 | seqan3::views::char_to<TypeParam> | seqan3::views::to<std::vector>;
-    vec[1] = in2 | seqan3::views::char_to<TypeParam> | seqan3::views::to<std::vector>;
-
-    std::vector<std::vector<seqan3::aa27> > cmp1{{"TYVR"_aa27}, {"SRAL"_aa27}};
-    std::vector<std::vector<seqan3::aa27> > cmp2{{"TYVR"_aa27}, {"YVRT"_aa27}, {"SRAL"_aa27}, {"AKAL"_aa27}};
-    std::vector<std::vector<seqan3::aa27> > cmp3{{"TYVR"_aa27}, {"RTYV"_aa27}, {"VRT"_aa27}, {"SRAL"_aa27},
-                                                 {"REL*"_aa27}, {"ESFS"_aa27}};
-    std::vector<std::vector<seqan3::aa27> > cmp4{{"TYVR"_aa27}, {"RTYV"_aa27}, {"VRT"_aa27}, {"YVRT"_aa27},
-                                                 {"TYVR"_aa27}, {"RTY"_aa27}, {"SRAL"_aa27}, {"REL*"_aa27},
-                                                 {"ESFS"_aa27}, {"AKAL"_aa27}, {"LKLS"_aa27}, {"*SSR"_aa27}};
-    std::vector<std::vector<seqan3::aa27> > cmp5{{"TYVR"_aa27}, {"VRT"_aa27}, {"SRAL"_aa27}, {"ESFS"_aa27}};
-    std::vector<std::vector<seqan3::aa27> > cmp6{{"CMHA"_aa27}, {"MHAC"_aa27}, {"SSRN"_aa27}, {"RFRE"_aa27}};
-    std::vector<std::vector<seqan3::aa27> > cmp7{{"CMHA"_aa27}};
+    std::vector<std::string> sequences{"ACGTACGTACGTA", "TCGAGAGCTTTAGC"};
+    auto vec = sequences | seqan3::views::char_to<TypeParam>;
 
     // default parameter translation_frames
     auto v1 = vec | seqan3::views::translate_join;
-    // == [[T,Y,V,R],[R,T,Y,V],[V,R,T],[Y,V,R,T],[T,Y,V,R],[R,T,Y],[S,R,A,L],[R,E,L,*],[E,S,F,S],[A,K,A,L],[L,K,L,S],[*,S,S,R]]
-    EXPECT_EQ(v1.size(), cmp4.size());
-    for (unsigned i = 0; i < v1.size(); i++)
-        EXPECT_RANGE_EQ(v1[i], cmp4[i]);
+    EXPECT_EQ(v1.size(), 12u);
+    EXPECT_RANGE_EQ("TYVR"_aa27, v1[0]);
+    EXPECT_RANGE_EQ("RTYV"_aa27, v1[1]);
+    EXPECT_RANGE_EQ("VRT"_aa27, v1[2]);
+    EXPECT_RANGE_EQ("YVRT"_aa27, v1[3]);
+    EXPECT_RANGE_EQ("TYVR"_aa27, v1[4]);
+    EXPECT_RANGE_EQ("RTY"_aa27, v1[5]);
+    EXPECT_RANGE_EQ("SRAL"_aa27, v1[6]);
+    EXPECT_RANGE_EQ("REL*"_aa27, v1[7]);
+    EXPECT_RANGE_EQ("ESFS"_aa27, v1[8]);
+    EXPECT_RANGE_EQ("AKAL"_aa27, v1[9]);
+    EXPECT_RANGE_EQ("LKLS"_aa27, v1[10]);
+    EXPECT_RANGE_EQ("*SSR"_aa27, v1[11]);
 
     // default parameter translation_frames
     auto v2 = vec | seqan3::views::translate_join();
-    // == [[T,Y,V,R],[R,T,Y,V],[V,R,T],[Y,V,R,T],[T,Y,V,R],[R,T,Y],[S,R,A,L],[R,E,L,*],[E,S,F,S],[A,K,A,L],[L,K,L,S],[*,S,S,R]]
-    EXPECT_EQ(v2.size(), cmp4.size());
-    for (unsigned i = 0; i < v2.size(); i++)
-        EXPECT_RANGE_EQ(v2[i], cmp4[i]);
+    EXPECT_EQ(v2.size(), 12u);
+    EXPECT_RANGE_EQ("TYVR"_aa27, v2[0]);
+    EXPECT_RANGE_EQ("RTYV"_aa27, v2[1]);
+    EXPECT_RANGE_EQ("VRT"_aa27, v2[2]);
+    EXPECT_RANGE_EQ("YVRT"_aa27, v2[3]);
+    EXPECT_RANGE_EQ("TYVR"_aa27, v2[4]);
+    EXPECT_RANGE_EQ("RTY"_aa27, v2[5]);
+    EXPECT_RANGE_EQ("SRAL"_aa27, v2[6]);
+    EXPECT_RANGE_EQ("REL*"_aa27, v2[7]);
+    EXPECT_RANGE_EQ("ESFS"_aa27, v2[8]);
+    EXPECT_RANGE_EQ("AKAL"_aa27, v2[9]);
+    EXPECT_RANGE_EQ("LKLS"_aa27, v2[10]);
+    EXPECT_RANGE_EQ("*SSR"_aa27, v2[11]);
 
     // single frame translation
     auto v3 = vec | seqan3::views::translate_join(seqan3::translation_frames::FWD_FRAME_0);
-    // == [[T,Y,V,R],[S,R,A,L]]
-    EXPECT_EQ(v3.size(), cmp1.size());
-    for (unsigned i = 0; i < v3.size(); i++)
-        EXPECT_RANGE_EQ(v3[i], cmp1[i]);
+    EXPECT_EQ(v3.size(), 2u);
+    EXPECT_RANGE_EQ(v3[0], "TYVR"_aa27);
+    EXPECT_RANGE_EQ(v3[1], "SRAL"_aa27);
 
     // reverse translation
     auto v4 = vec | seqan3::views::translate_join(seqan3::translation_frames::FWD_REV_0);
-    // == [[T,Y,V,R],Y,V,R,T],[S,R,A,L],[A,K,A,L]]
-    EXPECT_EQ(v4.size(), cmp2.size());
-    for (unsigned i = 0; i < v4.size(); i++)
-        EXPECT_RANGE_EQ(v4[i], cmp2[i]);
+    EXPECT_EQ(v4.size(), 4u);
+    EXPECT_RANGE_EQ(v4[0], "TYVR"_aa27);
+    EXPECT_RANGE_EQ(v4[1], "YVRT"_aa27);
+    EXPECT_RANGE_EQ(v4[2], "SRAL"_aa27);
+    EXPECT_RANGE_EQ(v4[3], "AKAL"_aa27);
 
     // forward frames translation
     auto v5 = vec | seqan3::views::translate_join(seqan3::translation_frames::FWD);
-    // == [[T,Y,V,R],[R,T,Y,V],[V,R,T],[S,R,A,L],[R,E,L,*],[E,S,F,S]]
-    EXPECT_EQ(v5.size(), cmp3.size());
-    for (unsigned i = 0; i < v5.size(); i++)
-        EXPECT_RANGE_EQ(v5[i], cmp3[i]);
+    EXPECT_EQ(v5.size(), 6u);
+    EXPECT_RANGE_EQ(v5[0], "TYVR"_aa27);
+    EXPECT_RANGE_EQ(v5[1], "RTYV"_aa27);
+    EXPECT_RANGE_EQ(v5[2], "VRT"_aa27);
+    EXPECT_RANGE_EQ(v5[3], "SRAL"_aa27);
+    EXPECT_RANGE_EQ(v5[4], "REL*"_aa27);
+    EXPECT_RANGE_EQ(v5[5], "ESFS"_aa27);
 
     // six frame translation
     auto v6 = vec | seqan3::views::translate_join(seqan3::translation_frames::SIX_FRAME);
-    // == [[T,Y,V,R],[R,T,Y,V],[V,R,T],[Y,V,R,T],[T,Y,V,R],[R,T,Y],[S,R,A,L],[R,E,L,*],[E,S,F,S],[A,K,A,L],[L,K,L,S],[*,S,S,R]]
-    EXPECT_EQ(v6.size(), cmp4.size());
-    for (unsigned i = 0; i < v6.size(); i++)
-        EXPECT_RANGE_EQ(v6[i], cmp4[i]);
+    EXPECT_EQ(v6.size(), 12u);
+    EXPECT_RANGE_EQ("TYVR"_aa27, v6[0]);
+    EXPECT_RANGE_EQ("RTYV"_aa27, v6[1]);
+    EXPECT_RANGE_EQ("VRT"_aa27, v6[2]);
+    EXPECT_RANGE_EQ("YVRT"_aa27, v6[3]);
+    EXPECT_RANGE_EQ("TYVR"_aa27, v6[4]);
+    EXPECT_RANGE_EQ("RTY"_aa27, v6[5]);
+    EXPECT_RANGE_EQ("SRAL"_aa27, v6[6]);
+    EXPECT_RANGE_EQ("REL*"_aa27, v6[7]);
+    EXPECT_RANGE_EQ("ESFS"_aa27, v6[8]);
+    EXPECT_RANGE_EQ("AKAL"_aa27, v6[9]);
+    EXPECT_RANGE_EQ("LKLS"_aa27, v6[10]);
+    EXPECT_RANGE_EQ("*SSR"_aa27, v6[11]);
 
     // user-defined frame combination
     auto v7 = vec
             | seqan3::views::translate_join(seqan3::translation_frames::FWD_FRAME_0
             | seqan3::translation_frames::FWD_FRAME_2);
-    // == [[T,Y,V,R],[V,R,T],[S,R,A,L],[E,S,F,S]]
-    EXPECT_EQ(v7.size(), cmp5.size());
-    for (unsigned i = 0; i < v7.size(); i++)
-        EXPECT_RANGE_EQ(v7[i], cmp5[i]);
+    EXPECT_EQ(v7.size(), 4u);
+    EXPECT_RANGE_EQ(v7[0], "TYVR"_aa27);
+    EXPECT_RANGE_EQ(v7[1], "VRT"_aa27);
+    EXPECT_RANGE_EQ(v7[2], "SRAL"_aa27);
+    EXPECT_RANGE_EQ(v7[3], "ESFS"_aa27);
 
     // function syntax
     auto v8 = seqan3::views::translate_join(vec, seqan3::translation_frames::FWD_REV_0);
-    // == [[T,Y,V,R],[R,T,Y,V],[V,R,T],[Y,V,R,T],[T,Y,V,R],[R,T,Y],[S,R,A,L], [R,E,L,*], [E,S,F,S], [A,K,A,L], [L,K,L,S], [*,S,S,R]]
-    EXPECT_EQ(v8.size(), cmp2.size());
-    for (unsigned i = 0; i < v8.size(); i++)
-        EXPECT_RANGE_EQ(v8[i], cmp2[i]);
+    EXPECT_EQ(v8.size(), 4u);
+    EXPECT_RANGE_EQ(v8[0], "TYVR"_aa27);
+    EXPECT_RANGE_EQ(v8[1], "YVRT"_aa27);
+    EXPECT_RANGE_EQ(v8[2], "SRAL"_aa27);
+    EXPECT_RANGE_EQ(v8[3], "AKAL"_aa27);
 
     // combinability
     auto v9 = vec | seqan3::views::complement | seqan3::views::translate_join(seqan3::translation_frames::FWD_REV_0);
-    // == [[C,M,H,A],[M,H,A,C],[S,S,R,N],[R,F,R,E]]
-    EXPECT_EQ(v9.size(), cmp6.size());
-    for (unsigned i = 0; i < v9.size(); i++)
-        EXPECT_RANGE_EQ(v9[i], cmp6[i]);
+    EXPECT_EQ(v9.size(), 4u);
+    EXPECT_RANGE_EQ(v9[0], "CMHA"_aa27);
+    EXPECT_RANGE_EQ(v9[1], "MHAC"_aa27);
+    EXPECT_RANGE_EQ(v9[2], "SSRN"_aa27);
+    EXPECT_RANGE_EQ(v9[3], "RFRE"_aa27);
 
     // combinability
     auto v10 = vec
              | seqan3::views::complement
              | seqan3::views::translate_join(seqan3::translation_frames::FWD_REV_0)
              | std::views::take(1);
-    // == [C,M,H,A]
-    EXPECT_EQ(v10.size(), cmp7.size());
-    for (unsigned i = 0; i < v10.size(); i++)
-        EXPECT_RANGE_EQ(v10[i], cmp7[i]);
+    EXPECT_EQ(v10.size(), 1u);
+    EXPECT_RANGE_EQ(v10[0], "CMHA"_aa27);
 
     // combinability and function syntax
     auto v11 = seqan3::detail::view_translate_join(seqan3::views::complement(vec),
                                                    seqan3::translation_frames::FWD_REV_0);
-    // == [[C,M,H,A],[M,H,A,C],[S,S,R,N],[R,F,R,E]]
-    EXPECT_EQ(v11.size(), cmp6.size());
-    for (unsigned i = 0; i < v11.size(); i++)
-        EXPECT_RANGE_EQ(v11[i], cmp6[i]);
+    EXPECT_EQ(v11.size(), 4u);
+    EXPECT_RANGE_EQ(v11[0], "CMHA"_aa27);
+    EXPECT_RANGE_EQ(v11[1], "MHAC"_aa27);
+    EXPECT_RANGE_EQ(v11[2], "SSRN"_aa27);
+    EXPECT_RANGE_EQ(v11[3], "RFRE"_aa27);
 }
 
 TYPED_TEST(nucleotide, view_translate_concepts)
 {
-    std::string const in1{"ACGTACGTACGTA"};
-    std::string const in2{"TCGAGAGCTTTAGC"};
-    std::vector<std::vector<TypeParam> > vec;
-    vec.resize(2);
-    vec[0] = in1 | seqan3::views::char_to<TypeParam> | seqan3::views::to<std::vector>;
-    vec[1] = in2 | seqan3::views::char_to<TypeParam> | seqan3::views::to<std::vector>;
+    std::vector<std::vector<TypeParam>> vec{};
 
     EXPECT_TRUE(std::ranges::forward_range<decltype(vec)>);
     EXPECT_TRUE(std::ranges::random_access_range<decltype(vec)>);
