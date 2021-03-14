4e92acd0c84d5afcd4f218769b7a7ccf5f014111
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2021-03-14 13:08:09 +0100

    [FEATURE] new way to define cpos via SEQAN3_CPO_OVERLOAD

--- include/seqan3/core/detail/customisation_point.hpp
+++ include/seqan3/core/detail/customisation_point.hpp
@@ -7,7 +7,7 @@
 
 /*!\file
  * \author Hannes Hauswedell <hannes.hauswedell AT fu-berlin.de>
- * \brief Helper utilities for defining customisation point objects.
+ * \brief Helper utilities for defining customisation point objects (CPOs).
  */
 
 #pragma once
@@ -42,6 +42,8 @@ struct priority_tag<0>
 // ============================================================================
 
 //!\brief A macro that helps defining the overload set of a customisation point.
+//!\deprecated Please use #SEQAN3_CPO_OVERLOAD to define seqan3::detail::customisation_point_object instead.
+#ifdef SEQAN3_DEPRECATED_310
 #define SEQAN3_CPO_IMPL(PRIO, TERM)                                                                                  \
 /*!\brief A customisation point overload.*/                                                                          \
 template <typename t, typename ...arg_ts>                                                                            \
@@ -54,3 +56,122 @@ static constexpr decltype(auto) impl(seqan3::detail::priority_tag<PRIO>,        
 {                                                                                                                    \
     return TERM;                                                                                                     \
 }
+#endif // SEQAN3_DEPRECATED_310
+
+#if SEQAN3_DOXYGEN_ONLY(1)0
+/*!\brief A macro helper for #SEQAN3_CPO_OVERLOAD.
+ * \details
+ * Please note that in order to allow a semicolon at the end when using this macro, i.e.
+ *
+ * ```cpp
+ *  SEQAN3_CPO_OVERLOAD_BODY
+ *  (
+ *      ...
+ *  );
+ * ```
+ *
+ * we need some expression at the end of the macro that can have a semicolon appended.
+ * We chose `static_assert(true)` for that reason.
+ */
+#define SEQAN3_CPO_OVERLOAD_BODY(...) noexcept(auto) { return __VA_ARGS__; }
+#else // ^^^ (simplified) doxygen version / normal definition vvv
+#define SEQAN3_CPO_OVERLOAD_BODY(...) \
+        noexcept(noexcept(__VA_ARGS__)) \
+        -> decltype(__VA_ARGS__) \
+    { return __VA_ARGS__; } \
+    static_assert(true)
+#endif
+
+/*!\brief A macro that helps to define a seqan3::detail::customisation_point_object.
+ * \details
+ * Expands to a function definition with the name `cpo_overload`.
+ *
+ * It puts the given expression via #SEQAN3_CPO_OVERLOAD_BODY as a single return statement in the function body, the
+ * noexcept declaration and requires declaration.
+ *
+ * \snippet test/snippet/core/detail/customisation_point.cpp SEQAN3_CPO_OVERLOAD
+ *
+ * expands to something similar to
+ *
+ * ```cpp
+ * template <typename range_t>
+ *     requires true // further constraints
+ * static constexpr auto cpo_overload(seqan3::detail::priority_tag<1>, range_t && range)
+ *
+ *     noexcept(noexcept(std::forward<range_t>(range).begin()))
+ *
+ *     requires requires()
+ *     {
+ *         {std::forward<range_t>(range).begin()};
+ *     }
+ *
+ * {
+ *     return std::forward<range_t>(range).begin();
+ * }
+ * ```
+ */
+#define SEQAN3_CPO_OVERLOAD(...) \
+    cpo_overload(__VA_ARGS__) \
+    SEQAN3_CPO_OVERLOAD_BODY
+
+namespace seqan3::detail
+{
+/*!\brief A CRTP base-class that defines a customisation_point_object (CPO).
+ * \tparam derived_t CRTP derived type.
+ * \tparam max_priority The (highest) start priority the CPO overloads will be checked from.
+ *
+ * You can define a CPO similar to `decltype(std::ranges::begin)` that accepts `range.begin()` (Member) or
+ * `begin(range)` (ADL):
+ *
+ * \snippet test/snippet/core/detail/customisation_point.cpp CPO Definition
+ *
+ * and instantiate it like std::ranges::begin:
+ *
+ * \snippet test/snippet/core/detail/customisation_point.cpp CPO Instance
+ *
+ * Any type that has a member function with the name "begin" will now work (a real definition of std::ranges::begin
+ * would, of course, further constraint the overload):
+ *
+ * \snippet test/snippet/core/detail/customisation_point.cpp CPO Member overload
+ *
+ * Furthermore, any unqualified function call "begin":
+ *
+ * \snippet test/snippet/core/detail/customisation_point.cpp CPO ADL overload
+ *
+ * seqan3::detail::customisation_point_object is designed to be SFINAE friendly:
+ *
+ * \snippet test/snippet/core/detail/customisation_point.cpp CPO SFINAE friendly
+ *
+ */
+template <typename derived_t, unsigned max_priority>
+struct customisation_point_object
+{
+private:
+    //!\brief Allow `derived_t` to inherit the constructors of this CRTP-class.
+    friend derived_t;
+
+    constexpr customisation_point_object() = default; //!< Defaulted.
+    constexpr customisation_point_object(customisation_point_object &&) = default; //!< Defaulted.
+    constexpr customisation_point_object(customisation_point_object const &) = default; //!< Defaulted.
+    constexpr customisation_point_object & operator=(customisation_point_object &&) = default; //!< Defaulted.
+    constexpr customisation_point_object & operator=(customisation_point_object const &) = default; //!< Defaulted.
+
+public:
+    /*!\brief SFINAE-friendly call-operator of this seqan3::detail::customisation_point_object instance.
+     * \details
+     * This operator implements the actual CPO overload resolution. Overload resolution will try each base class of
+     * seqan3::detail::priority_tag\<max_priority>, seqan3::detail::priority_tag\<max_priority - 1>, ...,
+     * seqan3::detail::priority_tag<0> as first argument to `derived_t::cpo_overload`. That means a high priority in
+     * seqan3::detail::priority_tag will be evaluated first and one can define an order which overload should be
+     * prioritised if multiple overloads match.
+     *
+     * It perfectly forwards the result and noexcept-property of the `derived_t::cpo_overload`.
+     */
+    template <typename ...args_t, typename derived_type = derived_t /*circumvent incomplete types*/>
+    constexpr auto operator()(args_t && ...args) const
+    SEQAN3_CPO_OVERLOAD_BODY
+    (
+        /*return*/ derived_type::cpo_overload(priority_tag<max_priority>{}, std::forward<args_t>(args)...) /*;*/
+    );
+};
+} // namespace seqan3::detail
---
+++ test/snippet/core/detail/customisation_point.cpp
@@ -0,0 +1,92 @@
+#include <seqan3/std/concepts>
+#include <vector>
+
+#include <seqan3/core/detail/customisation_point.hpp>
+
+//! [CPO Definition]
+namespace seqan3::detail::adl_only
+{
+// Poison-pill overload to prevent non-ADL forms of unqualified lookup.
+template <typename ...args_t>
+void begin(args_t ...) = delete;
+
+struct begin_cpo : public detail::customisation_point_object<begin_cpo, 1>
+{
+    using base_t = detail::customisation_point_object<begin_cpo, 1>;
+    // Only this class is allowed to import the constructors from base_t. (CRTP safety idiom)
+    using base_t::base_t;
+
+    // range.begin(), member access
+//! [SEQAN3_CPO_OVERLOAD]
+    template <typename range_t>
+        requires true // further constraints
+    static constexpr auto SEQAN3_CPO_OVERLOAD(seqan3::detail::priority_tag<1>, range_t && range)
+    (
+        /*return*/ std::forward<range_t>(range).begin() /*;*/
+    );
+//! [SEQAN3_CPO_OVERLOAD]
+
+    // begin(range), ADL access
+    template <typename range_t>
+    static constexpr auto SEQAN3_CPO_OVERLOAD(seqan3::detail::priority_tag<0>, range_t && range)
+    (
+        /*return*/ begin(std::forward<range_t>(range)) /*;*/
+    );
+};
+
+} // namespace seqan3::detail::adl_only
+//! [CPO Definition]
+
+//! [CPO Instance]
+namespace seqan3
+{
+// CPO is a normal function object that can be called via seqan3::begin(...)
+inline constexpr auto begin = detail::adl_only::begin_cpo{};
+} // namespace seqan3
+//! [CPO Instance]
+
+
+//! [ADL Definition]
+namespace other_library
+{
+struct foo
+{
+    friend int begin(foo const &) // ADL begin, as friend
+    {
+        return 0;
+    }
+};
+} // namespace other_library
+//! [ADL Definition]
+
+//! [CPO Member overload]
+// seqan3::begin CPO that will call the "begin" member function
+std::vector<int> vec{};
+
+static_assert(std::same_as<decltype(seqan3::begin(vec)), decltype(vec.begin())>); // same iterator type
+
+static_assert(noexcept(vec.begin())); // is noexcept
+static_assert(noexcept(seqan3::begin(vec)) == noexcept(vec.begin())); // perfect noexcept-forwarding
+//! [CPO Member overload]
+
+//! [CPO ADL overload]
+// seqan3::begin CPO that will call the "begin" function per ADL
+other_library::foo foo{};
+
+static_assert(std::same_as<decltype(seqan3::begin(foo)), decltype(begin(foo))>); // same value type
+
+static_assert(!noexcept(begin(foo))); // isn't noexcept
+static_assert(noexcept(seqan3::begin(foo)) == noexcept(begin(foo))); // perfect noexcept-forwarding
+//! [CPO ADL overload]
+
+//! [CPO SFINAE friendly]
+auto cpo_is_sfinae_friendly(...) -> void;
+
+template <typename range_t>
+auto cpo_is_sfinae_friendly(range_t && range) -> decltype(seqan3::begin(range));
+
+// seqan3::begin itself is SFINAE friendly, i.e. no-hard compiler errors, if no cpo overload matches
+static_assert(std::same_as<decltype(cpo_is_sfinae_friendly(0)), void>);
+
+static_assert(std::same_as<decltype(cpo_is_sfinae_friendly(vec)), decltype(vec.begin())>);
+//! [CPO SFINAE friendly]
