63ec11296c9453f6d8c1fdd68a29b40eb3050dcc
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2021-04-14 18:27:27 +0200

    [DOC] Declare range API-stability (gap_decorator)

--- include/seqan3/range/decorator/all.hpp
+++ include/seqan3/range/decorator/all.hpp
@@ -8,15 +8,13 @@
 /*!\file
  * \brief Meta-header for the \link decorator decorator submodule \endlink.
  * \author Svenja Mehringer <svenja.mehringer AT fu-berlin.de>
- */
-
-#pragma once
-
-#include <seqan3/core/platform.hpp>
-#include <seqan3/range/decorator/gap_decorator.hpp>
-
-/*!\defgroup decorator Decorator
+ *
+ * \defgroup decorator Decorator
  * \brief The decorator submodule contains special SeqAn decorators and generic decorator concepts.
  * \ingroup range
  * \sa seqan3/range/decorator/all.hpp
  */
+
+#pragma once
+
+#include <seqan3/range/decorator/gap_decorator.hpp>
