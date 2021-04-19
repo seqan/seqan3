5eb6a00fed41fb193c250d2c26ff88c225748a25
Lydia Buntrock <lydia.buntrock@fu-berlin.de>
2021-04-19 12:01:34 +0200

    [MISC] add type alias with deprecation for seqan3::sam_dna16
    Signed-off-by: Lydia Buntrock <lydia.buntrock@fu-berlin.de>

--- include/seqan3/alphabet/nucleotide/sam_dna16.hpp
+++ include/seqan3/alphabet/nucleotide/sam_dna16.hpp
@@ -8,12 +8,18 @@
 /*!\file
  * \brief [DEPRECATED] Provides seqan3::dna16sam.
  * \author Svenja Mehringer <svenja.mehringer AT fu-berlin.de>
- * \deprecated This header will be removed in 3.1. Please use seqan3/alphabet/nucleotide/dna16sam.hpp instead.
+ * \deprecated This header will be removed in 3.1.0; Please \#include seqan3/alphabet/nucleotide/dna16sam.hpp instead.
  */
 
 #pragma once
 
 #include <seqan3/alphabet/nucleotide/dna16sam.hpp>
+
+namespace seqan3
+{
+//!\deprecated Please use seqan3::dna16sam instead.
+using sam_dna16 SEQAN3_DEPRECATED_310 = seqan3::dna16sam;
+} // namespace seqan3
 
 SEQAN3_DEPRECATED_HEADER(
    "This header is deprecated and will be removed in SeqAn-3.1.0; Please #include <seqan3/alphabet/nucleotide/dna16sam.hpp> instead.")
