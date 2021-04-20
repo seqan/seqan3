d3d4a06169b48db8ff27da2ed8a4ac9b105fde44
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2021-04-20 19:40:25 +0200

    [FIX] slice_view: explictly type erase to replace {seqan3,std}::views::{take,drop}

--- include/seqan3/core/platform.hpp
+++ include/seqan3/core/platform.hpp
@@ -332,6 +332,15 @@
 #   endif
 #endif
 
+//!\brief See https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100139
+#ifndef SEQAN3_WORKAROUND_GCC_100139 // not yet fixed
+#   if defined(__GNUC__)
+#       define SEQAN3_WORKAROUND_GCC_100139 1
+#   else
+#       define SEQAN3_WORKAROUND_GCC_100139 0
+#   endif
+#endif
+
 /*!\brief This is needed to support CentOS 7 or RHEL 7; Newer CentOS's include a more modern default-gcc version making
  *        this macro obsolete.
  *
