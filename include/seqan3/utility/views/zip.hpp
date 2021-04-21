41df6c9e52e0cc31c74cdb3b23b1dc714cd4b27c
simonsasse <simonsasse97@gmail.com>
2021-04-21 15:29:40 +0200

    apply review

--- include/seqan3/utility/views/zip.hpp
+++ include/seqan3/utility/views/zip.hpp
@@ -12,10 +12,11 @@
 
 #pragma once
 
+#include <seqan3/std/ranges>
+
 #include <range/v3/view/zip.hpp>
 
 #include <seqan3/core/platform.hpp>
-#include <seqan3/std/ranges>
 
 namespace seqan3::views
 {
