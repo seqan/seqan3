3d48ed4f9e52d5f1ace028f924de7f1086eabc07
Lydia Buntrock <lydia.buntrock@fu-berlin.de>
2021-04-21 16:23:07 +0200

    [TEST, DOC] Update phred94 construction in snippet
    Signed-off-by: Lydia Buntrock <lydia.buntrock@fu-berlin.de>

--- test/snippet/alphabet/quality/phred94.cpp
+++ test/snippet/alphabet/quality/phred94.cpp
@@ -1,6 +1,8 @@
 #include <seqan3/alphabet/quality/phred94.hpp>
 #include <seqan3/core/debug_stream.hpp>
 
+using seqan3::operator""_phred94;
+
 int main()
 {
     seqan3::phred94 phred;
@@ -9,10 +11,13 @@ int main()
     seqan3::debug_stream << phred.to_char() << "\n";  // '#'
     seqan3::debug_stream << phred.to_rank() << "\n";  // 2
 
-    seqan3::phred94 another_phred{75};
+    seqan3::phred94 another_phred = '('_phred94;
+    seqan3::debug_stream << another_phred.to_phred() << "\n"; // 7
+
+    another_phred.assign_phred(75);
     seqan3::debug_stream << another_phred.to_phred() << "\n"; // 75
 
     seqan3::phred94 a_third_phred;
-    another_phred.assign_phred(105); // converted down to 93
-    seqan3::debug_stream << another_phred.to_phred() << "\n"; // 93
+    a_third_phred.assign_phred(105); // converted down to 93
+    seqan3::debug_stream << a_third_phred.to_phred() << "\n"; // 93
 }
