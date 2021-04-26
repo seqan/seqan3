51347e9ffd5713ecd68ff2cc8f041dfe8c94ad33
Mitra Darja Darvish <mitra.darvish@fu-berlin.de>
2021-04-26 13:38:09 +0200

    [MISC] Fix include order.

--- include/seqan3/utility/views/enforce_random_access.hpp
+++ include/seqan3/utility/views/enforce_random_access.hpp
@@ -12,11 +12,12 @@
 
 #pragma once
 
+#include <seqan3/std/iterator>
+#include <seqan3/std/ranges>
+
 #include <seqan3/range/views/detail.hpp>
 #include <seqan3/range/detail/inherited_iterator_base.hpp>
 #include <seqan3/range/concept.hpp>
-#include <seqan3/std/iterator>
-#include <seqan3/std/ranges>
 
 namespace seqan3::detail
 {
