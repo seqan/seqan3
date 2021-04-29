3cf190ce1895ee47472caca7c179397c16ea720e
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2021-04-29 23:11:36 +0200

    [FIX] gcc-10: missing RANGES_DEEP_STL_INTEGRATION
    resolves https://github.com/seqan/product_backlog/issues/372

--- test/unit/std/ranges_test.cpp
+++ test/unit/std/ranges_test.cpp
@@ -35,3 +35,21 @@ TEST(ranges_test, gcc10bug_rangev3_1514)
         EXPECT_EQ(*std::ranges::begin(iota), 0u);
     }
 }
+
+// https://github.com/seqan/product_backlog/issues/372
+TEST(ranges_test, issue372)
+{
+#ifdef __cpp_lib_ranges // >= gcc-10, range-v3 bug
+    std::string vec{};
+    std::istringstream istringstream{vec};
+
+    auto v1 = std::ranges::subrange{std::istream_iterator<char>{istringstream}, std::default_sentinel};
+    auto v2 = std::views::take(v1, 1);
+
+    using iterator2_t = std::ranges::iterator_t<decltype(v2)>;
+    EXPECT_TRUE(std::indirectly_readable<iterator2_t>);
+    EXPECT_TRUE(ranges::indirectly_readable<iterator2_t>); // this failed
+    EXPECT_TRUE(std::input_iterator<iterator2_t>);
+    EXPECT_TRUE(ranges::input_iterator<iterator2_t>); // this failed
+#endif // __cpp_lib_ranges
+}
