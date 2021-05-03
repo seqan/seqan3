bbebf0fbfcd9a22f9d8790f5733f75abf082709e
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2021-05-03 16:06:05 +0200

    [MISC] deprecate seqan3/range/container/*.hpp

---
+++ include/seqan3/range/container/aligned_allocator.hpp
@@ -0,0 +1,19 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2020, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2020, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \author Marcel Ehrhardt <marcel.ehrhardt AT fu-berlin.de>
+ * \brief Provides seqan3::aligned_allocator.
+ * \deprecated This header will be removed in 3.1. Please use seqan3/utility/container/aligned_allocator.hpp instead.
+ */
+
+#pragma once
+
+#include <seqan3/utility/container/aligned_allocator.hpp>
+
+SEQAN3_DEPRECATED_HEADER(
+   "This header is deprecated and will be removed in SeqAn-3.1.0; Please #include <seqan3/utility/container/aligned_allocator.hpp> instead.")
---
+++ include/seqan3/range/container/dynamic_bitset.hpp
@@ -0,0 +1,19 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2020, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2020, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \author Enrico Seiler <enrico.seiler@fu-berlin.de>
+ * \brief A constexpr bitset implementation with dynamic size at compile time.
+ * \deprecated This header will be removed in 3.1. Please use seqan3/utility/container/dynamic_bitset.hpp instead.
+ */
+
+#pragma once
+
+#include <seqan3/utility/container/dynamic_bitset.hpp>
+
+SEQAN3_DEPRECATED_HEADER(
+   "This header is deprecated and will be removed in SeqAn-3.1.0; Please #include <seqan3/utility/container/dynamic_bitset.hpp> instead.")
