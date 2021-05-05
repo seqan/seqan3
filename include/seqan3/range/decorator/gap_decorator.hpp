9d222bed2eb38129d2603f4b89638753e726e467
marehr <marehr-github@marehr.dialup.fu-berlin.de>
2021-05-06 01:38:53 +0200

    [MISC] deprecate seqan3::gap_decorator header

---
+++ include/seqan3/range/decorator/gap_decorator.hpp
@@ -0,0 +1,20 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2020, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2020, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
+/*!\file
+ * \brief [DEPRECATED] Provides seqan3::gap_decorator.
+ * \author Marie Hoffmann <marie.hoffmann AT fu-berlin.de>
+ * \author Svenja Mehringer <svenja.mehringer AT fu-berlin.de>
+ * \deprecated This header will be removed in 3.1. Please use seqan3/alignment/decorator/gap_decorator.hpp instead.
+ */
+
+#pragma once
+
+#include <seqan3/alignment/decorator/gap_decorator.hpp>
+
+SEQAN3_DEPRECATED_HEADER(
+   "This header is deprecated and will be removed in SeqAn-3.1.0; Please #include <seqan3/alignment/decorator/gap_decorator.hpp> instead.")
