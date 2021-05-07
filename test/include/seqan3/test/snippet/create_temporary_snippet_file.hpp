33a395ed47709bef98d7eb5a68aae615cf1b68cd
Marcel <marehr@users.noreply.github.com>
2021-05-07 16:33:20 +0200

    Update test/include/seqan3/test/snippet/create_temporary_snippet_file.hpp

--- test/include/seqan3/test/snippet/create_temporary_snippet_file.hpp
+++ test/include/seqan3/test/snippet/create_temporary_snippet_file.hpp
@@ -1,3 +1,10 @@
+// -----------------------------------------------------------------------------------------------------
+// Copyright (c) 2006-2020, Knut Reinert & Freie Universität Berlin
+// Copyright (c) 2016-2020, Knut Reinert & MPI für molekulare Genetik
+// This file may be used, modified and/or redistributed under the terms of the 3-clause BSD-License
+// shipped with this file and also available at: https://github.com/seqan/seqan3/blob/master/LICENSE.md
+// -----------------------------------------------------------------------------------------------------
+
 #pragma once
 
 #include <seqan3/std/filesystem>
