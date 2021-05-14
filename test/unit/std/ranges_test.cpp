90c276a9febee7715dd6e655d0a933ba0ebe167b
Marcel <marehr@users.noreply.github.com>
2021-05-14 16:17:12 +0200

    Borrowed range (#2651)
    * [MISC] fix std::ranges::borrowed_range definition for all applicable views
    
    https://eel.is/c++draft/ranges.syn defines more views to be
    `std::ranges::borrowed_range`s.
    
    This PR adds all of them.
    
    Doing this showed that std::views::take isn't a borrowed range in
    gcc-10, this PR will define that property also for gcc-10.
    
    * [FIX] gcc<=9: string_view is a borrowed_range
    
    * [TEST] add regression tests for SEQAN3_WORKAROUND_GCC_100139
    
    * Update include/seqan3/std/ranges
    
    Co-authored-by: MitraDarja <mitra.darvish@fu-berlin.de>
    
    * Apply suggestions from code review
    
    Co-authored-by: MitraDarja <mitra.darvish@fu-berlin.de>

--- test/unit/std/ranges_test.cpp
+++ test/unit/std/ranges_test.cpp
@@ -8,9 +8,167 @@
 #include <gtest/gtest.h>
 
 #include <seqan3/std/ranges>
+#include <seqan3/std/span>
 #include <string>
+#include <string_view>
+
+#include <seqan3/test/expect_same_type.hpp>
 
 #include <range/v3/view/take.hpp>
+
+TEST(ranges_test, string_view)
+{
+    std::string_view s{};
+    EXPECT_TRUE(std::ranges::borrowed_range<decltype(s)>);
+    EXPECT_TRUE(std::ranges::viewable_range<decltype(s)>);
+    EXPECT_TRUE(std::ranges::view<decltype(s)>);
+
+    EXPECT_TRUE(ranges::cpp20::borrowed_range<decltype(s)>);
+    EXPECT_TRUE(ranges::cpp20::viewable_range<decltype(s)>);
+    EXPECT_TRUE(ranges::cpp20::view<decltype(s)>);
+}
+
+TEST(ranges_test, span)
+{
+    std::span<int> s{};
+    EXPECT_TRUE(std::ranges::borrowed_range<decltype(s)>);
+    EXPECT_TRUE(std::ranges::viewable_range<decltype(s)>);
+    EXPECT_TRUE(std::ranges::view<decltype(s)>);
+
+    EXPECT_TRUE(ranges::cpp20::borrowed_range<decltype(s)>);
+    EXPECT_TRUE(ranges::cpp20::viewable_range<decltype(s)>);
+    EXPECT_TRUE(ranges::cpp20::view<decltype(s)>);
+}
+
+TEST(ranges_test, subrange)
+{
+    std::string s{};
+    std::ranges::subrange<std::string::iterator, std::string::iterator> v{s.begin(), s.end(), 0};
+    EXPECT_TRUE(std::ranges::borrowed_range<decltype(v)>);
+    EXPECT_TRUE(std::ranges::viewable_range<decltype(v)>);
+    EXPECT_TRUE(std::ranges::view<decltype(v)>);
+
+    EXPECT_TRUE(ranges::cpp20::borrowed_range<decltype(v)>);
+    EXPECT_TRUE(ranges::cpp20::viewable_range<decltype(v)>);
+    EXPECT_TRUE(ranges::cpp20::view<decltype(v)>);
+}
+
+TEST(ranges_test, empty_view)
+{
+    EXPECT_TRUE(std::ranges::borrowed_range<std::ranges::empty_view<int>>);
+    EXPECT_TRUE(std::ranges::viewable_range<std::ranges::empty_view<int>>);
+    EXPECT_TRUE(std::ranges::view<std::ranges::empty_view<int>>);
+
+    EXPECT_TRUE(ranges::cpp20::borrowed_range<std::ranges::empty_view<int>>);
+    EXPECT_TRUE(ranges::cpp20::viewable_range<std::ranges::empty_view<int>>);
+    EXPECT_TRUE(ranges::cpp20::view<std::ranges::empty_view<int>>);
+}
+
+TEST(ranges_test, iota_view)
+{
+    EXPECT_TRUE(std::ranges::borrowed_range<decltype(std::views::iota(0))>);
+    EXPECT_TRUE(std::ranges::viewable_range<decltype(std::views::iota(0))>);
+    EXPECT_TRUE(std::ranges::view<decltype(std::views::iota(0))>);
+
+    EXPECT_TRUE(ranges::cpp20::borrowed_range<decltype(std::views::iota(0))>);
+    EXPECT_TRUE(ranges::cpp20::viewable_range<decltype(std::views::iota(0))>);
+    EXPECT_TRUE(ranges::cpp20::view<decltype(std::views::iota(0))>);
+}
+
+TEST(ranges_test, ref_view)
+{
+    std::string s{};
+    EXPECT_TRUE(std::ranges::borrowed_range<decltype(std::views::all(s))>);
+    EXPECT_TRUE(std::ranges::viewable_range<decltype(std::views::all(s))>);
+    EXPECT_TRUE(std::ranges::view<decltype(std::views::all(s))>);
+
+    EXPECT_TRUE(ranges::cpp20::borrowed_range<decltype(std::views::all(s))>);
+    EXPECT_TRUE(ranges::cpp20::viewable_range<decltype(std::views::all(s))>);
+    EXPECT_TRUE(ranges::cpp20::view<decltype(std::views::all(s))>);
+}
+
+TEST(ranges_test, take_view)
+{
+    std::string s{};
+
+#if !SEQAN3_WORKAROUND_GCC_100139
+    EXPECT_SAME_TYPE(decltype(std::views::take(std::span<int>{}, 0)), std::span<int>);
+    EXPECT_SAME_TYPE(decltype(std::views::take(std::string_view{}, 0)), std::string_view);
+    EXPECT_SAME_TYPE(decltype(std::views::take(std::views::empty<int>, 0)), std::ranges::empty_view<int>);
+    EXPECT_SAME_TYPE(decltype(std::views::take(std::views::iota(0, 1), 0)), decltype(std::views::iota(0, 1)));
+
+    EXPECT_SAME_TYPE(decltype(std::views::take(s, 0)),
+                     (std::ranges::subrange<std::string::iterator, std::string::iterator>));
+#endif // !SEQAN3_WORKAROUND_GCC_100139
+
+    EXPECT_TRUE(std::ranges::borrowed_range<decltype(std::views::take(s, 0))>);
+    EXPECT_TRUE(std::ranges::viewable_range<decltype(std::views::take(s, 0))>);
+    EXPECT_TRUE(std::ranges::view<decltype(std::views::take(s, 0))>);
+
+    EXPECT_TRUE(ranges::cpp20::borrowed_range<decltype(std::views::take(s, 0))>);
+    EXPECT_TRUE(ranges::cpp20::viewable_range<decltype(std::views::take(s, 0))>);
+    EXPECT_TRUE(ranges::cpp20::view<decltype(std::views::take(s, 0))>);
+}
+
+TEST(ranges_test, drop_view)
+{
+    std::string s{};
+
+#if !SEQAN3_WORKAROUND_GCC_100139
+    EXPECT_SAME_TYPE(decltype(std::views::drop(std::span<int>{}, 0)), std::span<int>);
+    EXPECT_SAME_TYPE(decltype(std::views::drop(std::string_view{}, 0)), std::string_view);
+    EXPECT_SAME_TYPE(decltype(std::views::drop(std::views::empty<int>, 0)), std::ranges::empty_view<int>);
+    EXPECT_SAME_TYPE(decltype(std::views::drop(std::views::iota(0, 1), 0)), decltype(std::views::iota(0, 1)));
+
+    EXPECT_SAME_TYPE(decltype(std::views::drop(s, 0)),
+                     (std::ranges::subrange<std::string::iterator, std::string::iterator>));
+#endif // !SEQAN3_WORKAROUND_GCC_100139
+
+    EXPECT_TRUE(std::ranges::borrowed_range<decltype(std::views::drop(s, 0))>);
+    EXPECT_TRUE(std::ranges::viewable_range<decltype(std::views::drop(s, 0))>);
+    EXPECT_TRUE(std::ranges::view<decltype(std::views::drop(s, 0))>);
+
+    EXPECT_TRUE(ranges::cpp20::borrowed_range<decltype(std::views::drop(s, 0))>);
+    EXPECT_TRUE(ranges::cpp20::viewable_range<decltype(std::views::drop(s, 0))>);
+    EXPECT_TRUE(ranges::cpp20::view<decltype(std::views::drop(s, 0))>);
+}
+
+TEST(ranges_test, filter_view)
+{
+    std::string s{};
+    auto lambda = [](auto &&) { return true; };
+    EXPECT_FALSE(std::ranges::borrowed_range<decltype(std::views::filter(s, lambda))>);
+    EXPECT_TRUE(std::ranges::viewable_range<decltype(std::views::filter(s, lambda))>);
+    EXPECT_TRUE(std::ranges::view<decltype(std::views::filter(s, lambda))>);
+
+    EXPECT_FALSE(ranges::cpp20::borrowed_range<decltype(std::views::filter(s, lambda))>);
+    EXPECT_TRUE(ranges::cpp20::viewable_range<decltype(std::views::filter(s, lambda))>);
+    EXPECT_TRUE(ranges::cpp20::view<decltype(std::views::filter(s, lambda))>);
+}
+
+TEST(ranges_test, common_view)
+{
+    std::string s{};
+    EXPECT_TRUE(std::ranges::borrowed_range<decltype(std::views::common(s))>);
+    EXPECT_TRUE(std::ranges::viewable_range<decltype(std::views::common(s))>);
+    EXPECT_TRUE(std::ranges::view<decltype(std::views::common(s))>);
+
+    EXPECT_TRUE(ranges::cpp20::borrowed_range<decltype(std::views::common(s))>);
+    EXPECT_TRUE(ranges::cpp20::viewable_range<decltype(std::views::common(s))>);
+    EXPECT_TRUE(ranges::cpp20::view<decltype(std::views::common(s))>);
+}
+
+TEST(ranges_test, reverse_view)
+{
+    std::string s{};
+    EXPECT_TRUE(std::ranges::borrowed_range<decltype(std::views::reverse(s))>);
+    EXPECT_TRUE(std::ranges::viewable_range<decltype(std::views::reverse(s))>);
+    EXPECT_TRUE(std::ranges::view<decltype(std::views::reverse(s))>);
+
+    EXPECT_TRUE(ranges::cpp20::borrowed_range<decltype(std::views::reverse(s))>);
+    EXPECT_TRUE(ranges::cpp20::viewable_range<decltype(std::views::reverse(s))>);
+    EXPECT_TRUE(ranges::cpp20::view<decltype(std::views::reverse(s))>);
+}
 
 TEST(ranges_test, combine_std_with_range_v3)
 {
