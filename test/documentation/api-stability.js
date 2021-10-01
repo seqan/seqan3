4abb8798e76f13e32c665ba657ba905a604df038
smehringer <svenja.mehringer@fu-berlin.de>
2021-10-01 09:31:08 +0200

    [DOC] Make api tags more visible.

--- test/documentation/api-stability.js
+++ test/documentation/api-stability.js
@@ -13,12 +13,28 @@ $(document).ready(function()
         $(this).find('div.memproto').html(new_html);
     }
 
-    var add_labels = function(api_type, entry_type)
+    var add_labels = function(api_type, api_color, entry_obj, entry_type)
     {
         if (entry_type == "member")
+        {
             $(this).find('span.mlabels').prepend('<span class="mlabel ' + api_type + '">' + api_type + '</span>');
+
+            // get header above the item, append the api_type tag to the header and change the bg color
+            prev = entry_obj;
+            do prev = prev.previousSibling; while(prev && prev.nodeType !== 1);
+
+            prev.innerText = prev.innerText + '  ' + '[' + api_type + '] ';
+
+            if (api_type != "stable-api") // for now, leave stable api background in blue.
+            {
+                prev.style.backgroundImage = "none";
+                prev.style.backgroundColor = api_color;
+            }
+        }
         else if (entry_type == "header")
+        {
             $('h2.groupheader:contains("Detailed Description")').append('<span class="mlabel ' + api_type + ' header">' + api_type + '</span>');
+        }
     }
 
     var select_and_add_label = function(entry_obj, entry_type)
@@ -32,13 +48,13 @@ $(document).ready(function()
             add_mlabel.call(this);
 
         if (is_no_api)
-            add_labels.call(this, "no-api", entry_type);
+            add_labels.call(this, "no-api", '#ffb3b3', entry_obj, entry_type);
         else if (is_experimental_api)
-            add_labels.call(this, "experimental-api", entry_type);
+            add_labels.call(this, "experimental-api", '#FBF0E0', entry_obj, entry_type);
         else if (is_stable_api)
-            add_labels.call(this, "stable-api", entry_type);
+            add_labels.call(this, "stable-api", '#e8ffe8', entry_obj, entry_type);
         else
-            add_labels.call(this, "no-api", entry_type);
+            add_labels.call(this, "no-api", '#ffb3b3', entry_obj, entry_type);
     }
 
     // Badges for members
