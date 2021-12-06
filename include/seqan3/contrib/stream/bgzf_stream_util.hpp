fdaee58b4729a9cb8db611eb317171dabba900a8
Svenja Mehringer <svenja.mehringer@fu-berlin.de>
2021-12-06 11:22:20 +0100

    [MISC] Change default number of threads for bgzf decompression.

--- include/seqan3/contrib/stream/bgzf_stream_util.hpp
+++ include/seqan3/contrib/stream/bgzf_stream_util.hpp
@@ -35,10 +35,9 @@
 namespace seqan3::contrib
 {
 
-/*!\brief A static variable indicating the number of threads to use for the bgzf-streams.
- *       Defaults to std::thread::hardware_concurrency.
+/*!\brief A static variable indicating the number of threads to use for the bgzf-streams. Defaults to 4.
  */
-inline static uint64_t bgzf_thread_count = std::thread::hardware_concurrency();
+inline static uint64_t bgzf_thread_count = 4;
 
 // ============================================================================
 // Forwards
