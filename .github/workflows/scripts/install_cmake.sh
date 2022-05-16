937a34a8d231d539c7b386a5be7388906dd66c91
Enrico Seiler <enrico.seiler@hotmail.de>
2022-05-16 11:44:22 +0200

    [INFRA] Update CI

--- .github/workflows/scripts/install_cmake.sh
+++ .github/workflows/scripts/install_cmake.sh
@@ -11,7 +11,7 @@ else
 fi
 
 if [ "$RUNNER_OS" == "Linux" ] && [[ "$BUILD" =~ ^(external_project)$ ]]; then
-    sudo apt-get install libidn11 # this is needed for cmake 3.4 within the external_project test
+    sudo apt-get install libidn12 # this is needed for cmake 3.4 within the external_project test
 fi
 
 mkdir -p /tmp/cmake-download
