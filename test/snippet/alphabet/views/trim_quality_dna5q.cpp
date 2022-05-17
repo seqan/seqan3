60789bda8db5c1a5a95eca7799ec867cfc6b4211
Enrico Seiler <enrico.seiler@hotmail.de>
2022-05-17 12:43:50 +0200

    [FIX,TEST] Unused variable warnings in snippets

--- test/snippet/alphabet/views/trim_quality_dna5q.cpp
+++ test/snippet/alphabet/views/trim_quality_dna5q.cpp
@@ -6,6 +6,7 @@
 #include <seqan3/alphabet/quality/phred42.hpp>
 #include <seqan3/alphabet/views/to_char.hpp>
 #include <seqan3/alphabet/views/trim_quality.hpp>
+#include <seqan3/core/debug_stream.hpp>
 
 int main()
 {
@@ -16,20 +17,16 @@ int main()
                                    {'G'_dna5, '?'_phred42},
                                    {'A'_dna5, '5'_phred42},
                                    {'T'_dna5, '+'_phred42}};
-    std::vector<seqan3::dna5q> cmp{{'A'_dna5, 'I'_phred42},
-                                   {'G'_dna5, 'I'_phred42},
-                                   {'G'_dna5, '?'_phred42},
-                                   {'A'_dna5, '5'_phred42}};
 
     // trim by phred_value
     auto v1 = vec | seqan3::views::trim_quality(20u);
-    assert(std::ranges::equal(v1, cmp));
+    seqan3::debug_stream << v1 << '\n'; // AGGA
 
     // trim by quality character; in this case the nucleotide part of the character is irrelevant
     auto v2 = vec | seqan3::views::trim_quality(seqan3::dna5q{'C'_dna5, '5'_phred42});
-    assert(std::ranges::equal(v2, cmp));
+    seqan3::debug_stream << v2 << '\n'; // AGGA
 
     // combinability
     auto v3 = seqan3::views::trim_quality(vec, 20u) | seqan3::views::to_char;
-    assert(std::ranges::equal(std::string{"AGGA"}, v3));
+    seqan3::debug_stream << v3 << '\n'; // AGGA
 }
--- test/snippet/alphabet/views/trim_quality_phred42.cpp
+++ test/snippet/alphabet/views/trim_quality_phred42.cpp
@@ -4,6 +4,7 @@
 #include <seqan3/alphabet/quality/phred42.hpp>
 #include <seqan3/alphabet/views/to_char.hpp>
 #include <seqan3/alphabet/views/trim_quality.hpp>
+#include <seqan3/core/debug_stream.hpp>
 
 using namespace seqan3::literals;
 
@@ -12,14 +13,18 @@ int main()
     std::vector<seqan3::phred42> vec{"II?5+"_phred42};
 
     // trim by phred_value
-    auto v1 = vec | seqan3::views::trim_quality(20u);                            // == ['I','I','?','5']
+    auto v1 = vec | seqan3::views::trim_quality(20u);
+    seqan3::debug_stream << v1 << '\n'; // II?5
 
     // trim by quality character
-    auto v2 = vec | seqan3::views::trim_quality('I'_phred42);                    // == ['I','I']
+    auto v2 = vec | seqan3::views::trim_quality('I'_phred42);
+    seqan3::debug_stream << v2 << '\n'; // II
 
     // function syntax
-    auto v3 = seqan3::views::trim_quality(vec, '5'_phred42);                     // == ['I','I','?','5']
+    auto v3 = seqan3::views::trim_quality(vec, '5'_phred42);
+    seqan3::debug_stream << v3 << '\n'; // II?5
 
     // combinability
-    auto v4 = seqan3::views::trim_quality(vec, 20u) | seqan3::views::to_char;    // == "II?5"
+    auto v4 = seqan3::views::trim_quality(vec, 20u) | seqan3::views::to_char;
+    seqan3::debug_stream << v4 << '\n'; // II?5
 }
