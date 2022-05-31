385bf6b5a6f93e501138e5d6936fd5a5dae8b091
Svenja Mehringer <svenja.mehringer@fu-berlin.de>
2022-05-31 15:07:20 +0200

    [FEATURE] seqan3::views::chunk (#2975)
    * [FEATURE] Our implementation of seqan3::views::chunk.
    
    * nail care
    
    Co-authored-by: Enrico Seiler <eseiler@users.noreply.github.com>
    
    * update year
    
    * test output range with reference
    
    Co-authored-by: Enrico Seiler <eseiler@users.noreply.github.com>
    
    * more nail care
    
    * revert change
    
    * test default_constructible dependence
    
    * Update test/unit/utility/views/chunk_test.cpp
    
    * Update test/unit/utility/views/chunk_test.cpp
    
    * more tiny nail care
    
    * debug
    
    * Update test/unit/utility/views/chunk_test.cpp
    
    * Update test/unit/utility/views/chunk_test.cpp
    
    * fix
    
    * pair-programming review
    
    * break dependency cycle
    
    * fix and add tests
    
    Co-authored-by: Enrico Seiler <eseiler@users.noreply.github.com>
    Co-authored-by: Enrico Seiler <enrico.seiler@hotmail.de>

---
+++ test/snippet/utility/views/chunk.cpp
@@ -0,0 +1,28 @@
+#include <seqan3/std/ranges>
+#include <vector>
+
+#include <seqan3/alphabet/nucleotide/dna5.hpp>
+#include <seqan3/core/debug_stream.hpp>
+#include <seqan3/utility/views/chunk.hpp>
+#include <seqan3/utility/views/single_pass_input.hpp>
+
+int main()
+{
+    using namespace seqan3::literals;
+
+    std::vector<int> sequences{1, 2, 3, 4, 5, 6, 7, 8};
+
+    seqan3::debug_stream << (sequences | seqan3::views::chunk(3)) << '\n'; // [[1,2,3],[4,5,6],[7,8]]
+
+    // Note the behaviour on pure input ranges:
+    // When incrementing the chunk view iterator, the former chunk is fully consumed by the iterator.
+    // In other words, it is ensured that you always start at the beginning of a new chunk.
+    auto input_view = sequences | seqan3::views::single_pass_input | seqan3::views::chunk(3);
+
+    for (auto && val : input_view)
+        seqan3::debug_stream << "first value in subrange: " << *val.begin() << '\n'; // only access first value of chunk
+    // prints:
+    // first value in subrange: 1
+    // first value in subrange: 4
+    // first value in subrange: 7
+}
