60535a49aa668541a871e26bc0431639faa2b8f6
Enrico Seiler <enrico.seiler@hotmail.de>
2022-11-05 19:57:05 +0100

    [FIX] IBF snippet

--- test/snippet/search/dream_index/interleaved_bloom_filter_constructor_uncompress.cpp
+++ test/snippet/search/dream_index/interleaved_bloom_filter_constructor_uncompress.cpp
@@ -3,12 +3,12 @@
 int main()
 {
     // Construct an uncompressed Interleaved Bloom Filter.
-    seqan3::interleaved_bloom_filter ibf{seqan3::bin_count{4u}, seqan3::bin_size{128u}, seqan3::hash_function_count{3}};
+    seqan3::interleaved_bloom_filter ibf{seqan3::bin_count{9u}, seqan3::bin_size{128u}, seqan3::hash_function_count{3}};
 
-    // Insert the values `126`, `712` and `237` into bins `0`, `3` and `9` of the Interleaved Bloom Filter.
+    // Insert the values `126`, `712` and `237` into bins `0`, `3` and `8` of the Interleaved Bloom Filter.
     ibf.emplace(126, seqan3::bin_index{0u});
     ibf.emplace(712, seqan3::bin_index{3u});
-    ibf.emplace(237, seqan3::bin_index{9u});
+    ibf.emplace(237, seqan3::bin_index{8u});
 
     // Construct an immutable, compressed Interleaved Bloom Filter.
     seqan3::interleaved_bloom_filter<seqan3::data_layout::compressed> ibf_compressed{ibf};
