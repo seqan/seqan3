d2104a36023f94bcb4b44357d8f77247f6fc51f0
Enrico Seiler <enrico.seiler@hotmail.de>
2022-11-10 13:41:13 +0100

    [INFRA] Update script

--- test/scripts/add_snippets_to_cookbook.sh
+++ test/scripts/add_snippets_to_cookbook.sh
@@ -7,7 +7,7 @@
 # -----------------------------------------------------------------------------------------------------
 #
 # Usage: create_all_hpp.sh <SeqAn3 root directory>
-# Will update the cookbook to inlcude all snippets of the documentation
+# Will update the cookbook to include all snippets of the documentation.
 
 COOKBOOK="doc/cookbook/index.md"
 
@@ -16,12 +16,12 @@ if [[ $# -ne 1 ]]; then
     exit 1
 fi
 
-if [[ ! -d $1 ]]; then
+if [[ ! -d "$1" ]]; then
     echo "The directory $1 does not exist."
     exit 1
 fi
 
-if [[ ! -f $1/${COOKBOOK} ]]; then
+if [[ ! -f "$1/${COOKBOOK}" ]]; then
     echo "The directory $1 does not seem to be the SeqAn3 root directory."
     echo "Cannot find cookbook file $1/${COOKBOOK}."
     exit 1
@@ -36,17 +36,17 @@ fi
 
 LINE_NUMBER_OF_KEY_LINE=$(grep -n "${KEY_LINE_IN_COOKBOOK}" ${COOKBOOK} | cut -d : -f 1)
 
-TMP_FILE=$(mktemp);
 # Copy cookbook except the snippet includes into tmp file
+TMP_FILE=$(mktemp)
 head -n ${LINE_NUMBER_OF_KEY_LINE} ${COOKBOOK} > ${TMP_FILE}
 
-# Iterate through all file in include/seqan3/test/snippet/*
-for snippet in $(find test/snippet/ -type f -name "*.cpp"); do
-    echo "\include ${snippet}" >> $TMP_FILE
+# Iterate through all files in test/snippet/*
+# Order of results from find is not fixed, so we sort the results alphabetically.
+# Snippets of doc would be: find ./doc/ -type f -name "*.cpp" -and -not -path "./doc/cookbook/*"
+for snippet in $(find test/snippet/ -type f -name "*.cpp" | sort); do
+    echo "\include ${snippet}" >> ${TMP_FILE}
 done
 
-#Update cookbook
+# Update cookbook
 echo "Updating cookbook at ${COOKBOOK}"
-cp ${TMP_FILE} ${COOKBOOK}
-
-rm ${TMP_FILE}
+mv ${TMP_FILE} ${COOKBOOK}
