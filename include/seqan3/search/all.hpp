60301e6d0fc7955cef94408b390fae3d9806994a
smehringer <svenja.mehringer@gmail.com>
2022-12-07 13:55:19 +0100

    [DOC] Improve search module doc.

--- include/seqan3/search/all.hpp
+++ include/seqan3/search/all.hpp
@@ -12,9 +12,9 @@
 
 /*!\defgroup search Search
  * \brief Data structures and approximate string search algorithms for large collection of text (e.g. DNA).
- * \brief Meta-header for the \link search Search module \endlink.
  *
  * \details
+ *
  * Searching is a key component in many sequence analysis tools. The search module is a powerful and easy way to search
  * sequences in a large text or an arbitrary nested collection of texts. When it comes to searching, indices are a core
  * component for searching large amounts of data and are used for tools such as read mappers, assemblers or protein
@@ -39,15 +39,57 @@
  * # Search algorithm
  *
  * The Search module offers a unified search interface seqan3::search.
- * The function chooses the best search method based on the provided index and an optional configuration.
- * \snippet snippet/search/search.cpp Performing search
+ * The function chooses the best search method based on the provided **index** and an optional **configuration**.
  *
- * # Available Indices
- * ## (bidirectional) FM index
- * Two FM indices are available: seqan3::fm_index and seqan3::bi_fm_index.
- * The search algorithms for these FM indices implement either a trivial backtracking approach or an optimum search scheme.
- * The latter are currently only available for searches with up to three errors using bidirectional indices.
- * In the future we plan to improve the optimum search schemes to handle higher error counts.
+ * \snippet test/snippet/search/search_with_user_callback.cpp search_with_config
+ *
+ * ### The search configuration
+ *
+ * The `seqan3::search` algorithm can be configured in multiple ways. You simply pass a respective `config` object to
+ * the algorithm:
+ *
+ * The search configuration is independent of the index you use. It specifies for example how many errors are allowed
+ * in the your search to still consider it successful.
+ *
+ * See detailed documentation on \ref search_configuration for details.
+ *
+ * ### The search result
+ *
+ * The result is a range over "hits". We call a *hit* the position of a successful search.
+ * A *hit* in SeqAn is represented by the seqan3::search_result. You can configure what's part of the search result
+ * with the \ref search_configuration_subsection_output.
+ *
+ * You can iterate over the results with a normal for loop:
+ *
+ * \snippet test/snippet/search/search.cpp search_result_loop
+ *
+ * ### Which index do I use?
+ *
+ * In order to choose the right Index for your use case, have a look at the next section
+ * (\ref search_available_indices). The documentation of the respective index will lead you to a more detailed
+ * description on how to use our search algorithm.
+ *
+ * # Available Indices \anchor search_available_indices
+ *
+ * ## seqan3::fm_index
+ *
+ * The seqan3::fm_index implements a original [FM Index](https://en.wikipedia.org/wiki/FM-index) with a trivial
+ * backtracking approach. It is recommended for searches with no errors (exact searches). For eaxact searches, the
+ * original FM index is slightly faster than the bidirectional FM Index and in general it is only half the size.
+ *
+ * ## seqan3::bi_fm_index
+ *
+ * The seqan3::bi_fm_index is a bidirectional FM index [1]. It improves the original FM index by allowing to extend the
+ * query to the right and the left.  This make the bidirectional FM index more efficient than its predecessor when
+ * searching with errors (approximate search).
+ * The performance gain is enabled by using (optimum) *search schemes*.
+ * Currently, using *search schemes* is only supported for searches with up to three errors.
+ * In the future we plan to improve the search schemes to handle higher error counts.
+ *
+ * **Reference:**
+ *
+ * [1] Optimum Search Schemes for Approximate String Matching Using Bidirectional FM-Index. bioRxiv, 301085.
+ * https://doi.org/10.1101/301085, *Kianfar, K., Pockrandt, C., Torkamandi, B., Luo, H., & Reinert, K. (2018).*
  *
  * \if DEV
  * ### Implementation details of Search Schemes
@@ -70,33 +112,6 @@
  *
  * \endif
  *
- * **Reference:**
- *
- * Kianfar, K., Pockrandt, C., Torkamandi, B., Luo, H., & Reinert, K. (2018).
- *
- * Optimum Search Schemes for Approximate String Matching Using Bidirectional FM-Index. bioRxiv, 301085.
- * https://doi.org/10.1101/301085
- *
- *
- * \if KMER
- * ## K-mer index
- *
- * A k-mer index can be used to efficiently retrieve all occurrences of a certain k-mer in the text.
- * The k-mer can be either an exact string of length k or it can contain one or more wildcards,
- * which denote positions of arbitrary characters.
- *
- * An exact k-mer is represented as seqan3::ungapped, and wildcards can be defined with seqan3::shape.
- * Please check the respective documentation for details and examples.
- *
- * \note The k-mer index is not yet implemented.
- * \sa seqan3::views::kmer_hash
- * \sa seqan3::views::minimiser
- * \endif
- *
- * # Configuration
- *
- * The approximate string search algorithm can be configured in multiple ways.
- * See \ref search_configuration for details.
  */
 
 #pragma once
