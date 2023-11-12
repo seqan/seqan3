b3de151ecb2c59ee857f2f3b2b6dbf95dd1452f4
Enrico Seiler <enrico.seiler@hotmail.de>
2023-11-12 13:53:43 +0100

    [FIX] libc++: sequence_generator_test
    <random> is not portable

--- test/unit/test/sequence_generator_test.cpp
+++ test/unit/test/sequence_generator_test.cpp
@@ -21,8 +21,13 @@ TEST(random_sequence_generator, fixed_length)
 
     seqan3::test::random_sequence_generator<seqan3::dna4_vector> random_sequence_generator{3};
 
+#ifdef _LIBCPP_VERSION
+    EXPECT_EQ(random_sequence_generator(random_engine), "GTC"_dna4);
+    EXPECT_EQ(random_sequence_generator(random_engine), "GAG"_dna4);
+#else
     EXPECT_EQ(random_sequence_generator(random_engine), "TAG"_dna4);
     EXPECT_EQ(random_sequence_generator(random_engine), "AGC"_dna4);
+#endif
 }
 
 // generate (single) variable sized sequence, generates sequence with size ± size_variance.
@@ -35,9 +40,15 @@ TEST(random_sequence_generator, variable_length_and_different_random_engines)
 
     seqan3::test::random_sequence_generator<seqan3::dna5_vector> random_sequence_generator{4, 2};
 
+#ifdef _LIBCPP_VERSION
+    EXPECT_EQ(random_sequence_generator(random_engine1), "TCCGTT"_dna5);
+    EXPECT_EQ(random_sequence_generator(random_engine2), "CGTAAG"_dna5);
+    EXPECT_EQ(random_sequence_generator(random_engine3), "GA"_dna5);
+#else
     EXPECT_EQ(random_sequence_generator(random_engine1), "CCAN"_dna5);
     EXPECT_EQ(random_sequence_generator(random_engine2), "NN"_dna5);
     EXPECT_EQ(random_sequence_generator(random_engine3), "AG"_dna5);
+#endif
 }
 
 // generate a collection of variable/fixed sized sequences
@@ -54,7 +65,11 @@ TEST(random_sequence_generator, sequence_collection)
                               return random_sequence_generator(random_engine);
                           });
 
+#ifdef _LIBCPP_VERSION
+    std::vector<seqan3::dna5_vector> resulting_sequences = {"CTACN"_dna5, "AC"_dna5, "CTCGG"_dna5, "GGCANN"_dna5};
+#else
     std::vector<seqan3::dna5_vector> resulting_sequences = {"TA"_dna5, "GANG"_dna5, "TGNTCC"_dna5, "NNGC"_dna5};
+#endif
     EXPECT_EQ(sequences, resulting_sequences);
 }
 
@@ -73,9 +88,15 @@ TEST(random_sequence_generator, sequence_pairs)
             return {random_sequence_generator(random_engine), random_sequence_generator(random_engine)};
         });
 
+#ifdef _LIBCPP_VERSION
+    std::vector<std::tuple<seqan3::dna5_vector, seqan3::dna5_vector>> resulting_pairs{{"CTACN"_dna5, "AC"_dna5},
+                                                                                      {"CTCGG"_dna5, "GGCANN"_dna5},
+                                                                                      {"CACA"_dna5, "CNC"_dna5}};
+#else
     std::vector<std::tuple<seqan3::dna5_vector, seqan3::dna5_vector>> resulting_pairs{{"TA"_dna5, "GANG"_dna5},
                                                                                       {"TGNTCC"_dna5, "NNGC"_dna5},
                                                                                       {"GG"_dna5, "AA"_dna5}};
+#endif
 
     EXPECT_EQ(sequence_pairs, resulting_pairs);
 }
