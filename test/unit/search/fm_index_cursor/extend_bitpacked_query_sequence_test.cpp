2d9809e36bc7884c52b326b41a2d26b9ac9400a1
rrahn <rene.rahn@fu-berlin.de>
2024-07-16 15:21:43 +0200

    Adds missing copyright text in test file

--- test/unit/search/fm_index_cursor/extend_bitpacked_query_sequence_test.cpp
+++ test/unit/search/fm_index_cursor/extend_bitpacked_query_sequence_test.cpp
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2006-2024 Knut Reinert & Freie Universität Berlin
+// SPDX-FileCopyrightText: 2016-2024 Knut Reinert & MPI für molekulare Genetik
+// SPDX-License-Identifier: BSD-3-Clause
+
 #include <gtest/gtest.h>
 
 #include <vector>
