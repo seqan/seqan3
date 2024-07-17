d26ac7e58fd9e39d13398cbd6fab43062c1e3f59
Enrico Seiler <enrico.seiler@hotmail.de>
2024-07-17 11:48:30 +0200

    [MISC] Set CMake policy

--- test/external_project/install-sharg.cmake
+++ test/external_project/install-sharg.cmake
@@ -4,6 +4,11 @@
 
 cmake_minimum_required (VERSION 3.14)
 
+# https://cmake.org/cmake/help/latest/policy/CMP0135.html
+if (POLICY CMP0135)
+    cmake_policy (SET CMP0135 NEW)
+endif ()
+
 # We can only set one CMAKE_SYSTEM_PREFIX_PATH, i.e. it cannot be a list.
 # Hence we need to reuse the SEQAN3_SYSTEM_PREFIX.
 if (NOT DEFINED SEQAN3_SYSTEM_PREFIX)
