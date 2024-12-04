0b59eaac9aab29091a6673076e7d9ad1f1ecf91c
Enrico Seiler <enrico.seiler@hotmail.de>
2024-12-04 17:24:27 +0100

    [FIX] version check URL

--- include/seqan3/argument_parser/detail/version_check.hpp
+++ include/seqan3/argument_parser/detail/version_check.hpp
@@ -190,7 +190,7 @@ public:
         // build up command for server call
         std::string command = program + // no user defined input
                               " " + out_file.string() + " "
-                            + std::string{"http://seqan-update.informatik.uni-tuebingen.de/check/SeqAn3_"} +
+                            + std::string{"https://seqan-update.cs.uni-tuebingen.de/check/SeqAn3_"} +
 #ifdef __linux
                               "Linux" +
 #elif __APPLE__
