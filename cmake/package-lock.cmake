4274a796366e1be260b40fd149404570252ad975
Enrico Seiler <enrico.seiler@hotmail.de>
2024-12-15 16:44:01 +0100

    [INFRA] Make versions cache variables
    Exemplary use case: Installed benchmark version is 1.8.3.
    CPM will not use 1.8.3, because 1.8.3 < 1.9.1.
    
    Making the versions CACHE allows setting them on the command line.
    Making them FORCE means that the value given on the command line is not
    cached. The versions has to be provided for each CMake call, and will
    revert to the default value otherwise.

--- cmake/package-lock.cmake
+++ cmake/package-lock.cmake
@@ -13,8 +13,10 @@
 # Ideally, both are the same, which might not always be possible: https://github.com/cpm-cmake/CPM.cmake/issues/603
 # This is needed to support CPM_USE_LOCAL_PACKAGES
 
+# cmake-format: off
+
 # cereal
-set (SEQAN3_CEREAL_VERSION 1.3.2)
+set (SEQAN3_CEREAL_VERSION 1.3.2 CACHE STRING "" FORCE)
 CPMDeclarePackage (cereal
                    NAME cereal
                    VERSION ${SEQAN3_CEREAL_VERSION}
@@ -25,14 +27,14 @@ CPMDeclarePackage (cereal
 # Use URL download of the commit archive such that we do not clone submodules
 # Package name is still sdsl (name as v2 at xxsds/sdsl), but sdsl-lite is not currently being packaged
 # To avoid accidentally using the older sdsl, NAME is set to sdsl-lite
-set (SEQAN3_SDSL_VERSION 14cd017027ea742353fc5b500d1cb1d95896b77e)
+set (SEQAN3_SDSL_VERSION 14cd017027ea742353fc5b500d1cb1d95896b77e CACHE STRING "" FORCE)
 CPMDeclarePackage (sdsl-lite
                    NAME sdsl-lite
                    URL https://github.com/xxsds/sdsl-lite/archive/${SEQAN3_SDSL_VERSION}.tar.gz # master
                    DOWNLOAD_ONLY YES
                    QUIET YES)
 # benchmark
-set (SEQAN3_BENCHMARK_VERSION 1.9.1)
+set (SEQAN3_BENCHMARK_VERSION 1.9.1 CACHE STRING "" FORCE)
 CPMDeclarePackage (benchmark
                    NAME benchmark
                    VERSION ${SEQAN3_BENCHMARK_VERSION}
@@ -41,7 +43,7 @@ CPMDeclarePackage (benchmark
                    OPTIONS "BENCHMARK_ENABLE_TESTING OFF" "BENCHMARK_ENABLE_WERROR OFF"
                            "CMAKE_MESSAGE_LOG_LEVEL WARNING")
 # googletest
-set (SEQAN3_GOOGLETEST_VERSION 1.15.2)
+set (SEQAN3_GOOGLETEST_VERSION 1.15.2 CACHE STRING "" FORCE)
 CPMDeclarePackage (googletest
                    NAME GTest
                    VERSION ${SEQAN3_GOOGLETEST_VERSION}
@@ -49,7 +51,7 @@ CPMDeclarePackage (googletest
                    SYSTEM TRUE
                    OPTIONS "BUILD_GMOCK OFF" "INSTALL_GTEST OFF" "CMAKE_MESSAGE_LOG_LEVEL WARNING")
 # doxygen-awesome
-set (SEQAN3_DOXYGEN_AWESOME_VERSION 2.3.4)
+set (SEQAN3_DOXYGEN_AWESOME_VERSION 2.3.4 CACHE STRING "" FORCE)
 CPMDeclarePackage (doxygen_awesome
                    NAME doxygen_awesome
                    VERSION ${SEQAN3_DOXYGEN_AWESOME_VERSION}
@@ -57,7 +59,7 @@ CPMDeclarePackage (doxygen_awesome
                    DOWNLOAD_ONLY TRUE
                    QUIET YES)
 # seqan2
-set (SEQAN3_SEQAN2_VERSION 578218d744522c43bc9e769f14be9c26d7e5de85)
+set (SEQAN3_SEQAN2_VERSION 578218d744522c43bc9e769f14be9c26d7e5de85 CACHE STRING "" FORCE)
 CPMDeclarePackage (seqan
                    NAME seqan
                    GIT_TAG ${SEQAN3_SEQAN2_VERSION} # main
@@ -65,7 +67,7 @@ CPMDeclarePackage (seqan
                    DOWNLOAD_ONLY YES
                    QUIET YES)
 # use_ccache
-set (SEQAN3_USE_CCACHE_VERSION d2a54ef555b6fc2d496a4c9506dbeb7cf899ce37)
+set (SEQAN3_USE_CCACHE_VERSION d2a54ef555b6fc2d496a4c9506dbeb7cf899ce37 CACHE STRING "" FORCE)
 CPMDeclarePackage (use_ccache
                    NAME use_ccache
                    GIT_TAG ${SEQAN3_USE_CCACHE_VERSION} # main
@@ -73,3 +75,5 @@ CPMDeclarePackage (use_ccache
                    SOURCE_SUBDIR ccache
                    SYSTEM TRUE
                    EXCLUDE_FROM_ALL TRUE)
+
+# cmake-format: on
