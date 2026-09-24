0400b695f0b83cd195b2743c8b1b027372a506cf
Enrico Seiler <enrico.seiler@hotmail.de>
2026-09-24 14:59:16 +0200

    fix: charconv

--- test/unit/std/charconv_float_test.cpp
+++ test/unit/std/charconv_float_test.cpp
@@ -7,11 +7,12 @@
 // make sure that including the std header does not produce any errors
 // see https://github.com/seqan/seqan3/issues/2352
 #include <seqan3/std/charconv>
+#include <cerrno>
 #include <charconv>
 #include <cmath>
-#include <concepts>
 #include <iostream>
 #include <limits>
+#include <string_view>
 
 // =============================================================================
 // std::from_chars for float, double and long double
@@ -264,6 +265,228 @@ TYPED_TEST(from_char_real_test, non_valid_strings)
         auto res = std::from_chars(&str[0], &str[0] + str.size(), val);
         EXPECT_FLOAT_EQ(val, TypeParam{42});
         EXPECT_EQ(res.ec, std::errc::invalid_argument);
+    }
+}
+
+// =============================================================================
+// from_chars with chars_format and partial ranges
+// =============================================================================
+
+// "1234567890" repeated ten times and "8999" at the end.
+static constexpr std::string_view long_number{"123456789012345678901234567890123456789012345678901234567890"
+                                              "12345678901234567890123456789012345678999"};
+static_assert(long_number.size() == 101u);
+
+TYPED_TEST(from_char_real_test, general_partial_range)
+{
+    // The range [first, last) excludes the last character.
+    {
+        TypeParam val{42};
+        auto res = std::from_chars(long_number.data(),
+                                   long_number.data() + long_number.size() - 1u,
+                                   val,
+                                   std::chars_format::general);
+        EXPECT_EQ(res.ptr, long_number.data() + long_number.size() - 1u);
+        if constexpr (std::is_same_v<TypeParam, float>)
+        {
+            EXPECT_EQ(res.ec, std::errc::result_out_of_range); // libc++ sets val to inf.
+        }
+        else
+        {
+            EXPECT_EQ(res.ec, std::errc{});
+            EXPECT_NEAR(val, 1.2345678901234567890e99, 1e84);
+        }
+    }
+
+    {
+        TypeParam val{42};
+        std::string_view str{"15 foo"};
+        auto res = std::from_chars(str.data(), str.data() + str.size() - 1u, val, std::chars_format::general);
+        EXPECT_FLOAT_EQ(val, TypeParam{15});
+        EXPECT_EQ(res.ec, std::errc{});
+        EXPECT_EQ(res.ptr, str.data() + 2);
+    }
+
+    {
+        TypeParam val{42};
+        std::string_view str{"5000000000"};
+        auto res = std::from_chars(str.data(), str.data() + str.size() - 1u, val, std::chars_format::general);
+        EXPECT_FLOAT_EQ(val, TypeParam{500'000'000});
+        EXPECT_EQ(res.ec, std::errc{});
+        EXPECT_EQ(res.ptr, str.data() + str.size() - 1u);
+    }
+
+    // Longer than the internal stack buffer.
+    {
+        TypeParam val{42};
+        std::string str = std::string(150u, '0') + "12.5e1";
+        auto res = std::from_chars(str.data(), str.data() + str.size() - 1u, val, std::chars_format::general);
+        EXPECT_FLOAT_EQ(val, TypeParam{12.5});
+        EXPECT_EQ(res.ec, std::errc{});
+        EXPECT_EQ(res.ptr, str.data() + str.size() - 2u);
+    }
+
+    for (std::string_view const str : {"bar", " 42", "+42"})
+    {
+        TypeParam val{42};
+        auto res = std::from_chars(str.data(), str.data() + str.size() - 1u, val, std::chars_format::general);
+        EXPECT_FLOAT_EQ(val, TypeParam{42}) << str;
+        EXPECT_EQ(res.ec, std::errc::invalid_argument) << str;
+        EXPECT_EQ(res.ptr, str.data()) << str;
+    }
+
+    // The 0x prefix is not permitted: "0x123" is parsed as the value "0" with unparsed remainder "x123".
+    {
+        TypeParam val{42};
+        std::string_view str{"0x123"};
+        auto res = std::from_chars(str.data(), str.data() + str.size(), val);
+        EXPECT_FLOAT_EQ(val, TypeParam{0});
+        EXPECT_EQ(res.ec, std::errc{});
+        EXPECT_EQ(res.ptr, str.data() + 1);
+    }
+
+    // A stale errno does not affect the result.
+    {
+        TypeParam val{42};
+        std::string_view str{"1.5"};
+        errno = ERANGE;
+        auto res = std::from_chars(str.data(), str.data() + str.size(), val);
+        EXPECT_FLOAT_EQ(val, TypeParam{1.5});
+        EXPECT_EQ(res.ec, std::errc{});
+        EXPECT_EQ(res.ptr, str.data() + str.size());
+    }
+}
+
+TYPED_TEST(from_char_real_test, hex)
+{
+    {
+        TypeParam val{42};
+        std::string_view str{""};
+        auto res = std::from_chars(str.data(), str.data() + str.size(), val, std::chars_format::hex);
+        EXPECT_FLOAT_EQ(val, TypeParam{42});
+        EXPECT_EQ(res.ec, std::errc::invalid_argument);
+        EXPECT_EQ(res.ptr, str.data());
+    }
+
+    {
+        TypeParam val{42};
+        auto res =
+            std::from_chars(long_number.data(), long_number.data() + long_number.size(), val, std::chars_format::hex);
+        EXPECT_EQ(res.ptr, long_number.data() + long_number.size());
+        if constexpr (std::is_same_v<TypeParam, float>)
+        {
+            EXPECT_EQ(res.ec, std::errc::result_out_of_range); // libc++ sets val to inf.
+        }
+        else
+        {
+            EXPECT_EQ(res.ec, std::errc{});
+            EXPECT_NEAR(val, 0x1.2345678901234567890p400, 1e105);
+        }
+    }
+
+    {
+        TypeParam val{42};
+        std::string_view str{"15 foo"};
+        auto res = std::from_chars(str.data(), str.data() + str.size(), val, std::chars_format::hex);
+        EXPECT_FLOAT_EQ(val, TypeParam{0x15});
+        EXPECT_EQ(res.ec, std::errc{});
+        EXPECT_EQ(res.ptr, str.data() + 2);
+    }
+
+    {
+        TypeParam val{42};
+        std::string_view str{"bar"};
+        auto res = std::from_chars(str.data(), str.data() + str.size(), val, std::chars_format::hex);
+        EXPECT_FLOAT_EQ(val, TypeParam{0xba});
+        EXPECT_EQ(res.ec, std::errc{});
+        EXPECT_EQ(res.ptr, str.data() + 2);
+    }
+
+    {
+        TypeParam val{42};
+        std::string_view str{"5000000000"};
+        auto res = std::from_chars(str.data(), str.data() + str.size(), val, std::chars_format::hex);
+        EXPECT_FLOAT_EQ(val, TypeParam{0x50'00'00'00'00});
+        EXPECT_EQ(res.ec, std::errc{});
+        EXPECT_EQ(res.ptr, str.data() + str.size());
+    }
+
+    {
+        TypeParam val{42};
+        std::string_view str{"-1a"};
+        auto res = std::from_chars(str.data(), str.data() + str.size(), val, std::chars_format::hex);
+        EXPECT_FLOAT_EQ(val, TypeParam{-0x1a});
+        EXPECT_EQ(res.ec, std::errc{});
+        EXPECT_EQ(res.ptr, str.data() + str.size());
+    }
+
+    // The 0x prefix is not permitted: "0x123" is parsed as the value "0" with unparsed remainder "x123".
+    {
+        TypeParam val{42};
+        std::string_view str{"0x123"};
+        auto res = std::from_chars(str.data(), str.data() + str.size(), val, std::chars_format::hex);
+        EXPECT_FLOAT_EQ(val, TypeParam{0});
+        EXPECT_EQ(res.ec, std::errc{});
+        EXPECT_EQ(res.ptr, str.data() + 1);
+    }
+
+    {
+        TypeParam val{};
+        std::string_view str{"-inf"};
+        auto res = std::from_chars(str.data(), str.data() + str.size(), val, std::chars_format::hex);
+        EXPECT_EQ(val, -std::numeric_limits<TypeParam>::infinity());
+        EXPECT_EQ(res.ec, std::errc{});
+        EXPECT_EQ(res.ptr, str.data() + str.size());
+    }
+
+    for (std::string_view const str : {" 42", "+42", "xyz"})
+    {
+        TypeParam val{42};
+        auto res = std::from_chars(str.data(), str.data() + str.size(), val, std::chars_format::hex);
+        EXPECT_FLOAT_EQ(val, TypeParam{42}) << str;
+        EXPECT_EQ(res.ec, std::errc::invalid_argument) << str;
+        EXPECT_EQ(res.ptr, str.data()) << str;
+    }
+}
+
+TYPED_TEST(from_char_real_test, fixed_and_scientific)
+{
+    // fixed: The exponent is not part of the pattern.
+    {
+        TypeParam val{42};
+        std::string_view str{"1.5e3"};
+        auto res = std::from_chars(str.data(), str.data() + str.size(), val, std::chars_format::fixed);
+        EXPECT_FLOAT_EQ(val, TypeParam{1.5});
+        EXPECT_EQ(res.ec, std::errc{});
+        EXPECT_EQ(res.ptr, str.data() + 3);
+    }
+
+    // scientific: The exponent is required.
+    {
+        TypeParam val{42};
+        std::string_view str{"1.5e3"};
+        auto res = std::from_chars(str.data(), str.data() + str.size(), val, std::chars_format::scientific);
+        EXPECT_FLOAT_EQ(val, TypeParam{1500});
+        EXPECT_EQ(res.ec, std::errc{});
+        EXPECT_EQ(res.ptr, str.data() + str.size());
+    }
+
+    for (std::string_view const str : {"1.5", "1.5e", "1.5e+"})
+    {
+        TypeParam val{42};
+        auto res = std::from_chars(str.data(), str.data() + str.size(), val, std::chars_format::scientific);
+        EXPECT_FLOAT_EQ(val, TypeParam{42}) << str;
+        EXPECT_EQ(res.ec, std::errc::invalid_argument) << str;
+        EXPECT_EQ(res.ptr, str.data()) << str;
+    }
+
+    {
+        TypeParam val{};
+        std::string_view str{"inf"};
+        auto res = std::from_chars(str.data(), str.data() + str.size(), val, std::chars_format::scientific);
+        EXPECT_EQ(val, std::numeric_limits<TypeParam>::infinity());
+        EXPECT_EQ(res.ec, std::errc{});
+        EXPECT_EQ(res.ptr, str.data() + str.size());
     }
 }
 
